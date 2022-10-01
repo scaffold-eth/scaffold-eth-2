f6e476c5f767eff37c7747cafce2ac0a583680f3
Shravan Sunder <ShravanSunder@users.noreply.github.com>
2022-09-30 21:30:21 -0400

    #1 better comments

--- packages/frontend/pages/index.tsx
+++ packages/frontend/pages/index.tsx
@@ -8,7 +8,7 @@ import { useTempTestContract } from "~~/components/useTempTestContract";
 import { tempContract } from "~~/generated/tempContract";
 import Address from "../components/scaffold-eth/Address";
 
-// todo: move this later scaffold config
+// todo: move this later scaffold config.  See TAutoConnect for comments on each prop
 const tempAutoConnectConfig: TAutoConnect = {
   enableBurnerWallet: true,
   alwaysAutoConnectToBurnerOnLoad: false,
