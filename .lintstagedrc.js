52bf1f3811c4948151441e51c73dd8aa008793f0
Shiv Bhonde <shivbhonde04@gmail.com>
2022-10-03 15:15:51 +0530

    Setup basic husky, lint-staged and Github Actions
    Co-authored-by: Carlos Sánchez <oceanrdn@gmail.com>

---
+++ .lintstagedrc.js
@@ -0,0 +1,10 @@
+const path = require("path");
+
+const buildEslintCommand = (filenames) =>
+  `yarn next:lint --fix --file ${filenames
+    .map((f) => path.relative(path.join("packages", "frontend"), f))
+    .join(" --file ")}`;
+
+module.exports = {
+  "packages/frontend/**/*.{ts,tsx}": [buildEslintCommand],
+};
