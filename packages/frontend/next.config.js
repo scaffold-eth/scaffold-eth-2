5c999d510761b98f7fe7f981f1e55b2a0676134e
Carlos Sánchez <oceanrdn@gmail.com>
2022-10-09 18:52:57 -0500

    Remove unrelated/extra stuff

--- packages/frontend/next.config.js
+++ packages/frontend/next.config.js
@@ -3,8 +3,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: { esmExternals: true, externalDir: true, swcFileReading: true },
-  productionBrowserSourceMaps: true,
 };
 
 module.exports = nextConfig;
