15ee827e1519aaf30e93be8c08c31a0ede41f390
NaimBijapure <naim@vinnovatetechnologies.com>
2022-10-14 11:48:46 +0530

    Title:update:removed Balance component from this pr to avoid conflicts

--- packages/frontend/pages/EthComponents.tsx
+++ packages/frontend/pages/EthComponents.tsx
@@ -1,15 +1,11 @@
 import type { NextPage } from "next";
-import Balance from "../components/scaffold-eth/Balance";
-import AddressInput from "../components/scaffold-eth/AddressInput";
 import { useState } from "react";
+import AddressInput from "../components/scaffold-eth/AddressInput";
 
 const EthComponents: NextPage = () => {
   const [address, setAddress] = useState<string>("");
   return (
     <div className="px-8 flex flex-col items-center justify-center">
-      <h2 className="text-xl">Balance component</h2>
-      <Balance address="0x0fAb64624733a7020D332203568754EB1a37DB89" price={1350} wrapperClasses="" />
-
       <h2 className="text-xl">Address Input</h2>
       <AddressInput value={address} onChange={setAddress} placeholder="Enter address" wrapperClasses="" />
     </div>
