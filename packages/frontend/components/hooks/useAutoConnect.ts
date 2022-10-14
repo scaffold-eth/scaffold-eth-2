7c28a34ce476e790c1fba55ad566af40334ea963
Shravan Sunder <ShravanSunder@users.noreply.github.com>
2022-10-14 11:57:45 -0400

    #1 Burner Wallet (#8)
    * #1 started creating BurnerConnector.  additionally edited some config
    
    * #1 changing rainbow connectors.  extracted connectors to its own folder
    
    * #1 burner wallet is working
    
    * #1 preminimnarly autoconnect working.  added some temp files to help check.  added useBurnerSigner
    
    * #1 Finished autoconnect and burner wallet
    
    * #1 Finished autoconnect and burner wallet
    
    * #1 Finished autoconnect and burner wallet
    
    * #1 better comments
    
    * #1 better comments
    
    * #1 better comments and cleaned up structure
    
    * #1 simplified options
    
    * minor
    
    * Remove unrelated/extra stuff
    
    * Prettify with exiting rules
    
    * We want prettier to fix errors with eol without causing errors.  warnings will cause prettier to autofix errors without unecessary issues.
    
    * Revert "We want prettier to fix errors with eol without causing errors.  warnings will cause prettier to autofix errors without unecessary issues."
    
    This reverts commit 1282c22614aeceab6dbe5e0b84841b9ec94e6bfa.
    
    * #1 We want prettier to fix errors with eol without causing errors.  warnings will cause prettier to autofix errors without unecessary issues.
    
    * We want vscode to have linting properlly work on all of the workspaces.  We also want it to exclude settings files so that search and files are easier to use.
    
    * #1 fixed issue with eslint
    
    * #1 fixed nots
    
    * #1 remove unused variables
    
    * #1 removed comments
    
    * #1 fixes for bugs.  TODO: write is no longer working with burner, need to find out why signer doesn't work
    
    * #1 pushed changes to get the signer to work properly
    
    * #1 removed imports
    
    * #1 changed how initial save of sk was happening
    
    * updated git ignore
    
    Co-authored-by: Carlos Sánchez <oceanrdn@gmail.com>

---
+++ packages/frontend/components/hooks/useAutoConnect.ts
@@ -0,0 +1,79 @@
+import { useEffect } from "react";
+import { Connector, useAccount, useConnect } from "wagmi";
+import { useEffectOnce, useLocalStorage } from "usehooks-ts";
+import { burnerWalletId, defaultBurnerChainId } from "~~/web3/wagmi-burner";
+
+export type TAutoConnect = {
+  /**
+   * Enable the burner wallet.  If this is disabled, burner wallet is entierly disabled
+   */
+  enableBurnerWallet: boolean;
+  /**
+   * Auto connect:
+   * 1. If the user was connected into a wallet before, on page reload reconnect automatically
+   * 2. If user is not connected to any wallet:  On reload, connect to burner wallet
+   */
+  autoConnect: boolean;
+};
+
+const walletIdStorageKey = "scaffoldEth2.wallet";
+
+/**
+ * This function will get the initial connector (if any), the app will connect to
+ * @param config
+ * @param previousWalletId
+ * @param connectors
+ * @returns
+ */
+const getInitialConnector = (
+  config: TAutoConnect,
+  previousWalletId: string,
+  connectors: Connector<any, any, any>[],
+): { connector: Connector | undefined; chainId?: number } | undefined => {
+  const allowBurner = config.enableBurnerWallet;
+
+  if (!previousWalletId) {
+    // The user was not connected to a wallet
+    if (allowBurner && config.autoConnect) {
+      const connector = connectors.find(f => f.id === burnerWalletId);
+      return { connector, chainId: defaultBurnerChainId };
+    }
+  } else {
+    // the user was connected to wallet
+    if (config.autoConnect) {
+      const connector = connectors.find(f => f.id === previousWalletId);
+      return { connector };
+    }
+  }
+
+  return undefined;
+};
+
+/**
+ * Automatically connect to a wallet/connector based on config and prior wallet
+ * @param config
+ */
+export const useAutoConnect = (config: TAutoConnect): void => {
+  const [walletId, setWalletId] = useLocalStorage<string>(walletIdStorageKey, "");
+  const connectState = useConnect();
+  const accountState = useAccount();
+
+  useEffect(() => {
+    if (accountState.isConnected) {
+      // user is connected, set walletName
+      setWalletId(accountState.connector?.id ?? "");
+    } else {
+      // user has disconnected, reset walletName
+      setWalletId("");
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [accountState.isConnected, accountState.connector?.name]);
+
+  useEffectOnce(() => {
+    const initialConnector = getInitialConnector(config, walletId, connectState.connectors);
+
+    if (initialConnector?.connector) {
+      connectState.connect({ connector: initialConnector.connector, chainId: initialConnector.chainId });
+    }
+  });
+};
--- packages/frontend/pages/index.tsx
+++ packages/frontend/pages/index.tsx
@@ -1,18 +1,39 @@
 import { ConnectButton } from "@rainbow-me/rainbowkit";
 import type { NextPage } from "next";
 import Head from "next/head";
+import { TAutoConnect, useAutoConnect } from "~~/components/hooks/useAutoConnect";
+import { useTempTestContract } from "~~/components/useTempTestContract";
 import Address from "../components/scaffold-eth/Address";
 
+// todo: move this later scaffold config.  See TAutoConnect for comments on each prop
+const tempAutoConnectConfig: TAutoConnect = {
+  enableBurnerWallet: true,
+  autoConnect: true,
+};
+
 const Home: NextPage = () => {
+  const tempTest = useTempTestContract();
+  useAutoConnect(tempAutoConnectConfig);
+
   return (
     <div className="px-8">
       <Head>
-        <title>Scaffod-eth App</title>
+        <title>Scaffold-eth App</title>
         <meta name="description" content="Created with 🏗 scaffold-eth" />
       </Head>
 
       <main className="flex items-center flex-col py-16">
-        <ConnectButton />
+        <ConnectButton
+          accountStatus={{
+            smallScreen: "avatar",
+            largeScreen: "full",
+          }}
+          showBalance={true}
+          chainStatus={{
+            smallScreen: "icon",
+            largeScreen: "icon",
+          }}
+        />
 
         <h1 className="text-center my-12 text-4xl">
           Welcome to{" "}
@@ -34,7 +55,9 @@ const Home: NextPage = () => {
         <Address address="0xd8da6bf26964af9d7eed9e03e53415d37aa96045" />
 
         <p>
-          <button className="btn btn-primary">Daisy UI Button</button>
+          <button className="btn btn-primary" onClick={() => tempTest.onClick()}>
+            Daisy UI Button
+          </button>
         </p>
       </main>
     </div>
