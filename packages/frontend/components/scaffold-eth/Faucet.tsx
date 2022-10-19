899e88290c914f903f8531c4739caede98776c6e
Shiv Bhonde <shivbhonde04@gmail.com>
2022-10-20 01:00:18 +0530

    clean up faucet

--- packages/frontend/components/scaffold-eth/Faucet.tsx
+++ packages/frontend/components/scaffold-eth/Faucet.tsx
@@ -1,47 +1,38 @@
 import { ethers } from "ethers";
-import React, { useMemo } from "react";
+import React, { useState } from "react";
 import { chain, useAccount, useNetwork } from "wagmi";
 import { getLocalProvider } from "~~/utils";
-// todo: Add condition for burner wallet
-// todo: Update Comments
+
+// Number of ETH faucet sends to an address
+const NUM_OF_ETH = "1";
 
 /**
-  Faucet button which lets you grab 1 eth.
+  Faucet button which lets you grab eth.
 **/
 
 export default function Faucet() {
   const { address } = useAccount();
   const { chain: ConnectedChain } = useNetwork();
+  const [loading, setLoading] = useState(false);
 
-  // const provider = useProvider();
-  // console.log("⚡️ ~ file: Faucet.tsx ~ line 20 ~ Faucet ~ provider", provider.getSigner());
+  const sendETH = async () => {
+    try {
+      setLoading(true);
+      const provider = getLocalProvider(chain.localhost);
+      const signer = provider?.getSigner();
+      await signer?.sendTransaction({ to: address, value: ethers.utils.parseEther(NUM_OF_ETH) });
+      setLoading(false);
+    } catch (error) {
+      console.error("⚡️ ~ file: Faucet.tsx ~ line 26 ~ sendETH ~ error", error);
+      setLoading(false);
+    }
+  };
 
-  // const { config } = usePrepareSendTransaction({
-  //   request: { to: address, value: ethers.utils.parseEther("1") },
-  // });
-  // console.log("⚡️ ~ file: Faucet.tsx ~ line 25 ~ Faucet ~ config", config);
-
-  const provider = useMemo(() => getLocalProvider(chain.localhost), []);
-
-  // const { data, isLoading, isSuccess, sendTransaction } = useSendTransaction(config);
-  // console.log("⚡️ ~ file: Faucet.tsx ~ line 24 ~ Faucet ~ data", data);
-
-  // If not on local chain return empty element
-  if (ConnectedChain?.id === 31337 && provider) {
+  // Render only on local chain
+  if (ConnectedChain?.id === 31337) {
     return (
-      <button
-        className="btn btn-primary"
-        onClick={async () => {
-          const signer = provider.getSigner();
-          await signer.sendTransaction({
-            value: ethers.utils.parseEther("1"),
-            to: address,
-            gasPrice: ethers.utils.parseUnits("4.1", "gwei"),
-            gasLimit: ethers.utils.hexlify(120000),
-          });
-        }}
-      >
-        Grab 1 eth
+      <button className={`btn btn-warning ${loading && "loading"} text-gray-700`} onClick={sendETH} disabled={loading}>
+        💰 Get funds from faucet
       </button>
     );
   } else {
