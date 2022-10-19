a1d4099251451612cfc1ffa8e860d0fa7d21f131
eda <edakturk96@gmail.com>
2022-10-19 18:02:11 -0500

    Add networks & private key conf to hardhat config (#24)

--- packages/hardhat/hardhat.config.ts
+++ packages/hardhat/hardhat.config.ts
@@ -1,6 +1,13 @@
+import * as dotenv from "dotenv";
+dotenv.config();
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "hardhat-deploy";
+
+// ToDo: add default api key
+const defaultApiKey = process.env.ALCHEMY_API_KEY ?? '';
+// ToDo: auto generate mnenoic for the deployer account
+const deployerPrivateKey = process.env.DEPLOYER_PRIVATE_KEY ?? '';
 
 const config: HardhatUserConfig = {
   defaultNetwork: "localhost",
@@ -8,6 +15,40 @@ const config: HardhatUserConfig = {
     deployer: {
       // By default, it will take the first account as deployer
       default: 0,
+    },
+  },
+  networks: {
+    arbitrum: {
+      url: `https://arb-mainnet.g.alchemy.com/v2/${defaultApiKey}`,
+      accounts: [deployerPrivateKey]
+    },
+    arbitrumGoerli: {
+      url: `https://arb-goerli.g.alchemy.com/v2/${defaultApiKey}`,
+      accounts: [deployerPrivateKey]
+    },
+    goerli: {
+      url: `https://eth-goerli.alchemyapi.io/v2/${defaultApiKey}`,
+      accounts: [deployerPrivateKey]
+    },
+    mainnet: {
+      url: `https://eth-mainnet.alchemyapi.io/v2/${defaultApiKey}`,
+      accounts: [deployerPrivateKey]
+    },
+    optimisim: {
+      url: `https://opt-mainnet.g.alchemy.com/v2/${defaultApiKey}`,
+      accounts: [deployerPrivateKey]
+    },
+    optimisimKovan: {
+      url: `https://opt-kovan.g.alchemy.com/v2/${defaultApiKey}`,
+      accounts: [deployerPrivateKey]
+    },
+    polygon: {
+      url: `https://polygon-mainnet.g.alchemy.com/v2/${defaultApiKey}`,
+      accounts: [deployerPrivateKey]
+    },
+    polygonMumbai: {
+      url: `https://polygon-mumbai.g.alchemy.com/v2/${defaultApiKey}`,
+      accounts: [deployerPrivateKey]
     },
   },
   solidity: "0.8.17",
