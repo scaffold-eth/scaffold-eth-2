cc3e231d97283671048c45f814ca43a59afff559
Shiv Bhonde | shivbhonde.eth <shivbhonde04@gmail.com>
2022-10-20 22:28:13 +0530

    Add the faucet component for the local network (#27)
    Co-authored-by: Carlos Sánchez <oceanrdn@gmail.com>

---
+++ packages/frontend/components/scaffold-eth/Faucet.tsx
@@ -0,0 +1,41 @@
+import { ethers } from "ethers";
+import React, { useState } from "react";
+import { chain, useAccount, useNetwork } from "wagmi";
+import { getLocalProvider } from "~~/utils/scaffold-eth";
+
+// Number of ETH faucet sends to an address
+const NUM_OF_ETH = "1";
+
+/**
+  Faucet button which lets you grab eth.
+**/
+
+export default function Faucet() {
+  const { address } = useAccount();
+  const { chain: ConnectedChain } = useNetwork();
+  const [loading, setLoading] = useState(false);
+
+  const sendETH = async () => {
+    try {
+      setLoading(true);
+      const provider = getLocalProvider(chain.localhost);
+      const signer = provider?.getSigner();
+      await signer?.sendTransaction({ to: address, value: ethers.utils.parseEther(NUM_OF_ETH) });
+      setLoading(false);
+    } catch (error) {
+      console.error("⚡️ ~ file: Faucet.tsx ~ line 26 ~ sendETH ~ error", error);
+      setLoading(false);
+    }
+  };
+
+  // Render only on local chain
+  if (ConnectedChain?.id !== chain.hardhat.id) {
+    return null;
+  }
+
+  return (
+    <button className={`btn btn-warning ${loading && "loading"} text-gray-700`} onClick={sendETH} disabled={loading}>
+      💰 Get funds from faucet
+    </button>
+  );
+}
