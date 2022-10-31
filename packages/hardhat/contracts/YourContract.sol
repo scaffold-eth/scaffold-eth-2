16ae7a73937723334f813354f7aa811362522095
Carlos Sánchez <oceanrdn@gmail.com>
2022-10-31 11:08:23 -0500

    Update test contract with all the types

--- packages/hardhat/contracts/YourContract.sol
+++ packages/hardhat/contracts/YourContract.sol
@@ -7,6 +7,9 @@ contract YourContract {
     event SetPurpose(address sender, string purpose);
 
     string public purpose = "Building Unstoppable Apps!!!";
+    uint256 public totalSupply = 2.5 ether;
+    mapping (address => uint) public counter;
+    bytes32 public myBytes;
 
     constructor() payable {
         // what should we do on deploy?
@@ -16,6 +19,16 @@ contract YourContract {
         purpose = newPurpose;
         console.log(msg.sender, "set purpose to",purpose);
         emit SetPurpose(msg.sender, purpose);
+    }
+
+    function withAddressAndAndValueIncrementCounter(address _add) payable public {
+        require(msg.value > 0.0001 ether, "Not enough");
+        counter[_add] += 1;
+    }
+
+    function withUint8AndBytes(uint8 _u, bytes32 _b) public {
+        counter[msg.sender] = _u;
+        myBytes = _b;
     }
 
     // to support receiving ETH by default
