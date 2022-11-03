b1c6cc846f27980ca502b53f1447d29bf7704579
Ataxia123 <vp.ca94@gmail.com>
2022-11-02 23:54:24 -0500

    removed extra stuff

--- packages/frontend/pages/index.tsx
+++ packages/frontend/pages/index.tsx
@@ -17,41 +17,18 @@ const Home: NextPage = () => {
   return (
     <div className="px-8">
       <Head>
-        <title>Scaffold-eth App</title>
+        <title>Scaffold-EthOs Starter Kit</title>
         <meta name="description" content="Created with 🏗 scaffold-eth" />
       </Head>
 
       <main className="flex items-center flex-col py-16">
         <h1 className="text-center my-12 text-4xl">
-          Welcome to{" "}
-          <a
-            className="text-blue-600 hover:underline"
-            href="https://github.com/scaffold-eth/se-2"
-            target="_blank"
-            rel="noreferrer"
-          >
-            scaffold-eth 2
+          Welcome to the{" "}
+          <a className="text-blue-600 hover:underline" href="https://ethOs.eth.limo" target="_blank" rel="noreferrer">
+            Scaffold-EthOs Starter Kit
           </a>
+          <div>a developer kit to build with EthereansOs</div>
         </h1>
-
-        <p className="text-center text-xl">
-          Get started by editing <code className="italic bg-gray-200">packages/frontend/pages/index.tsx</code>
-        </p>
-
-        <h3 className="font-bold">Address Component</h3>
-        <Address address="0xd8da6bf26964af9d7eed9e03e53415d37aa96045" />
-
-        <p>
-          <button className="btn btn-primary" onClick={() => tempTest.onClick()}>
-            Daisy UI Button
-          </button>
-        </p>
-
-        <h3 className="font-bold">Balance Component</h3>
-        <Balance address="0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266" />
-
-        <h3 className="font-bold mt-4">Address Input Component</h3>
-        <AddressInput placeholder="Enter address" />
       </main>
     </div>
   );
