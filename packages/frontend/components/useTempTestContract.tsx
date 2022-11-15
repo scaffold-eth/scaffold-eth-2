6d4b0a1ab9834b23f917426010eacb416808b154
Ataxia123 <vp.ca94@gmail.com>
2022-11-15 17:16:05 -0500

    changed tempteststuff

--- packages/frontend/components/useTempTestContract.tsx
+++ packages/frontend/components/useTempTestContract.tsx
@@ -1,29 +1,29 @@
 import { useEffect } from "react";
-import { useContractWrite, useContractRead, chain } from "wagmi";
-import { tempContract } from "~~/generated/tempContract";
+import { useContractRead, chain } from "wagmi";
+import { FarmMainRegularMinStakeABI } from "~~/contracts/farmingContract";
 import { useAppStore } from "~~/services/store/store";
 
 // todo remove this, this is until we have contract element
 
-const testChainId = chain.hardhat.id;
-
+const testChainId = chain.mainnet.id;
+// uses app store to create a slice of the contract state
 export const useTempTestContract = () => {
   const tempState = useAppStore(state => state.tempSlice.tempState);
-  const setTempState = useAppStore(state => state.tempSlice.setTempState);
+  //const setTempState = useAppStore(state => state.tempSlice.setTempState);
 
   useEffect(() => {
     console.log("test state, in useTempTestContract: " + tempState.tempStuff);
   }, [tempState?.tempStuff]);
-
+  // reads contract state
   const cRead = useContractRead({
-    addressOrName: tempContract.address,
-    contractInterface: tempContract.abi,
-    functionName: "purpose",
+    addressOrName: FarmMainRegularMinStakeABI.address,
+    contractInterface: FarmMainRegularMinStakeABI.abi,
+    functionName: "setups",
     chainId: testChainId,
     watch: true,
     cacheOnBlock: false,
   });
-
+  /*//writes contract state
   const cWrite = useContractWrite({
     mode: "recklesslyUnprepared",
     addressOrName: tempContract.address,
@@ -32,17 +32,19 @@ export const useTempTestContract = () => {
     args: "new purpose",
     chainId: testChainId,
   });
-
+  //*/
+  //when loading... read state
   useEffect(() => {
     if (cRead.isSuccess) {
       console.log("read contract:  ", cRead.data);
     }
   }, [cRead.data, cRead.isSuccess]);
-
+  /*// on click -> write state
   const onClick = () => {
     console.log("...attempting to write");
     cWrite.write?.();
     setTempState({ tempStuff: "wow i set the state" });
   };
   return { onClick };
+    //*/
 };
