77a3b458f316f8f2ac9741fd6760f577f91b23af
Ataxia123 <vp.ca94@gmail.com>
2022-11-15 22:55:13 -0500

    showing data on f/e

--- packages/frontend/pages/index.tsx
+++ packages/frontend/pages/index.tsx
@@ -4,6 +4,7 @@ import { useTempTestContract } from "~~/components/useTempTestContract";
 import { useAppStore } from "~~/services/store/store";
 import { Address, AddressInput, Balance } from "../components/scaffold-eth";
 import { useEffect } from "react";
+import { BigNumber } from "ethers";
 
 const Home: NextPage = () => {
   const tempTest = useTempTestContract();
@@ -23,12 +24,19 @@ const Home: NextPage = () => {
       <div>
         {" "}
         display tempstate as array
-        {tempState?.tempStuff?.map((setup, index) => (
-          <div key={index}>
-            <div>setup {index}</div>
-            <div>setupId: {setup?.setupId}</div>
-          </div>
-        ))}
+        {tempState?.tempStuff?.map(
+          (setup, index) => (
+            console.log("setup", setup),
+            (
+              <div key={index}>
+                <div>setup {index}</div>
+                <div>RewardsPerBlock: {setup.rewardPerBlock?.toString()}</div>
+                <div>Endblock: {setup.endBlock?.toNumber()}</div>
+                <div>Supply: {setup.totalSupply?.toString()}</div>
+              </div>
+            )
+          ),
+        )}
       </div>
     </div>
   );
