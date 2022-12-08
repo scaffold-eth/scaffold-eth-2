d2071a2fe1b5ae1210289338905692c506972487
Shiv Bhonde <shivbhonde04@gmail.com>
2022-12-08 16:52:14 +0530

    use gurad cluse instread wrpping whole thing inside if

--- packages/nextjs/hooks/scaffold-eth/useTransactor.tsx
+++ packages/nextjs/hooks/scaffold-eth/useTransactor.tsx
@@ -27,78 +27,80 @@ export const useTransactor = (_signer?: Signer, gasPrice?: number): TTransaction
   }
 
   const result: TTransactionFunc = async (tx, callback) => {
+    if (!signer) {
+      return;
+    }
+
     let toastId = null;
     let transactionReceipt: TransactionReceipt | undefined;
     let transactionResponse: SendTransactionResult | TransactionResponse | undefined;
-    if (signer) {
-      try {
-        const chainId = await signer.getChainId();
-        const provider = signer.provider;
-        const networkName = (await provider?.getNetwork())?.name;
+    try {
+      const chainId = await signer.getChainId();
+      const provider = signer.provider;
+      const networkName = (await provider?.getNetwork())?.name;
 
-        let etherscanNetwork = "";
-        if (networkName && chainId && chainId > 1) {
-          etherscanNetwork = networkName + ".";
-        }
-
-        let etherscanTxUrl = "https://" + etherscanNetwork + "etherscan.io/tx/";
-        if (chainId && chainId === 100) {
-          // eslint-disable-next-line @typescript-eslint/no-unused-vars
-          etherscanTxUrl = "https://blockscout.com/poa/xdai/tx/";
-        }
-
-        if (tx instanceof Promise) {
-          if (DEBUG) console.log("AWAITING TX", tx);
-
-          toastId = toast.loading("Awaiting for user confirmation");
-          const transactionRes = await tx;
-          toast.remove(toastId);
-
-          // TODO add etherscan url in loading toast
-          toastId = toast.loading("Mining transaction, Hold tight!");
-          transactionReceipt = await transactionRes.wait();
-          toast.remove(toastId);
-
-          customToast.tx(`${etherscanTxUrl}${transactionRes.hash}`);
-        } else if (tx != null) {
-          if (!tx.gasPrice) {
-            tx.gasPrice = gasPrice || ethers.utils.parseUnits("4.1", "gwei");
-          }
-          if (!tx.gasLimit) {
-            tx.gasLimit = BigNumber.from(ethers.utils.hexlify(120000));
-          }
-          if (DEBUG) console.log("RUNNING TX", tx);
-
-          toastId = toast.loading("Awaiting for user confirmation");
-          const transactionRes = await signer.sendTransaction(tx);
-          toast.remove(toastId);
-
-          // TODO add etherscan url in loading toast
-          toastId = toast.loading("Mining transaction, Hold tight!");
-          transactionReceipt = await transactionRes.wait();
-          toast.remove(toastId);
-
-          customToast.tx(`${etherscanTxUrl}${transactionRes.hash}`);
-        }
-
-        if (transactionReceipt) {
-          if (callback != null && transactionReceipt.blockHash != null && transactionReceipt.confirmations >= 1) {
-            callback({ ...transactionResponse, ...transactionReceipt });
-          }
-        }
-      } catch (error: any) {
-        if (toastId) {
-          toast.remove(toastId);
-        }
-        // TODO handle error properly
-        console.log("⚡️ ~ file: useTransactor.ts ~ line 98 ~ constresult:TTransactionFunc= ~ error", error);
-        const message = getParsedEthersError(error);
-        toast.error(message, {
-          style: {
-            wordBreak: "break-all",
-          },
-        });
+      let etherscanNetwork = "";
+      if (networkName && chainId && chainId > 1) {
+        etherscanNetwork = networkName + ".";
       }
+
+      let etherscanTxUrl = "https://" + etherscanNetwork + "etherscan.io/tx/";
+      if (chainId && chainId === 100) {
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        etherscanTxUrl = "https://blockscout.com/poa/xdai/tx/";
+      }
+
+      if (tx instanceof Promise) {
+        if (DEBUG) console.log("AWAITING TX", tx);
+
+        toastId = toast.loading("Awaiting for user confirmation");
+        const transactionRes = await tx;
+        toast.remove(toastId);
+
+        // TODO add etherscan url in loading toast
+        toastId = toast.loading("Mining transaction, Hold tight!");
+        transactionReceipt = await transactionRes.wait();
+        toast.remove(toastId);
+
+        customToast.tx(`${etherscanTxUrl}${transactionRes.hash}`);
+      } else if (tx != null) {
+        if (!tx.gasPrice) {
+          tx.gasPrice = gasPrice || ethers.utils.parseUnits("4.1", "gwei");
+        }
+        if (!tx.gasLimit) {
+          tx.gasLimit = BigNumber.from(ethers.utils.hexlify(120000));
+        }
+        if (DEBUG) console.log("RUNNING TX", tx);
+
+        toastId = toast.loading("Awaiting for user confirmation");
+        const transactionRes = await signer.sendTransaction(tx);
+        toast.remove(toastId);
+
+        // TODO add etherscan url in loading toast
+        toastId = toast.loading("Mining transaction, Hold tight!");
+        transactionReceipt = await transactionRes.wait();
+        toast.remove(toastId);
+
+        customToast.tx(`${etherscanTxUrl}${transactionRes.hash}`);
+      }
+
+      if (transactionReceipt) {
+        if (callback != null && transactionReceipt.blockHash != null && transactionReceipt.confirmations >= 1) {
+          callback({ ...transactionResponse, ...transactionReceipt });
+        }
+      }
+    } catch (error: any) {
+      if (toastId) {
+        toast.remove(toastId);
+      }
+      // TODO handle error properly
+      console.log("⚡️ ~ file: useTransactor.ts ~ line 98 ~ constresult:TTransactionFunc= ~ error", error);
+      const message = getParsedEthersError(error);
+      toast.error(message, {
+        style: {
+          wordBreak: "break-all",
+        },
+      });
     }
 
     return transactionResponse;
