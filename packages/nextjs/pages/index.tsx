ce20aa23c46d201d1f26a8581879ddee949e3d80
Carlos Sánchez <oceanrdn@gmail.com>
2022-12-12 19:08:52 +0100

    Main page styling / content: first iteration

--- packages/nextjs/pages/index.tsx
+++ packages/nextjs/pages/index.tsx
@@ -1,53 +1,55 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import { useAppStore } from "~~/services/store/store";
-import { Address, AddressInput, Balance } from "../components/scaffold-eth";
-import { useEffect } from "react";
+import { BugAntIcon, SparklesIcon } from "@heroicons/react/24/outline";
 import Link from "next/link";
+import React from "react";
 
 const Home: NextPage = () => {
-  const tempState = useAppStore(state => state.tempSlice.tempState);
-
-  useEffect(() => {
-    console.log("test state, in index.tsx:", tempState?.tempStuff);
-  }, [tempState?.tempStuff]);
-
   return (
-    <div className="px-8">
+    <div>
       <Head>
         <title>Scaffold-eth App</title>
         <meta name="description" content="Created with 🏗 scaffold-eth" />
       </Head>
 
-      <main className="flex items-center flex-col py-16">
-        <h1 className="text-center my-12 text-4xl">
-          Welcome to{" "}
-          <a
-            className="text-blue-600 hover:underline"
-            href="https://github.com/scaffold-eth/se-2"
-            target="_blank"
-            rel="noreferrer"
-          >
-            scaffold-eth 2
-          </a>
+      <main className="flex items-center flex-col pt-10">
+        <h1 className="text-center mb-8">
+          <span className="block text-2xl mb-2">Welcome to</span>
+          <span className="block text-4xl font-bold">scaffold-eth 2</span>
         </h1>
-
-        <p className="text-center text-xl">
-          Get started by editing <code className="italic bg-gray-200">packages/nextjs/pages/index.tsx</code>
+        <p className="text-center text-lg">
+          Get started by editing{" "}
+          <code className="italic bg-secondary text-base font-bold">packages/nextjs/pages/index.tsx</code>
+        </p>
+        <p className="text-center text-lg">
+          Edit your smart contract <code className="italic bg-secondary text-base font-bold">YourContract.sol</code> in{" "}
+          <code className="italic bg-secondary text-base font-bold">packages/hardhat/contracts</code>
         </p>
 
-        <Link href={"/debug"}>
-          <button className="mt-4 btn btn-primary">Go to contract component</button>
-        </Link>
-
-        <h3 className="font-bold mt-8">Address Component</h3>
-        <Address address="0xd8da6bf26964af9d7eed9e03e53415d37aa96045" />
-
-        <h3 className="font-bold mt-8">Balance Component</h3>
-        <Balance address="0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266" />
-
-        <h3 className="font-bold mt-8">Address Input Component</h3>
-        <AddressInput placeholder="Enter address" />
+        <div className="bg-secondary w-full mt-16 px-8 py-16">
+          <div className="flex justify-center gap-12">
+            <div className="flex flex-col bg-white px-16 py-10 text-center items-center max-w-sm">
+              <BugAntIcon className="h-10 w-10" />
+              <p>
+                Tinker with your smart contract using the{" "}
+                <Link href="/debug" passHref>
+                  <a className="underline">Debug Contract</a>
+                </Link>{" "}
+                tab.
+              </p>
+            </div>
+            <div className="flex flex-col bg-white px-16 py-10 text-center items-center max-w-sm">
+              <SparklesIcon className="h-10 w-10" />
+              <p>
+                Experiment with{" "}
+                <Link href="/example-ui" passHref>
+                  <a className="underline">Example UI</a>
+                </Link>{" "}
+                to build your own UI.
+              </p>
+            </div>
+          </div>
+        </div>
       </main>
     </div>
   );
