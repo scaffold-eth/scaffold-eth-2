ced0ab30bdda97a1f2329f130638c82d4fa1767d
Shiv Bhonde | shivbhonde.eth <shivbhonde04@gmail.com>
2022-12-12 15:25:27 +0530

    Basic Transactor (#55)
    Co-authored-by: Carlos Sánchez <oceanrdn@gmail.com>

---
+++ packages/nextjs/hooks/scaffold-eth/useTransactor.tsx
@@ -0,0 +1,91 @@
+import { TransactionRequest, TransactionResponse, TransactionReceipt } from "@ethersproject/abstract-provider";
+import { SendTransactionResult } from "@wagmi/core";
+import { BigNumber, ethers, Signer } from "ethers";
+import { Deferrable } from "ethers/lib/utils";
+import { useSigner } from "wagmi";
+import toast from "react-hot-toast";
+import { getParsedEthersError } from "~~/components/scaffold-eth/Contract/utilsContract";
+import { toast as customToast } from "~~/components/scaffold-eth/index";
+import { getBlockExplorerTxLink } from "~~/utils/scaffold-eth";
+
+type TTransactionFunc = (
+  tx: Promise<SendTransactionResult> | Deferrable<TransactionRequest> | undefined,
+  callback?: ((_param: any) => void) | undefined,
+) => Promise<Record<string, any> | undefined>;
+
+/**
+ * Runs TXs showing UI feedback.
+ * @param _signer
+ * @param gasPrice
+ * @dev If signer is provided => dev wants to send a raw tx.
+ */
+export const useTransactor = (_signer?: Signer, gasPrice?: number): TTransactionFunc | undefined => {
+  let signer = _signer;
+  const { data } = useSigner();
+  if (signer === undefined && data) {
+    signer = data;
+  }
+
+  const result: TTransactionFunc = async (tx, callback) => {
+    if (!signer) {
+      return;
+    }
+
+    let toastId = null;
+    let transactionReceipt: TransactionReceipt | undefined;
+    let transactionResponse: SendTransactionResult | TransactionResponse | undefined;
+    try {
+      const provider = signer.provider;
+      const network = await provider?.getNetwork();
+
+      toastId = customToast.txnLoading("Awaiting for user confirmation", "");
+      if (tx instanceof Promise) {
+        // Tx is already prepared by the caller
+        transactionResponse = await tx;
+      } else if (tx != null) {
+        // Raw tx
+        if (!tx.gasPrice) {
+          tx.gasPrice = gasPrice || ethers.utils.parseUnits("4.1", "gwei");
+        }
+        if (!tx.gasLimit) {
+          tx.gasLimit = BigNumber.from(ethers.utils.hexlify(120000));
+        }
+
+        transactionResponse = await signer.sendTransaction(tx);
+      } else {
+        throw new Error("Incorrect transaction passed to transactor");
+      }
+      toast.remove(toastId);
+
+      const blockExplorerTxURL = network ? getBlockExplorerTxLink(network, transactionResponse.hash) : "";
+
+      toastId = customToast.txnLoading("Mining transaction, Hold tight!", blockExplorerTxURL);
+      transactionReceipt = await transactionResponse.wait();
+      toast.remove(toastId);
+
+      customToast.txnSuccess("Mined successfully !", blockExplorerTxURL);
+
+      if (transactionReceipt) {
+        if (callback != null && transactionReceipt.blockHash != null && transactionReceipt.confirmations >= 1) {
+          callback({ ...transactionResponse, ...transactionReceipt });
+        }
+      }
+    } catch (error: any) {
+      if (toastId) {
+        toast.remove(toastId);
+      }
+      // TODO handle error properly
+      console.error("⚡️ ~ file: useTransactor.ts ~ line 98 ~ constresult:TTransactionFunc= ~ error", error);
+      const message = getParsedEthersError(error);
+      toast.error(message, {
+        style: {
+          wordBreak: "break-all",
+        },
+      });
+    }
+
+    return transactionResponse;
+  };
+
+  return result;
+};
