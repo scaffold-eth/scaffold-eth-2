db2c0b119b22e12ddb21e896674812d93f5d498e
Carlos Sánchez <oceanrdn@gmail.com>
2022-12-12 13:46:21 +0100

    Initial Daisy theme set up + Header styling (#60)

--- packages/nextjs/tailwind.config.js
+++ packages/nextjs/tailwind.config.js
@@ -1,8 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {},
-  },
   plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        scaffoldEth: {
+          primary: "#93BBFB",
+          "primary-content": "#000000",
+          "primary-focus": "#7eaaef",
+          secondary: "#D0E2FF",
+          "secondary-content": "#000000",
+          accent: "#37cdbe",
+          "accent-content": "#163835",
+          neutral: "#3d4451",
+          "neutral-content": "#ffffff",
+          "base-100": "#ffffff",
+          "base-200": "#F2F2F2",
+          "base-300": "#E7EFFF",
+          "base-content": "#1f2937",
+
+          "--rounded-btn": "0",
+          "--navbar-padding": "0",
+        },
+      },
+    ],
+  },
 };
