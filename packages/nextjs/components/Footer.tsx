661e40e1b5381e37c844029256a8596221334fe7
Eda Akturk <edaakturk@192.168.1.100>
2022-12-16 19:23:31 +0300

    Update footer

--- packages/nextjs/components/Footer.tsx
+++ packages/nextjs/components/Footer.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { CurrencyDollarIcon } from "@heroicons/react/24/outline";
 import { useAppStore } from "~~/services/store/store";
+import { HeartIcon } from "@heroicons/react/24/outline";
 
 /**
  * Site footer
@@ -9,8 +10,8 @@ export default function Footer() {
   const ethPrice = useAppStore(state => state.ethPriceSlice.ethPrice);
 
   return (
-    <div className="navbar bg-secondary min-h-0 p-4">
-      <div className="navbar-start">
+    <div className="bg-secondary min-h-0 p-4 flex justify-between items-center">
+      <div>
         {ethPrice > 0 && (
           <div className="btn btn-primary btn-sm font-normal cursor-auto">
             <CurrencyDollarIcon className="text-black h-4 w-4 mr-0.5" />
@@ -18,29 +19,46 @@ export default function Footer() {
           </div>
         )}
       </div>
-      <div className="navbar-middle">
+      <div>
         <ul className="menu menu-horizontal px-1">
-          <div className="flex items-center gap-4">
+          <div className="flex items-center gap-2 text-sm">
             <div>
-              <a href="https://github.com/scaffold-eth/se-2" target="_blank" rel="noreferrer">
+              <a
+                href="https://github.com/scaffold-eth/se-2"
+                target="_blank"
+                rel="noreferrer"
+                className="underline underline-offset-2"
+              >
                 Fork Me
               </a>
             </div>
-            <div>
-              Built with 💖 at 🏰{" "}
-              <a href="https://buidlguidl.com/" target="_blank" rel="noreferrer">
+            <span>·</span>
+            <div className="flex">
+              Built with <HeartIcon className="h-4 w-4" /> at 🏰&nbsp;
+              <a
+                href="https://buidlguidl.com/"
+                target="_blank"
+                rel="noreferrer"
+                className="underline underline-offset-2"
+              >
                 BuidlGuidl
               </a>
             </div>
+            <span>·</span>
             <div>
-              <a href="https://t.me/joinchat/KByvmRe5wkR-8F_zz6AjpA" target="_blank" rel="noreferrer">
+              <a
+                href="https://t.me/joinchat/KByvmRe5wkR-8F_zz6AjpA"
+                target="_blank"
+                rel="noreferrer"
+                className="underline underline-offset-2"
+              >
                 Support
               </a>
             </div>
           </div>
         </ul>
       </div>
-      <div className="navbar-end mr-4">
+      <div className="mr-4 text-sm">
         <div>Color Switch Placeholder</div>
       </div>
     </div>
