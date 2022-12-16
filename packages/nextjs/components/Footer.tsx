2ef8c54301e7d2a335a83a665859ef43a641caab
Eda Akturk <edaakturk@192.168.1.100>
2022-12-16 16:42:54 +0300

    Update footer

--- packages/nextjs/components/Footer.tsx
+++ packages/nextjs/components/Footer.tsx
@@ -9,13 +9,40 @@ export default function Footer() {
   const ethPrice = useAppStore(state => state.ethPriceSlice.ethPrice);
 
   return (
-    <div className="bg-secondary p-4 flex-shrink-0">
-      {ethPrice > 0 && (
-        <div className="btn btn-primary btn-sm font-normal cursor-auto">
-          <CurrencyDollarIcon className="text-black h-4 w-4 mr-0.5" />
-          <span>{ethPrice}</span>
-        </div>
-      )}
+    <div className="navbar bg-secondary min-h-0 p-4">
+      <div className="navbar-start">
+        {ethPrice > 0 && (
+          <div className="btn btn-primary btn-sm font-normal cursor-auto">
+            <CurrencyDollarIcon className="text-black h-4 w-4 mr-0.5" />
+            <span>{ethPrice}</span>
+          </div>
+        )}
+      </div>
+      <div className="navbar-middle">
+        <ul className="menu menu-horizontal px-1">
+          <div className="flex items-center gap-4">
+            <div>
+              <a href="https://github.com/scaffold-eth/se-2" target="_blank" rel="noreferrer">
+                Fork Me
+              </a>
+            </div>
+            <div>
+              Built with 💖 at 🏰{" "}
+              <a href="https://buidlguidl.com/" target="_blank" rel="noreferrer">
+                BuidlGuidl
+              </a>
+            </div>
+            <div>
+              <a href="https://google.com/" target="_blank" rel="noreferrer">
+                Support
+              </a>
+            </div>
+          </div>
+        </ul>
+      </div>
+      <div className="navbar-end mr-4">
+        <div>Color Switch Placeholder</div>
+      </div>
     </div>
   );
 }
