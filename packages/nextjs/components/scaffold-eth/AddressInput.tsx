02f6e2c0574484719c07b8f7e27a9b89a95d71a3
Carlos Sánchez <oceanrdn@gmail.com>
2023-01-02 18:52:48 +0100

    Show resolved ENS on Address input

--- packages/nextjs/components/scaffold-eth/AddressInput.tsx
+++ packages/nextjs/components/scaffold-eth/AddressInput.tsx
@@ -17,6 +17,7 @@ const isENS = (address = "") => address.endsWith(".eth") || address.endsWith(".x
  */
 const AddressInput = ({ value, name, placeholder, onChange }: TAddressInputProps) => {
   const [address, setAddress] = useState("");
+  const [resolvedEns, setResolvedEns] = useState("");
 
   const isControlledInput = value !== undefined;
 
@@ -28,6 +29,7 @@ const AddressInput = ({ value, name, placeholder, onChange }: TAddressInputProps
   });
 
   const onChangeAddress = async (event: ChangeEvent<HTMLInputElement>) => {
+    setResolvedEns("");
     setAddress(event.target.value);
     if (onChange) {
       onChange(event.target.value);
@@ -38,16 +40,20 @@ const AddressInput = ({ value, name, placeholder, onChange }: TAddressInputProps
     if (!ensData) return;
 
     // ENS resolved successfully
+    setResolvedEns(address);
     setAddress(ensData);
     if (onChange) {
       onChange(ensData);
     }
+    // We don't want to run it on address change
+    // eslint-disable-next-line
   }, [ensData, onChange]);
 
   return (
     <>
       <div className="form-control grow">
         <label className="input-group">
+          {resolvedEns && <span className="text-accent">{resolvedEns}</span>}
           <input
             name={name}
             type="text"
