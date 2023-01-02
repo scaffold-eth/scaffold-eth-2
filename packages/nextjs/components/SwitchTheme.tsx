2adc1d74a9bbcada612436b93a7cda832e19db24
Shiv Bhonde <shivbhonde04@gmail.com>
2023-01-02 14:44:20 +0530

    add dynamic icons to represent dark and light mode

--- packages/nextjs/components/SwitchTheme.tsx
+++ packages/nextjs/components/SwitchTheme.tsx
@@ -1,9 +1,10 @@
 //SwitchTheme.tsx
 import React, { useEffect } from "react";
-import { useDarkMode } from "usehooks-ts";
-import { SunIcon } from "@heroicons/react/24/outline";
+import { useDarkMode, useIsMounted } from "usehooks-ts";
+import { MoonIcon, SunIcon } from "@heroicons/react/24/outline";
 const SwitchTheme = () => {
   const { isDarkMode, toggle } = useDarkMode(false);
+  const isMounted = useIsMounted();
 
   useEffect(() => {
     const body = document.body;
@@ -12,8 +13,19 @@ const SwitchTheme = () => {
 
   return (
     <div className="flex space-x-2">
-      <input type="checkbox" className="toggle toggle-primary bg-primary" onChange={toggle} checked={isDarkMode} />
-      <SunIcon className="swap-off h-6 w-6" />
+      <input
+        id="theme-toggle"
+        type="checkbox"
+        className="toggle toggle-primary bg-primary"
+        onChange={toggle}
+        checked={isDarkMode}
+      />
+      {isMounted() && (
+        <label htmlFor="theme-toggle" className={`swap swap-rotate ${!isDarkMode ? "swap-active" : ""}`}>
+          <SunIcon className="swap-on h-5 w-5" />
+          <MoonIcon className="swap-off h-5 w-5" />
+        </label>
+      )}
     </div>
   );
 };
