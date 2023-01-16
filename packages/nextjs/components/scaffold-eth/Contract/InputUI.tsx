9a2d945c7306960e39ee45a82e86f867f07986ea
Rinat Akhunianov <rinat@hey.com>
2023-01-16 23:21:27 +0400

    fix: display converters

--- packages/nextjs/components/scaffold-eth/Contract/InputUI.tsx
+++ packages/nextjs/components/scaffold-eth/Contract/InputUI.tsx
@@ -2,7 +2,7 @@ import { FunctionFragment } from "ethers/lib/utils";
 import React, { Dispatch, ReactElement, SetStateAction } from "react";
 import { AddressInput } from "~~/components/scaffold-eth";
 
-import { ConvertStringToBytes, ConvertStringToBytes32, ConvertUintToEther } from "./utilsDisplay";
+import { StringToBytesConverter, StringToBytes32Converter, UintToEtherConverter } from "./utilsDisplay";
 
 type ParamType = {
   name: string | null;
@@ -25,15 +25,15 @@ const InputUI = ({ setForm, form, stateObjectKey, paramType }: TInputUIProps) =>
 
   switch (paramType.type) {
     case "bytes32":
-      inputSuffix = <ConvertStringToBytes32 setForm={setForm} form={form} stateObjectKey={stateObjectKey} />;
+      inputSuffix = <StringToBytes32Converter setForm={setForm} form={form} stateObjectKey={stateObjectKey} />;
       break;
 
     case "bytes":
-      inputSuffix = <ConvertStringToBytes setForm={setForm} form={form} stateObjectKey={stateObjectKey} />;
+      inputSuffix = <StringToBytesConverter setForm={setForm} form={form} stateObjectKey={stateObjectKey} />;
       break;
 
     case "uint256":
-      inputSuffix = <ConvertUintToEther setForm={setForm} form={form} stateObjectKey={stateObjectKey} />;
+      inputSuffix = <UintToEtherConverter setForm={setForm} form={form} stateObjectKey={stateObjectKey} />;
       break;
   }
 
