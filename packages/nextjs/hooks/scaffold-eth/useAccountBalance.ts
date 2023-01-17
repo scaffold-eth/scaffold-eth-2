3e7fada007b5f021cac3955d45e2beb94cbc964c
Rinat <rinat@hey.com>
2023-01-17 22:38:08 +0400

    Grab funds tooltip (#94)
    Co-authored-by: Carlos Sánchez <oceanrdn@gmail.com>

---
+++ packages/nextjs/hooks/scaffold-eth/useAccountBalance.ts
@@ -0,0 +1,30 @@
+import { useCallback, useEffect, useState } from "react";
+import { useBalance } from "wagmi";
+import { useAppStore } from "~~/services/store/store";
+
+export function useAccountBalance(address?: string) {
+  const [isEthBalance, setIsEthBalance] = useState(true);
+  const [balance, setBalance] = useState<number | null>(null);
+  const price = useAppStore(state => state.ethPriceSlice.ethPrice);
+
+  const {
+    data: fetchedBalanceData,
+    isError,
+    isLoading,
+  } = useBalance({
+    addressOrName: address,
+    watch: true,
+  });
+
+  const onToggleBalance = useCallback(() => {
+    setIsEthBalance(!isEthBalance);
+  }, [isEthBalance]);
+
+  useEffect(() => {
+    if (fetchedBalanceData?.formatted) {
+      setBalance(Number(fetchedBalanceData.formatted));
+    }
+  }, [fetchedBalanceData]);
+
+  return { balance, price, isError, isLoading, onToggleBalance, isEthBalance };
+}
