9821201bfe437a7991042ef90690afe4465f874e
Shiv Bhonde <shivbhonde04@gmail.com>
2023-01-18 15:51:55 +0900

    return undefined if the string is empty instread of always returning -1 in parseTxnValue

--- packages/nextjs/utils/scaffold-eth/parseTxnValue.ts
+++ packages/nextjs/utils/scaffold-eth/parseTxnValue.ts
@@ -3,11 +3,12 @@ import { BigNumber } from "ethers";
 /**
  * @dev utility to parse string to wagmi's PayableOverrides.value.Returns BigNumber if its convertible or returns -1 if there is error or invalid string
  * @param  str - any string
- * @return {BigNumber | number}
+ * @return {BigNumber | number | undefined}
  */
-export default function parseTxnValue(str: string): BigNumber | number {
+export default function parseTxnValue(str: string): BigNumber | number | undefined {
   try {
     if (isNaN(parseFloat(str))) {
+      if (str === "") return undefined;
       return -1;
     }
     return BigNumber.from(str);
