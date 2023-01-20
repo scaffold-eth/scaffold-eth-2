f4c9f0a0eff5eb4571a3f1a04ca95bd2b158ea89
Eda Akturk <edaakturk@192.168.1.108>
2023-01-20 09:46:24 +0300

    Update comments in YourContract.sol

--- packages/hardhat/contracts/YourContract.sol
+++ packages/hardhat/contracts/YourContract.sol
@@ -4,7 +4,7 @@ pragma solidity >=0.8.0 <0.9.0;
 import "hardhat/console.sol";
 
 /**
- * A smart contract that allows setting a purpose of the contract and tracking the changes
+ * A smart contract that allows changing a state variable of the contract and tracking the changes
  * It also allows the owner to withdraw the Ether in the contract
  * @author BuidlGuidl
  */
@@ -17,8 +17,8 @@ contract YourContract {
     uint256 public totalCounter = 0;
     mapping(address => uint) public userPurposeCounter;
 
-    // Events
-    event PurposeChange(address purposeSetter, string newPurpose, bool premium, uint256 value); // Triggered when the purpose of the contract is changed
+    // Events: a way to emit log statements from smart contract that can be listened to by external parties
+    event PurposeChange(address purposeSetter, string newPurpose, bool premium, uint256 value);
 
     // Constructor: Called once on contract deployment
     // Check packages/hardhat/deploy/00_deploy_your_contract.ts
@@ -26,15 +26,16 @@ contract YourContract {
         owner = _owner;
     }
 
-    // Modifier: Can be applied to functions.
+    // Modifier: used to define a set of rules that must be met before or after a function is executed
     // Check the withdraw() function
     modifier isOwner() {
+        // msg.sender: predefined variable that represents address of the account that called the current function
         require(msg.sender == owner, "Not the Owner");
         _;
     }
 
     /**
-     * Function that allows anyone to change the purpose of the contract and increase the counters
+     * Function that allows anyone to change the state variable:purpose of the contract and increase the counters
      *
      * @param _newPurpose (string memory) - new purpose of the contract
      */
@@ -43,12 +44,14 @@ contract YourContract {
         totalCounter += 1;
         userPurposeCounter[msg.sender] += 1;
 
+        // msg.value: predefined variable that represents the amount of ether sent with the transaction
         if (msg.value > 0) {
             premium = true;
         } else {
             premium = false;
         }
 
+        // emit: keyword used to trigger an event
         emit PurposeChange(msg.sender, _newPurpose, msg.value > 0, 0);
     }
 
