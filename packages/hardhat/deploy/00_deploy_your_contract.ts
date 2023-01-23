4e15559a06c768c88b2d8c6bc1b0b11b9020a4d5
Eda Akturk <edaakturk@192.168.1.108>
2023-01-23 12:19:12 +0300

    Add comments to 00_deploy_your_contracts.ts

--- packages/hardhat/deploy/00_deploy_your_contract.ts
+++ packages/hardhat/deploy/00_deploy_your_contract.ts
@@ -1,6 +1,13 @@
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 import { DeployFunction } from "hardhat-deploy/types";
 
+/**
+ * Deploys a contract named "YourContract" using the deployer account and
+ * constructor arguments set to the deployer address.
+ *
+ * @param hre HardhatRuntimeEnvironment object.
+ * @author BuidlGuidl
+ */
 const deployYourContract: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployer } = await hre.getNamedAccounts();
   const { deploy } = hre.deployments;
@@ -11,7 +18,8 @@ const deployYourContract: DeployFunction = async function (hre: HardhatRuntimeEn
     // Use your frontend address
     args: [deployer],
     log: true,
-    // Speed up deployment on local network, no effect on live networks
+    // autoMine: can be passed to the deploy function to make the deployment process faster on local networks by
+    // automatically mining the contract deployment transaction. There is no effect on live networks
     autoMine: true,
   });
 
