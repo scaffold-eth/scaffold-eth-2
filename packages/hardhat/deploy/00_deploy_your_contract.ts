13ce1f56cbeef18fc479464b5a1fcf69b9fd8d7f
Eda Akturk <edaakturk@192.168.1.108>
2023-01-24 11:09:48 +0300

    Code commenst to hardhat files

--- packages/hardhat/deploy/00_deploy_your_contract.ts
+++ packages/hardhat/deploy/00_deploy_your_contract.ts
@@ -3,7 +3,7 @@ import { DeployFunction } from "hardhat-deploy/types";
 
 /**
  * Deploys a contract named "YourContract" using the deployer account and
- * constructor arguments set to the deployer address.
+ * constructor arguments set to the deployer address
  *
  * @param hre HardhatRuntimeEnvironment object.
  */
@@ -13,21 +13,22 @@ const deployYourContract: DeployFunction = async function (hre: HardhatRuntimeEn
 
   await deploy("YourContract", {
     from: deployer,
-    // Constructor args.
-    // Use your frontend address
+    // By default the deployer account will be one that comes with Hardhat, which anyone can access.
+    // You can use your frontend address as the deployer account to run the deploy script.
+    // The deployer account should have sufficient balance to pay for the gas fee for contract creation.
     args: [deployer],
     log: true,
     // autoMine: can be passed to the deploy function to make the deployment process faster on local networks by
-    // automatically mining the contract deployment transaction. There is no effect on live networks
+    // automatically mining the contract deployment transaction. There is no effect on live networks.
     autoMine: true,
   });
 
-  // Get the deployed contract.
+  // Get the deployed contract
   // const yourContract = await hre.ethers.getContract("YourContract", deployer);
 };
 
 export default deployYourContract;
 
-// Tags are useful if you have multiple deploy files and only want to run one of them
+// Tags are useful if you have multiple deploy files and only want to run one of them.
 // e.g. yarn deploy --tags YourContract
 deployYourContract.tags = ["YourContract"];
