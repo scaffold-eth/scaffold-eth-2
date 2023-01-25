0a0e780d2d17b565094ef5e5191fc0f21ee5a8b7
Shiv Bhonde <shivbhonde04@gmail.com>
2023-01-25 12:08:46 +0900

    remove @ts-expect-error, global.d.ts and types dir

--- packages/nextjs/global.d.ts
+++
@@ -1,8 +0,0 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import { Config } from "abitype";
-
-declare module "abitype" {
-  export interface Config {
-    AddressType: string;
-  }
-}
