cb12260b470e3e224b4bb9847feb5659058b1cb5
Rinat Akhunianov <rinat@hey.com>
2023-01-27 20:24:55 +0400

    feat: contract data bottom

--- packages/nextjs/components/ExampleUi/ContractData/index.tsx
+++ packages/nextjs/components/ExampleUi/ContractData/index.tsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 
 export const ContractData = () => {
   const [currentPurpose] = useState("This is your purpose");
+  const [isRightDirection, setIsRightDirection] = useState(true);
+
   return (
     <div className="flex justify-center items-center bg-gradient bg-[length:100%_100%] px-32">
       <div className="flex flex-col bg-base-200 opacity-80 rounded-2xl shadow-lg px-5 py-4 w-full">
@@ -12,10 +14,29 @@ export const ContractData = () => {
             <div className="text-4xl text-right min-w-[3rem] px-2 py-1 flex justify-end">3</div>
           </div>
         </div>
+
         <div className="mt-3 border border-black bg-white rounded-3xl py-8 px-4 text-secondary text-8xl overflow-hidden whitespace-nowrap w-full">
           <div key="purpose-1">{currentPurpose}</div>
           <div key="purpose-2">{currentPurpose}</div>
           <div key="purpose-3">{currentPurpose}</div>
+        </div>
+
+        <div className="mt-3 flex items-end justify-between">
+          <button
+            className={`btn btn-circle btn-ghost border border-black hover:border-black w-12 h-12 p-1 bg-white flex items-center ${
+              isRightDirection ? "justify-end" : "justify-start"
+            }`}
+            onClick={() => setIsRightDirection(!isRightDirection)}
+          >
+            <div className="border border-black rounded-full bg-secondary w-2 h-2" />
+          </button>
+          <div className="h-4 px-0.5 flex items-center bg-white border border-black rounded-full">
+            <progress
+              className="progress progress-secondary w-40 border border-black rounded-full"
+              value="10"
+              max="100"
+            />
+          </div>
         </div>
       </div>
     </div>
