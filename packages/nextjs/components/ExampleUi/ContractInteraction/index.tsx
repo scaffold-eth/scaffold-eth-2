73f6e94693a4d50154b85c005f7130097b65662c
Rinat Akhunianov <rinat@hey.com>
2023-01-27 16:38:43 +0400

    feat: interaction bg icons

--- packages/nextjs/components/ExampleUi/ContractInteraction/index.tsx
+++ packages/nextjs/components/ExampleUi/ContractInteraction/index.tsx
@@ -1,3 +1,13 @@
+import { CopyIcon } from "./CopyIcon";
+import { DiamondIcon } from "./DiamondIcon";
+import { HareIcon } from "./HareIcon";
+
 export const ContractInteraction = () => {
-  return <div className="flex bg-base-300">Left block</div>;
+  return (
+    <div className="flex bg-base-300 relative">
+      <DiamondIcon className="absolute top-24" />
+      <CopyIcon className="absolute bottom-0 left-36" />
+      <HareIcon className="absolute right-0 bottom-24" />
+    </div>
+  );
 };
