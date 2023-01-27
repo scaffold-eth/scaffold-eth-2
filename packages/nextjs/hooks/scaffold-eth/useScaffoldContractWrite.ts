9552b168b91e9bb77a4d52a8e813c308d04fd074
Shiv Bhonde <shivbhonde04@gmail.com>
2023-01-27 20:46:59 +0900

    make args optional for contractWrite and set basic input for setting purpose

--- packages/nextjs/hooks/scaffold-eth/useScaffoldContractWrite.ts
+++ packages/nextjs/hooks/scaffold-eth/useScaffoldContractWrite.ts
@@ -2,7 +2,7 @@ import { useContractWrite, useNetwork, usePrepareContractWrite } from "wagmi";
 import { getDeployedContract } from "~~/components/scaffold-eth/Contract/utilsContract";
 import { parseTxnValue } from "~~/utils/scaffold-eth";
 
-const useScaffoldContractWrite = (contractName: string, functionName: string, args: any[], value?: string) => {
+const useScaffoldContractWrite = (contractName: string, functionName: string, args?: any[], value?: string) => {
   const { chain } = useNetwork();
   const deployedContractData = getDeployedContract(chain?.id.toString(), contractName);
   const { config } = usePrepareContractWrite({
