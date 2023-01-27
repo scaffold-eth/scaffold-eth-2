8e4e9b1edfb917a1019d721564fc8ec0259f5d23
Simon Emanuel Schmid <simon@schmid.io>
2023-01-27 19:30:43 +0100

    Ugly UI but works

--- packages/nextjs/pages/example-ui.tsx
+++ packages/nextjs/pages/example-ui.tsx
@@ -12,7 +12,21 @@ const ExampleUI: NextPage = () => {
 
   return (
     <div className="flex flex-col items-center justify-center">
-      <h1 className="text-2xl my-5">Example UI</h1>
+      <h1 className="text-2xl my-5">Last posts from schmidsi.lens</h1>
+      {!fetching && (
+        <>
+          asdf {data?.publications.items.length}
+          {data?.publications.items.map((item: any) => (
+            <div key={item["id"]} style={{ border: "1px solid black", marginBottom: 20 }}>
+              <h2>{item.metadata.name}</h2>
+              <div>
+                {item.metadata.description}
+                {item.metadata.content}
+              </div>
+            </div>
+          ))}
+        </>
+      )}
     </div>
   );
 };
