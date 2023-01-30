688e2e2057c4c7c630f83205fd0a811d48389f72
Rinat Akhunianov <rinat@hey.com>
2023-01-30 18:14:11 +0400

    fix: description bg

--- packages/nextjs/components/ExampleUi/ContractInteraction/UiDescription.tsx
+++ packages/nextjs/components/ExampleUi/ContractInteraction/UiDescription.tsx
@@ -5,7 +5,7 @@ export const UiDescription = () => {
   const [visible, setVisible] = useState(true);
   return (
     <div className={`mt-7 flex gap-2 ${visible ? "" : "invisible"} max-w-2xl`}>
-      <div className="flex gap-5 bg-base-200 opacity-80 p-7 rounded-2xl shadow-lg">
+      <div className="flex gap-5 bg-base-200 bg-opacity-80 z-0 p-7 rounded-2xl shadow-lg">
         <span className="text-3xl">👋🏻</span>
         <div>
           <span className="block">
@@ -20,7 +20,7 @@ export const UiDescription = () => {
         </div>
       </div>
       <button
-        className="btn btn-circle btn-ghost h-6 w-6 bg-base-200 opacity-80 min-h-0 drop-shadow-md"
+        className="btn btn-circle btn-ghost h-6 w-6 bg-base-200 bg-opacity-80 z-0 min-h-0 drop-shadow-md"
         onClick={() => setVisible(false)}
       >
         <XMarkIcon className="h-4 w-4" />
