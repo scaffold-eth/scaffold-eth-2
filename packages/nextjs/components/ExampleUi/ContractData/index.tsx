92416cad63c8b9d7c3d16e7ba827474a5633e2c4
Rinat Akhunianov <rinat@hey.com>
2023-01-30 21:35:24 +0400

    fix: enabling/disabling

--- packages/nextjs/components/ExampleUi/ContractData/index.tsx
+++ packages/nextjs/components/ExampleUi/ContractData/index.tsx
@@ -25,11 +25,17 @@ export const ContractData = () => {
       <div className="flex flex-col bg-base-200 bg-opacity-70 rounded-2xl shadow-lg px-5 py-4 w-full">
         <div className="flex justify-between w-full">
           <button
-            className="btn btn-circle btn-ghost bg-[url('/assets/switcher.png')] bg-center"
+            className="btn btn-circle btn-ghost relative bg-center bg-[url('/assets/switch-button-on.png')] bg-no-repeat"
             onClick={() => {
               setTransitionEnabled(!transitionEnabled);
             }}
-          />
+          >
+            <div
+              className={`absolute inset-0 bg-center bg-no-repeat bg-[url('/assets/switch-button-off.png')] transition-opacity ${
+                transitionEnabled ? "opacity-0" : "opacity-100"
+              }`}
+            />
+          </button>
           <div className="bg-secondary border border-black rounded-xl flex">
             <div className="p-2 py-1 border-r border-black flex items-end">Total count</div>
             <div className="text-4xl text-right min-w-[3rem] px-2 py-1 flex justify-end">3</div>
@@ -42,13 +48,31 @@ export const ContractData = () => {
             <div className="absolute -left-[9999rem]" ref={purposeRef}>
               {currentPurpose}
             </div>
-            <Marquee key="1" direction={isRightDirection ? "right" : "left"} gradient={false} speed={marqueeSpeed}>
+            <Marquee
+              key="1"
+              direction={isRightDirection ? "right" : "left"}
+              gradient={false}
+              play={transitionEnabled}
+              speed={marqueeSpeed}
+            >
               {currentPurpose}
             </Marquee>
-            <Marquee key="2" direction={isRightDirection ? "left" : "right"} gradient={false} speed={marqueeSpeed}>
+            <Marquee
+              key="2"
+              direction={isRightDirection ? "left" : "right"}
+              gradient={false}
+              play={transitionEnabled}
+              speed={marqueeSpeed}
+            >
               {currentPurpose}
             </Marquee>
-            <Marquee key="3" direction={isRightDirection ? "right" : "left"} gradient={false} speed={marqueeSpeed}>
+            <Marquee
+              key="3"
+              direction={isRightDirection ? "right" : "left"}
+              gradient={false}
+              play={transitionEnabled}
+              speed={marqueeSpeed}
+            >
               {currentPurpose}
             </Marquee>
           </div>
@@ -69,10 +93,13 @@ export const ContractData = () => {
           </button>
           <div
             className={`w-44 p-0.5 flex items-center bg-neutral border border-black rounded-full ${
-              isRightDirection ? "rotate-180" : ""
+              transitionEnabled && isRightDirection ? "rotate-180" : ""
             }`}
           >
-            <div className="h-1.5 border border-black rounded-full bg-secondary animate-grow" />
+            <div
+              className="h-1.5 border border-black rounded-full bg-secondary animate-grow"
+              style={{ animationPlayState: transitionEnabled ? "running" : "paused" }}
+            />
           </div>
         </div>
       </div>
