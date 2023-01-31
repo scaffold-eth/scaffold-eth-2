a23bec7eb0c6b2a6d8219b86a5cfb28f5284465b
Rinat Akhunianov <rinat@hey.com>
2023-01-31 20:10:36 +0400

    fix: styles adjustments

--- packages/nextjs/components/ExampleUi/ContractInteraction/index.tsx
+++ packages/nextjs/components/ExampleUi/ContractInteraction/index.tsx
@@ -10,7 +10,7 @@ export const ContractInteraction = () => {
       <DiamondIcon className="absolute top-24" />
       <CopyIcon className="absolute bottom-0 left-36" />
       <HareIcon className="absolute right-0 bottom-24" />
-      <div className="flex flex-col w-full mx-8 2xl:mx-20">
+      <div className="flex flex-col w-full mx-5 sm:mx-8 2xl:mx-20">
         <UiDescription />
         <PurposeSetter />
       </div>
