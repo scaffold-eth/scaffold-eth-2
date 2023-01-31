7671db94a1f41e054662b74af9e2b03dcdc8a3b2
Rinat Akhunianov <rinat@hey.com>
2023-01-31 13:38:14 +0400

    fix: google fonts

--- packages/nextjs/pages/example-ui.tsx
+++ packages/nextjs/pages/example-ui.tsx
@@ -1,13 +1,22 @@
 import type { NextPage } from "next";
+import Head from "next/head";
 import { ContractData } from "~~/components/ExampleUi/ContractData";
 import { ContractInteraction } from "~~/components/ExampleUi/ContractInteraction";
 
 const ExampleUI: NextPage = () => {
   return (
-    <div className="grid grid-cols-2 flex-grow" data-theme="exampleUi">
-      <ContractInteraction />
-      <ContractData />
-    </div>
+    <>
+      <Head>
+        <title>Scaffold-eth Example Ui</title>
+        <meta name="description" content="Created with 🏗 scaffold-eth" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Bai+Jamjuree&display=swap" rel="stylesheet" />
+      </Head>
+      <div className="grid grid-cols-2 flex-grow" data-theme="exampleUi">
+        <ContractInteraction />
+        <ContractData />
+      </div>
+    </>
   );
 };
 
