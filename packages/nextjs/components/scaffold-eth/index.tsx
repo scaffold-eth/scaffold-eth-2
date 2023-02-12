621704a153797f844b21d139d942a7672e86b411
Carlos Sánchez <oceanrdn@gmail.com>
2023-02-12 02:06:13 +0100

    EtherInput component (#153)

--- packages/nextjs/components/scaffold-eth/index.tsx
+++ packages/nextjs/components/scaffold-eth/index.tsx
@@ -2,5 +2,6 @@ export { default as Faucet } from "./Faucet";
 export { default as Address } from "./Address";
 export { default as Balance } from "./Balance";
 export { default as AddressInput } from "./AddressInput";
+export { default as EtherInput } from "./EtherInput";
 export { default as BlockieAvatar } from "./BlockieAvatar";
 export { default as ContractUI } from "./Contract/ContractUI";
