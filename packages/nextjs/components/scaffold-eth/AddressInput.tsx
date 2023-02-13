d70f89fb56266b5b9a552aa3799b34be51d0084e
Rinat Akhunianov <rinat@hey.com>
2023-02-13 23:07:54 +0400

    feat: address -> ens + avatar

--- packages/nextjs/components/scaffold-eth/AddressInput.tsx
+++ packages/nextjs/components/scaffold-eth/AddressInput.tsx
@@ -1,6 +1,6 @@
-import { ChangeEvent, useEffect, useState } from "react";
+import { ChangeEvent, useState } from "react";
 import Blockies from "react-blockies";
-import { useEnsAddress, useEnsAvatar } from "wagmi";
+import { useEnsAddress, useEnsAvatar, useEnsName } from "wagmi";
 import { isAddress } from "ethers/lib/utils";
 
 type TAddressInputProps = {
@@ -18,48 +18,40 @@ const isENS = (address = "") => address.endsWith(".eth") || address.endsWith(".x
  */
 const AddressInput = ({ value, name, placeholder, onChange }: TAddressInputProps) => {
   const [address, setAddress] = useState("");
-  const [resolvedEns, setResolvedEns] = useState("");
-
   // Controlled vs Uncontrolled input.
   const currentValue = value !== undefined ? value : address || "";
 
-  const { data: ensData, isLoading } = useEnsAddress({
-    name: address,
-    enabled: isENS(address),
+  const isEns = isENS(currentValue);
+
+  const { data: ensAddress, isLoading: isEnsAddressLoading } = useEnsAddress({
+    name: currentValue,
+    enabled: isEns,
     chainId: 1,
     cacheTime: 30_000,
   });
 
-  const { data: ensAvatarData } = useEnsAvatar({
-    address,
-    enabled: isAddress(address),
+  const { data: ensName, isLoading: isEnsNameLoading } = useEnsName({
+    address: currentValue,
+    enabled: isAddress(address) && !isEns,
     chainId: 1,
     cacheTime: 30_000,
   });
 
-  useEffect(() => {
-    // Reset when clearing controlled input
-    if (!currentValue) {
-      setResolvedEns("");
-      setAddress("");
-    }
-  }, [currentValue]);
+  const { data: ensAvatar } = useEnsAvatar({
+    address: ensAddress || currentValue,
+    enabled: !!ensAddress || isAddress(currentValue),
+    chainId: 1,
+    cacheTime: 30_000,
+  });
 
-  useEffect(() => {
-    if (!ensData) return;
+  const isLoading = isEnsAddressLoading || isEnsNameLoading;
 
-    // ENS resolved successfully
-    setResolvedEns(address);
-    setAddress(ensData);
-    if (onChange) {
-      onChange(ensData);
-    }
-    // We don't want to run it on address change
-    // eslint-disable-next-line
-  }, [ensData, onChange]);
+  const isEnsValid = !isLoading && ((isEns && ensAddress !== null) || (currentValue && ensName));
+
+  // since it could be lots of ENSs for one address, we show ENS entered by user
+  const visibleEnsAddress = isEns ? currentValue : ensName;
 
   const onChangeAddress = async (event: ChangeEvent<HTMLInputElement>) => {
-    setResolvedEns("");
     setAddress(event.target.value);
     if (onChange) {
       onChange(event.target.value);
@@ -70,14 +62,14 @@ const AddressInput = ({ value, name, placeholder, onChange }: TAddressInputProps
     <div className="rounded-full border-2 border-base-300">
       <div className="form-control grow">
         <div className="flex w-full">
-          {resolvedEns && (
+          {isEnsValid && (
             <div className="flex bg-secondary rounded-l-full items-center">
-              <span className={ensAvatarData ? "w-[35px]" : ""}>
+              <span className={ensAvatar ? "w-[35px]" : ""}>
                 {/* Don't want to use nextJS Image here (and adding remote patterns for the URL) */}
                 {/* eslint-disable-next-line  */}
-                {ensAvatarData && <img className="w-full rounded-full" src={ensAvatarData} alt={`${ensData} avatar`} />}
+                {ensAvatar && <img className="w-full rounded-full" src={ensAvatar} alt={`${ensAddress} avatar`} />}
               </span>
-              <span className="text-accent px-2">{resolvedEns}</span>
+              <span className="text-accent px-2">{visibleEnsAddress}</span>
             </div>
           )}
           <input
@@ -85,15 +77,15 @@ const AddressInput = ({ value, name, placeholder, onChange }: TAddressInputProps
             type="text"
             placeholder={placeholder}
             className={`input input-ghost focus:outline-none focus:bg-transparent focus:text-gray-400 h-[2.2rem] min-h-[2.2rem] border w-full font-medium placeholder:text-accent/50 text-gray-400 grow ${
-              ensData === null ? "input-error" : ""
+              ensAddress === null ? "input-error" : ""
             }`}
-            value={currentValue}
+            value={ensAddress || currentValue}
             onChange={onChangeAddress}
-            disabled={isLoading}
+            disabled={isEnsAddressLoading}
           />
-          {address && (
+          {currentValue && (
             <span className="p-0 bg-base-100">
-              <Blockies className="!rounded-full" seed={address?.toLowerCase() as string} size={7} scale={5} />
+              <Blockies className="!rounded-full" seed={currentValue?.toLowerCase() as string} size={7} scale={5} />
             </span>
           )}
         </div>
