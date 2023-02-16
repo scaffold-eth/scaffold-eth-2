d51dfdd570d457cb913f436fc8b1a2b0e4888956
Shiv Bhonde <shivbhonde04@gmail.com>
2023-02-16 13:10:04 +0530

    add type for callbackListener function

--- packages/nextjs/hooks/scaffold-eth/useScaffoldEventsRead.ts
+++ packages/nextjs/hooks/scaffold-eth/useScaffoldEventsRead.ts
@@ -10,17 +10,21 @@ import { useDeployedContractInfo } from "./useDeployedContractInfo";
  * @param callbackListener - the callback that receives event
  * @param once - if set to true it will receive only a single event, then stop listening for the event. Defaults to false
  */
-export const useScaffoldEventsRead = (contractName: string, eventName: string, callbackListener: any, once = false) => {
+export const useScaffoldEventsRead = (
+  contractName: string,
+  eventName: string,
+  callbackListener: (...args: unknown[]) => void,
+  once = false,
+) => {
   const configuredChain = getTargetNetwork();
   const deployedContractData = useDeployedContractInfo(contractName);
 
   return useContractEvent({
     address: deployedContractData?.address,
     abi: deployedContractData?.abi,
-    eventName,
-    listener: callbackListener,
-
     chainId: configuredChain.id,
+    listener: callbackListener,
+    eventName,
     once,
   });
 };
