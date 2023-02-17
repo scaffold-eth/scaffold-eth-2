185c33469115ecbd8d97b5c8c5c04d9160c1a439
Austin Griffith <austin@ethereum.org>
2023-02-17 03:31:46 -0700

    Debug page UI refactor (#174)
    Co-authored-by: Rinat Akhunianov <rinat@hey.com>
    Co-authored-by: Carlos Sánchez <oceanrdn@gmail.com>

--- packages/nextjs/components/scaffold-eth/Contract/ContractUI.tsx
+++ packages/nextjs/components/scaffold-eth/Contract/ContractUI.tsx
@@ -8,8 +8,8 @@ import {
   getContractWriteMethods,
 } from "./utilsContract";
 import { Balance, Address } from "~~/components/scaffold-eth";
-import { useNetworkColor } from "~~/utils/scaffold-eth/useNetworkColor";
 import { useDeployedContractInfo } from "~~/hooks/scaffold-eth";
+import { useNetworkColor } from "~~/utils/scaffold-eth/useNetworkColor";
 
 type TContractUIProps = {
   contractName: string;
@@ -59,8 +59,26 @@ const ContractUI = ({ contractName }: TContractUIProps) => {
   }
 
   return (
-    <div className="col-span-5 grid grid-cols-1 lg:grid-cols-3 gap-10 justify-between">
-      <div className="col-span-2 flex flex-col gap-6">
+    <div className="col-span-5 grid grid-cols-1 lg:grid-cols-3 gap-10">
+      <div className="col-span-1 flex flex-col">
+        <div className="bg-base-100 border-base-300 border shadow-md shadow-secondary rounded-3xl px-8 mb-6 space-y-1 py-4">
+          <div className="flex">
+            <div className="flex gap-1">
+              <Address address={contractAddress} />
+              <Balance address={contractAddress} />
+            </div>
+          </div>
+          {chain && (
+            <p className="my-0 text-sm" style={{ color: networkColor }}>
+              <span className="font-bold">Network</span> : {chain.name}
+            </p>
+          )}
+        </div>
+        <div className="bg-base-300 rounded-3xl px-8 py-4 shadow-lg shadow-base-300">
+          {contractVariablesDisplay.methods.length > 0 ? contractVariablesDisplay.methods : "No contract variables"}
+        </div>
+      </div>
+      <div className="col-span-1 lg:col-span-2 flex flex-col gap-6">
         <div className="z-10">
           <div className="bg-base-100 rounded-3xl shadow-md shadow-secondary border border-base-300 collapse collapse-arrow overflow-visible flex flex-col mt-10 ">
             <input
@@ -94,24 +112,6 @@ const ContractUI = ({ contractName }: TContractUIProps) => {
               {contractWriteMethods.methods.length > 0 ? contractWriteMethods.methods : "No write methods"}
             </div>
           </div>
-        </div>
-      </div>
-      <div className="row-span-1 self-start flex flex-col">
-        <div className="bg-base-100 border-base-300 border shadow-md shadow-secondary rounded-3xl px-8 mb-6 space-y-1 py-4">
-          {chain && (
-            <p className="font-medium my-0" style={{ color: networkColor }}>
-              {chain.name}
-            </p>
-          )}
-          <div className="flex">
-            <div className="flex gap-1">
-              <Address address={contractAddress} />
-              <Balance address={contractAddress} />
-            </div>
-          </div>
-        </div>
-        <div className="bg-base-300 rounded-3xl px-8 py-4 shadow-lg shadow-base-300">
-          {contractVariablesDisplay.methods.length > 0 ? contractVariablesDisplay.methods : "No contract variables"}
         </div>
       </div>
     </div>
