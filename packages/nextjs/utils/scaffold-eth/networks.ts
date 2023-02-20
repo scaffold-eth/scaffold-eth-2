4a950d73959d354ae5284d1ad24f8b5b44a365d4
Carlos Sánchez <oceanrdn@gmail.com>
2023-02-20 17:38:20 +0100

    Remove unused attr

--- packages/nextjs/utils/scaffold-eth/networks.ts
+++ packages/nextjs/utils/scaffold-eth/networks.ts
@@ -2,72 +2,46 @@ import * as chains from "wagmi/chains";
 import { Network } from "@ethersproject/networks";
 
 export type TChainAttributes = {
-  name: string;
   // color | [lightThemeColor, darkThemeColor]
   color: string | [string, string];
-  id: number;
 };
 
 export const NETWORKS_EXTRA_DATA: Record<string, TChainAttributes> = {
   [chains.hardhat.id]: {
-    name: "hardhat",
     color: "#b8af0c",
-    id: 31337,
   },
   [chains.mainnet.id]: {
-    name: "mainnet",
     color: "#ff8b9e",
-    id: 1,
   },
   [chains.goerli.id]: {
-    name: "goerli",
     color: "#0975F6",
-    id: 5,
   },
   [chains.gnosis.id]: {
-    name: "gnosis",
     color: "#48a9a6",
-    id: 100,
   },
   [chains.polygon.id]: {
-    name: "polygon",
     color: "#2bbdf7",
-    id: 137,
   },
   [chains.polygonMumbai.id]: {
-    name: "mumbai",
     color: "#92D9FA",
-    id: 80001,
   },
   [chains.optimismGoerli.id]: {
-    name: "goerliOptimism",
     color: "#f01a37",
-    id: 420,
   },
   [chains.optimism.id]: {
-    name: "optimism",
     color: "#f01a37",
-    id: 10,
   },
   [chains.arbitrumGoerli.id]: {
-    name: "goerliArbitrum",
     color: "#28a0f0",
-    id: 421613,
   },
   [chains.arbitrum.id]: {
-    name: "arbitrum",
     color: "#28a0f0",
-    id: 42161,
   },
   [chains.fantom.id]: {
-    name: "fantom",
     color: "#1969ff",
-    id: 250,
   },
   [chains.fantomTestnet.id]: {
-    name: "testnetFantom",
     color: "#1969ff",
-    id: 4002,
   },
 };
 
