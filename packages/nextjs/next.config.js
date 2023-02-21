e9ec9c2d1d49809956f9ae043b9714a0dac9ae57
Shiv Bhonde <shivbhonde04@gmail.com>
2023-02-21 09:54:32 +0530

    add option & env to ignoreBuildError

--- packages/nextjs/next.config.js
+++ packages/nextjs/next.config.js
@@ -3,6 +3,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  typescript: {
+    ignoreBuildErrors: process.env.NEXT_PUBLIC_IGNORE_BUILD_ERROR === "true" ? true : false,
+  },
 };
 
 module.exports = nextConfig;
