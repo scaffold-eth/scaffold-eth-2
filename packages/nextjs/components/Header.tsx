0452b39eee2b2bf5fd7784566e6207508cf40cc1
Carlos Sánchez <oceanrdn@gmail.com>
2023-02-28 08:25:58 -0700

    Logo tweak

--- packages/nextjs/components/Header.tsx
+++ packages/nextjs/components/Header.tsx
@@ -79,13 +79,13 @@ export default function Header() {
             </ul>
           )}
         </div>
-        <div className="hidden lg:flex items-center gap-2 mx-4">
+        <div className="hidden lg:flex items-center gap-2 ml-4 mr-6">
           <Link href="/" passHref className="flex relative w-10 h-10">
             <Image alt="SE2 logo" className="cursor-pointer" fill src="/logo.svg" />
           </Link>
           <div className="flex flex-col">
-            <span className="font-bold">Scaffold-eth</span>
-            <span className="text-xs">Forkable Ethereum dev stack</span>
+            <span className="font-bold leading-tight">Scaffold-eth</span>
+            <span className="text-xs">Ethereum dev stack</span>
           </div>
         </div>
         <ul className="hidden lg:flex lg:flex-nowrap menu menu-horizontal px-1 gap-2">{navLinks}</ul>
