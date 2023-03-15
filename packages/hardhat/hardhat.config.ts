8f19b21cd837aaa9bc48927d74942dcece9940da
Shiv Bhonde <shivbhonde04@gmail.com>
2023-03-15 10:41:03 +0530

    put hardhat env's in root config

--- packages/hardhat/hardhat.config.ts
+++ packages/hardhat/hardhat.config.ts
@@ -1,24 +1,18 @@
+// This adds support for typescript paths mappings
+import "tsconfig-paths/register";
+import * as path from "path";
 import * as dotenv from "dotenv";
-dotenv.config();
+dotenv.config({ path: path.join(__dirname, "..", "..", ".env") });
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "hardhat-deploy";
 import { kebabToCamelCase } from "./utils";
-import scaffoldConfig from "../../scaffold.config";
+import scaffoldConfig from "@root/scaffold.config";
 
 const defaultNetwork =
   scaffoldConfig.targetNetwork.network === "hardhat"
     ? "localhost"
     : kebabToCamelCase(scaffoldConfig.targetNetwork.network);
-
-// If not set, it uses ours Alchemy's default API key.
-// You can get your own at https://dashboard.alchemyapi.io
-const providerApiKey = process.env.ALCHEMY_API_KEY || "oKxs-03sij-U_N0iOlrSsZFr29-IqbuF";
-// If not set, it uses the hardhat account 0 private key.
-const deployerPrivateKey =
-  process.env.DEPLOYER_PRIVATE_KEY ?? "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80";
-// If not set, it uses ours Etherscan default API key.
-const etherscanApiKey = process.env.ETHERSCAN_API_KEY || "DNXJA8RX2Q3VZ4URQIWP7Z68CJXQZSC6AW";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
@@ -34,50 +28,50 @@ const config: HardhatUserConfig = {
     // If the network you are looking for is not here you can add new network settings
     hardhat: {
       forking: {
-        url: `https://eth-mainnet.alchemyapi.io/v2/${providerApiKey}`,
+        url: `https://eth-mainnet.alchemyapi.io/v2/${scaffoldConfig.providerApiKey}`,
         enabled: process.env.MAINNET_FORKING_ENABLED === "true",
       },
     },
     mainnet: {
-      url: `https://eth-mainnet.alchemyapi.io/v2/${providerApiKey}`,
-      accounts: [deployerPrivateKey],
+      url: `https://eth-mainnet.alchemyapi.io/v2/${scaffoldConfig.providerApiKey}`,
+      accounts: [scaffoldConfig.deployerPrivateKey],
     },
     sepolia: {
       url: "https://rpc.sepolia.org",
-      accounts: [deployerPrivateKey],
+      accounts: [scaffoldConfig.deployerPrivateKey],
     },
     goerli: {
-      url: `https://eth-goerli.alchemyapi.io/v2/${providerApiKey}`,
-      accounts: [deployerPrivateKey],
+      url: `https://eth-goerli.alchemyapi.io/v2/${scaffoldConfig.providerApiKey}`,
+      accounts: [scaffoldConfig.deployerPrivateKey],
     },
     arbitrum: {
-      url: `https://arb-mainnet.g.alchemy.com/v2/${providerApiKey}`,
-      accounts: [deployerPrivateKey],
+      url: `https://arb-mainnet.g.alchemy.com/v2/${scaffoldConfig.providerApiKey}`,
+      accounts: [scaffoldConfig.deployerPrivateKey],
     },
     arbitrumGoerli: {
-      url: `https://arb-goerli.g.alchemy.com/v2/${providerApiKey}`,
-      accounts: [deployerPrivateKey],
+      url: `https://arb-goerli.g.alchemy.com/v2/${scaffoldConfig.providerApiKey}`,
+      accounts: [scaffoldConfig.deployerPrivateKey],
     },
     optimism: {
-      url: `https://opt-mainnet.g.alchemy.com/v2/${providerApiKey}`,
-      accounts: [deployerPrivateKey],
+      url: `https://opt-mainnet.g.alchemy.com/v2/${scaffoldConfig.providerApiKey}`,
+      accounts: [scaffoldConfig.deployerPrivateKey],
     },
     optimismGoerli: {
-      url: `https://opt-goerli.g.alchemy.com/v2/${providerApiKey}`,
-      accounts: [deployerPrivateKey],
+      url: `https://opt-goerli.g.alchemy.com/v2/${scaffoldConfig.providerApiKey}`,
+      accounts: [scaffoldConfig.deployerPrivateKey],
     },
     polygon: {
-      url: `https://polygon-mainnet.g.alchemy.com/v2/${providerApiKey}`,
-      accounts: [deployerPrivateKey],
+      url: `https://polygon-mainnet.g.alchemy.com/v2/${scaffoldConfig.providerApiKey}`,
+      accounts: [scaffoldConfig.deployerPrivateKey],
     },
     polygonMumbai: {
-      url: `https://polygon-mumbai.g.alchemy.com/v2/${providerApiKey}`,
-      accounts: [deployerPrivateKey],
+      url: `https://polygon-mumbai.g.alchemy.com/v2/${scaffoldConfig.providerApiKey}`,
+      accounts: [scaffoldConfig.deployerPrivateKey],
     },
   },
   verify: {
     etherscan: {
-      apiKey: `${etherscanApiKey}`,
+      apiKey: `${scaffoldConfig.etherscanApiKey}`,
     },
   },
 };
--- scaffold.config.ts
+++ scaffold.config.ts
@@ -1,13 +1,23 @@
 import * as chain from "@wagmi/chains";
 
 type ScaffoldConfig = {
-	targetNetwork: chain.Chain;
-	pollingInterval: number;
+  targetNetwork: chain.Chain;
+  pollingInterval: number;
+  deployerPrivateKey: string;
+  etherscanApiKey: string;
+  providerApiKey: string;
 };
 
 const scaffoldConfig: ScaffoldConfig = {
-	targetNetwork: chain.polygonMumbai,
-	pollingInterval: 30000,
+  targetNetwork: chain.hardhat,
+  pollingInterval: 30000,
+  deployerPrivateKey:
+    process.env.DEPLOYER_PRIVATE_KEY ??
+    "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80",
+  etherscanApiKey:
+    process.env.ETHERSCAN_API_KEY || "DNXJA8RX2Q3VZ4URQIWP7Z68CJXQZSC6AW",
+  providerApiKey:
+    process.env.ALCHEMY_API_KEY || "oKxs-03sij-U_N0iOlrSsZFr29-IqbuF",
 } as const;
 
 export default scaffoldConfig;
