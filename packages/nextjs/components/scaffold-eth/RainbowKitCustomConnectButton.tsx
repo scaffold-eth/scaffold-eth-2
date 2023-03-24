670fd026e11eb1b09280a7a0664a2d906c512406
Shiv Bhonde <shivbhonde04@gmail.com>
2023-03-24 13:55:09 +0530

    set enableBurnerWalletOnLoad to true as default

--- packages/nextjs/components/scaffold-eth/RainbowKitCustomConnectButton.tsx
+++ packages/nextjs/components/scaffold-eth/RainbowKitCustomConnectButton.tsx
@@ -6,7 +6,7 @@ import { getTargetNetwork } from "~~/utils/scaffold-eth";
 
 // todo: move this later scaffold config.  See TAutoConnect for comments on each prop
 const tempAutoConnectConfig: TAutoConnect = {
-  enableBurnerWalletOnLoad: false,
+  enableBurnerWalletOnLoad: true,
   autoConnect: true,
 };
 
