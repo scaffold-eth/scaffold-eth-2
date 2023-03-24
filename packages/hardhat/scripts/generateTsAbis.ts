8e2380ed341810f8f2cdc7278d5facf7db6f9af9
sverps <sverps@gmail.com>
2023-03-24 11:30:09 +0100

    build: contract generation cleanup

--- packages/hardhat/scripts/generateTsAbis.ts
+++ packages/hardhat/scripts/generateTsAbis.ts
@@ -1,21 +1,19 @@
 import * as fs from "fs";
 //@ts-expect-error  This script runs after `hardhat deploy --export` therefore its deterministic that it will present
-import allGeneratedContracts from "../../nextjs/generated/hardhat_contracts.json";
+import allGeneratedContracts from "../generated/hardhat_contracts.json";
 import prettier from "prettier";
 
 function main() {
-  const GENERATED_PATH = "../nextjs/generated/hardhat_contracts";
+  const TARGET_PATH = "../nextjs/generated/hardhat_contracts";
 
   const fileContent = Object.entries(allGeneratedContracts).reduce((content, [chainId, chainConfig]) => {
     return `${content}${parseInt(chainId).toFixed(0)}:${JSON.stringify(chainConfig, null, 2)},`;
   }, "");
 
   fs.writeFileSync(
-    `${GENERATED_PATH}.ts`,
+    `${TARGET_PATH}.ts`,
     prettier.format(`export default {${fileContent}} as const;`, { parser: "typescript" }),
   );
-  // remove json file due to ambiguity
-  fs.unlinkSync(`${GENERATED_PATH}.json`);
 }
 
 try {
