978cdd28b8295c237a6c9af5949fc9700c62f138
Shiv Bhonde <shivbhonde04@gmail.com>
2023-03-24 13:35:50 +0530

    revert Scaffold.config back to hardhat

--- packages/nextjs/scaffold.config.ts
+++ packages/nextjs/scaffold.config.ts
@@ -8,7 +8,7 @@ type ScaffoldConfig = {
 
 const scaffoldConfig = {
   // The network where your DApp lives in
-  targetNetwork: chains.goerli,
+  targetNetwork: chains.hardhat,
 
   // The interval at which your front-end polls the RPC servers for new data
   // it has no effect on the local network
