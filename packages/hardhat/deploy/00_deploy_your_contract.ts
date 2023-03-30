9b37c8bcc040fcd8818708f5fd70117a2b2f46e6
anfro.eth <aridding18@gmail.com>
2023-03-30 15:01:25 -0400

    updated deploy to reference new AddEquip

--- packages/hardhat/deploy/00_deploy_your_contract.ts
+++ packages/hardhat/deploy/00_deploy_your_contract.ts
@@ -21,7 +21,7 @@ const deployYourContract: DeployFunction = async function (hre: HardhatRuntimeEn
   const { deployer } = await hre.getNamedAccounts();
   const { deploy } = hre.deployments;
 
-  await deploy("YourContract", {
+  await deploy("AddEquip", {
     from: deployer,
     // Contract constructor arguments
     args: [deployer],
@@ -39,4 +39,4 @@ export default deployYourContract;
 
 // Tags are useful if you have multiple deploy files and only want to run one of them.
 // e.g. yarn deploy --tags YourContract
-deployYourContract.tags = ["YourContract"];
+deployYourContract.tags = ["AddEquip"];
