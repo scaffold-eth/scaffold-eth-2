c546f8ba73bfbde6fd9e298649e5124fe3d65979
mercuricchloride <mercuryprotocol@protonmail.com>
2023-04-01 17:55:36 -0700

    latest

--- packages/nextjs/components/Three.tsx
+++ packages/nextjs/components/Three.tsx
@@ -1,41 +1,41 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import { useGLTF, Clone } from "@react-three/drei";
 import { Object3D } from "three";
 import { Object3DProps, useFrame } from "@react-three/fiber";
 
-/* const Oceans: React.FC<props> = ({ count }) => {
- *   const model = useGLTF("/ocean.glb");
- *   const [oceans, setOceans] = useState([]);
- *
- *   useEffect(() => {
- *     const newOceans = [];
- *     for (let i = 0; i < count; i++) {
- *       newOceans.push({
- *         position: {
- *           x: i * 100,
- *           z: i * 100,
- *         },
- *       });
- *     }
- *     setOceans(newOceans);
- *   }, [count]);
- *
- *   return (
- *     <Instances limit={100} range={100}>
- *       <mesh>
- *         <primitive object={model.scene} />
- *       </mesh>
- *       <Instance position={[1, 2, 3]} />
- *     </Instances>
- *   );
- * }; */
-
-interface ShipProps extends Object3DProps {
-  count?: number;
-  range?: number;
+interface OceanProps extends Object3DProps {
+  x: number;
+  z: number;
 }
+const Ocean = ({ x, z }: OceanProps) => {
+  const model = useGLTF("/ocean.glb");
+  return (
+    <object3D position={[x, 0, z]} scale={[0.05, 0.05, 0.05]}>
+      <primitive object={model.scene.clone()} />
+    </object3D>
+  );
+};
 
-interface MakeShipProps {
+interface OceansProps extends Object3DProps {
+  sideLength: number;
+}
+const Oceans = ({ sideLength }: OceansProps) => {
+  const oceans = Array.from({ length: sideLength * sideLength }).map((_, index) => {
+    const x = (index % sideLength) * 90 - (sideLength * 90) / 2;
+    const z = Math.floor(index / sideLength) * 90 - (sideLength * 90) / 2;
+    return { x, z };
+  });
+
+  return (
+    <group>
+      {oceans.map((ocean, index) => (
+        <Ocean key={index} {...ocean} />
+      ))}
+    </group>
+  );
+};
+
+interface ShipProps {
   x: number;
   y: number;
   z: number;
@@ -43,25 +43,63 @@ interface MakeShipProps {
   rate: number;
 }
 
-function Ship({ x, y, z, rotation, rate }: MakeShipProps) {
+function Ship({ x, y, z, rotation, rate }: ShipProps) {
   const model = useGLTF("/ship.glb");
-  const ref = useRef<Object3D>(null!);
+  const ref = useRef<Object3D>(null);
+  const [clicked, setClicked] = useState(false);
 
   useFrame(() => {
+    if (!ref.current) return;
     if (ref.current.rotation.x > 0.2 || ref.current.rotation.x < -0.2) {
       rate = -rate;
     }
     ref.current.rotation.x += rate;
+
+    /* if (clicked) {
+     *     ref.current.position.set(ref.current.position.x, ref.current.position.y - 0.1, ref.current.position.z);
+     * } */
   });
 
+  const onClick = () => {
+    console.log("clicked a super epic ship");
+    if (!ref.current) return;
+    ref.current.position.set(ref.current.position.x, ref.current.position.y - 10, ref.current.position.z);
+    setClicked(!clicked);
+  };
+
+  const onPointerOver = () => {
+    if (!ref.current) return;
+
+    ref.current.scale.set(20, 20, 20);
+  };
+
+  const onPointerLeave = () => {
+    if (!ref.current) return;
+
+    ref.current.scale.set(10, 10, 10);
+  };
+
   return (
-    <object3D ref={ref} position={[x, y, z]} scale={[10, 10, 10]} rotation={[0, rotation, 0]}>
-      <Clone object={model.scene} />
+    <object3D
+      ref={ref}
+      position={[x, y, z]}
+      scale={[10, 10, 10]}
+      rotation={[0, rotation, 0]}
+      onClick={onClick}
+      onPointerOver={onPointerOver}
+      onPointerLeave={onPointerLeave}
+    >
+      <Clone object={model.scene} deep />
     </object3D>
   );
 }
 
-function Ships({ count = 10, range = 10 }: ShipProps) {
+interface ShipsProps extends Object3DProps {
+  count?: number;
+  range?: number;
+}
+
+function Ships({ count = 10, range = 10 }: ShipsProps) {
   const ships = Array.from({ length: count }, () => {
     const x = Math.random() * range;
     const y = 0;
@@ -75,5 +113,10 @@ function Ships({ count = 10, range = 10 }: ShipProps) {
 }
 
 export default function Three() {
-  return <Ships count={10} range={200} />;
+  return (
+    <group>
+      <Ships count={10} range={400} />;
+      <Oceans sideLength={10} />
+    </group>
+  );
 }
