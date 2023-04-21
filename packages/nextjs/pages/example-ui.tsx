790d299f3804cf78e33688a9425fb545155a42fb
Shiv Bhonde <shivbhonde04@gmail.com>
2023-04-21 13:45:37 +0530

    update nomanclautre inline with latest naming

--- packages/nextjs/pages/example-ui.tsx
+++ packages/nextjs/pages/example-ui.tsx
@@ -7,8 +7,8 @@ const ExampleUI: NextPage = () => {
   return (
     <>
       <Head>
-        <title>Scaffold-eth Example Ui</title>
-        <meta name="description" content="Created with 🏗 scaffold-eth" />
+        <title>Scaffold-ETH 2 Example Ui</title>
+        <meta name="description" content="Created with 🏗 scaffold-eth-2" />
         {/* We are importing the font this way to lighten the size of SE2. */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link href="https://fonts.googleapis.com/css2?family=Bai+Jamjuree&display=swap" rel="stylesheet" />
