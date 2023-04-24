8b4856069a0bf0855ec3dafd6afdbf416be4c670
Kevin Joshi <kevinjoshi46b@gmail.com>
2023-04-24 15:33:34 +0530

    feat: EthPrice now fetched as per target network

--- packages/nextjs/utils/scaffold-eth/fetchPriceFromUniswap.ts
+++ packages/nextjs/utils/scaffold-eth/fetchPriceFromUniswap.ts
@@ -1,15 +1,22 @@
-import { Fetcher, Route, Token, WETH } from "@uniswap/sdk";
+import { Fetcher, Route, Token } from "@uniswap/sdk";
 import { Provider } from "@wagmi/core";
+import { getTargetNetwork } from "~~/utils/scaffold-eth";
 
 export const fetchPriceFromUniswap = async (provider: Provider): Promise<number> => {
-  try {
-    const DAI = new Token(1, "0x6B175474E89094C44Da98b954EedeAC495271d0F", 18);
-    const pair = await Fetcher.fetchPairData(DAI, WETH[DAI.chainId], provider);
-    const route = new Route([pair], WETH[DAI.chainId]);
-    const price = parseFloat(route.midPrice.toSignificant(6));
-    return price;
-  } catch (error) {
-    console.error("useEthPrice - Error fetching ETH price from Uniswap: ", error);
+  const configuredNetwork = getTargetNetwork();
+  if (configuredNetwork.tokenAddress) {
+    try {
+      const DAI = new Token(1, "0x6B175474E89094C44Da98b954EedeAC495271d0F", 18);
+      const TOKEN = await Fetcher.fetchTokenData(1, configuredNetwork.tokenAddress, provider);
+      const pair = await Fetcher.fetchPairData(DAI, TOKEN, provider);
+      const route = new Route([pair], TOKEN);
+      const price = parseFloat(route.midPrice.toSignificant(6));
+      return price;
+    } catch (error) {
+      console.error("useEthPrice - Error fetching ETH price from Uniswap: ", error);
+      return 0;
+    }
+  } else {
     return 0;
   }
 };
--- packages/nextjs/utils/scaffold-eth/networks.ts
+++ packages/nextjs/utils/scaffold-eth/networks.ts
@@ -5,47 +5,61 @@ import scaffoldConfig from "~~/scaffold.config";
 export type TChainAttributes = {
   // color | [lightThemeColor, darkThemeColor]
   color: string | [string, string];
+  tokenAddress?: string;
 };
 
 export const NETWORKS_EXTRA_DATA: Record<string, TChainAttributes> = {
   [chains.hardhat.id]: {
     color: "#b8af0c",
+    tokenAddress: "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
   },
   [chains.mainnet.id]: {
     color: "#ff8b9e",
+    tokenAddress: "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
   },
   [chains.sepolia.id]: {
     color: ["#5f4bb6", "#87ff65"],
+    tokenAddress: "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
   },
   [chains.goerli.id]: {
     color: "#0975F6",
+    tokenAddress: "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
   },
   [chains.gnosis.id]: {
     color: "#48a9a6",
+    tokenAddress: "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
   },
   [chains.polygon.id]: {
     color: "#2bbdf7",
+    tokenAddress: "0x7D1AfA7B718fb893dB30A3aBc0Cfc608AaCfeBB0",
   },
   [chains.polygonMumbai.id]: {
     color: "#92D9FA",
+    tokenAddress: "0x7D1AfA7B718fb893dB30A3aBc0Cfc608AaCfeBB0",
   },
   [chains.optimismGoerli.id]: {
     color: "#f01a37",
+    tokenAddress: "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
   },
   [chains.optimism.id]: {
     color: "#f01a37",
+    tokenAddress: "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
   },
   [chains.arbitrumGoerli.id]: {
     color: "#28a0f0",
+    tokenAddress: "0xB50721BCf8d664c30412Cfbc6cf7a15145234ad1",
   },
   [chains.arbitrum.id]: {
     color: "#28a0f0",
+    tokenAddress: "0xB50721BCf8d664c30412Cfbc6cf7a15145234ad1",
   },
   [chains.fantom.id]: {
     color: "#1969ff",
+    tokenAddress: "0x4E15361FD6b4BB609Fa63C81A2be19d873717870",
   },
   [chains.fantomTestnet.id]: {
     color: "#1969ff",
+    tokenAddress: "0x4E15361FD6b4BB609Fa63C81A2be19d873717870",
   },
 };
 
