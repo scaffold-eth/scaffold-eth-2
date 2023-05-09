07df166c878e606a5009ce07c614949ab49a7ea5
Shiv Bhonde <shivbhonde04@gmail.com>
2023-05-10 01:55:55 +0530

    rm px for small devices in navbar

--- packages/nextjs/components/Header.tsx
+++ packages/nextjs/components/Header.tsx
@@ -55,7 +55,7 @@ export const Header = () => {
   );
 
   return (
-    <div className="sticky lg:static top-0 navbar bg-base-100 min-h-0 flex-shrink-0 justify-between z-20 shadow-md shadow-secondary">
+    <div className="sticky lg:static top-0 navbar bg-base-100 min-h-0 flex-shrink-0 justify-between z-20 shadow-md shadow-secondary px-0 sm:px-2">
       <div className="navbar-start w-auto lg:w-1/2">
         <div className="lg:hidden dropdown" ref={burgerMenuRef}>
           <label
