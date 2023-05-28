92f2078bdaf5a0bc9c2b4db8c26603a9fd889aa0
abydenes <108868128+abydenes@users.noreply.github.com>
2023-05-28 19:05:45 +0300

    Show deployed bytecode and opcodes in address page

---
+++ packages/nextjs/components/blockexplorer/index.tsx
@@ -0,0 +1,5 @@
+export * from "./AddressCodeTab";
+export * from "./PaginationButton";
+export * from "./SearchBar";
+export * from "./TransactionHash";
+export * from "./TransactionsTable";
--- packages/nextjs/pages/blockexplorer/address/[address].tsx
+++ packages/nextjs/pages/blockexplorer/address/[address].tsx
@@ -1,13 +1,41 @@
+import { useEffect, useMemo, useState } from "react";
 import { useRouter } from "next/router";
-import { PaginationButton } from "~~/components/blockexplorer/PaginationButton";
-import { TransactionsTable } from "~~/components/blockexplorer/TransactionsTable";
+import { ethers } from "ethers";
+import fs from "fs";
+import { GetServerSideProps } from "next";
+import path from "path";
+import { ParsedUrlQuery } from "querystring";
+import { AddressCodeTab, PaginationButton, TransactionsTable } from "~~/components/blockexplorer/";
 import { Address, Balance } from "~~/components/scaffold-eth";
 import { useFetchBlocks } from "~~/hooks/scaffold-eth";
 
-const AddressPage = () => {
+type AddressCodeTabProps = {
+  bytecode: string;
+  assembly: string;
+};
+
+type PageProps = {
+  address: string;
+  contractData: AddressCodeTabProps | null;
+};
+
+const AddressPage = ({ address, contractData }: PageProps) => {
   const router = useRouter();
-  const address = typeof router.query.address === "string" ? router.query.address : "";
+  // const address = typeof router.query.address === "string" ? router.query.address : "";
   const { blocks, transactionReceipts, currentPage, totalBlocks, setCurrentPage, isLoading } = useFetchBlocks();
+  const [activeTab, setActiveTab] = useState("transactions");
+  const [isContract, setIsContract] = useState(false);
+
+  const provider = useMemo(() => new ethers.providers.JsonRpcProvider("http://localhost:8545"), []);
+
+  useEffect(() => {
+    const checkIsContract = async () => {
+      const contractCode = await provider.getCode(address);
+      setIsContract(contractCode !== "0x");
+    };
+
+    checkIsContract();
+  }, [address, provider]);
 
   // Filter blocks that contain transactions related to the desired address
   const filteredBlocks = blocks.filter(block =>
@@ -38,10 +66,103 @@ const AddressPage = () => {
           </div>
         </div>
       </div>
-      <TransactionsTable blocks={filteredBlocks} transactionReceipts={transactionReceipts} isLoading={isLoading} />
-      <PaginationButton currentPage={currentPage} totalItems={totalBlocks} setCurrentPage={setCurrentPage} />
+      {isContract && (
+        <div className="tabs">
+          <button
+            className={`tab tab-lg tab-lifted ${activeTab === "transactions" ? "tab-active" : ""}`}
+            onClick={() => setActiveTab("transactions")}
+          >
+            Transactions
+          </button>
+          <button
+            className={`tab tab-lg tab-lifted ${activeTab === "code" ? "tab-active" : ""}`}
+            onClick={() => setActiveTab("code")}
+          >
+            Code
+          </button>
+          <button
+            className={`tab tab-lg tab-lifted ${activeTab === "storage" ? "tab-active" : ""}`}
+            onClick={() => setActiveTab("storage")}
+          >
+            Storage
+          </button>
+          <button
+            className={`tab tab-lg tab-lifted ${activeTab === "logs" ? "tab-active" : ""}`}
+            onClick={() => setActiveTab("logs")}
+          >
+            Logs
+          </button>
+        </div>
+      )}
+      {activeTab === "transactions" && (
+        <div className="pt-4">
+          <TransactionsTable blocks={filteredBlocks} transactionReceipts={transactionReceipts} isLoading={isLoading} />
+          <PaginationButton currentPage={currentPage} totalItems={totalBlocks} setCurrentPage={setCurrentPage} />
+        </div>
+      )}
+      {activeTab === "code" && contractData && (
+        <AddressCodeTab bytecode={contractData.bytecode} assembly={contractData.assembly} />
+      )}
+      {/* {activeTab === 'storage' && <AddressStorageTab address={address} />}
+      {activeTab === 'logs' && <AddressLogsTab address={address} />} */}
     </div>
   );
 };
 
 export default AddressPage;
+
+interface Params extends ParsedUrlQuery {
+  address: string;
+}
+
+export const getServerSideProps: GetServerSideProps = async context => {
+  const address = (context.params as Params).address;
+
+  // Set the path to the build-info directory
+  const buildInfoDirectory = path.join(
+    __dirname,
+    "..",
+    "..",
+    "..",
+    "..",
+    "..",
+    "..",
+    "hardhat",
+    "artifacts",
+    "build-info",
+  );
+
+  // Check if the directory exists
+  if (!fs.existsSync(buildInfoDirectory)) {
+    throw new Error(`Directory ${buildInfoDirectory} not found.`);
+  }
+
+  // Read the directory
+  const buildInfoFiles = fs.readdirSync(buildInfoDirectory);
+
+  let bytecode = "";
+  let assembly = "";
+
+  // Iterate over the files in the directory
+  for (let i = 0; i < buildInfoFiles.length; i++) {
+    const filePath = path.join(buildInfoDirectory, buildInfoFiles[i]);
+
+    // Parse the JSON file
+    const buildInfo = JSON.parse(fs.readFileSync(filePath, "utf8"));
+
+    // Check if the contract address exists in the file
+    for (const contractName in buildInfo.output.contracts) {
+      for (const contract in buildInfo.output.contracts[contractName]) {
+        bytecode = buildInfo.output.contracts[contractName][contract].evm.deployedBytecode.object;
+        assembly = buildInfo.output.contracts[contractName][contract].evm.bytecode.opcodes;
+        break;
+      }
+    }
+
+    if (bytecode && assembly) {
+      break;
+    }
+  }
+
+  return { props: { address, contractData: { bytecode, assembly } } };
+};
