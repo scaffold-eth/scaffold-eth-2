930a573a3f8c3d4a0359d9d37428350865427afe
Shiv Bhonde <shivbhonde04@gmail.com>
2023-06-01 11:42:46 +0530

    remove extra state from AdressLogsTab since already maintaing state in `useContractLogs` which is returned

--- packages/nextjs/components/blockexplorer/AddressLogsTab.tsx
+++ packages/nextjs/components/blockexplorer/AddressLogsTab.tsx
@@ -1,20 +1,13 @@
-import { useEffect, useState } from "react";
 import { useContractLogs } from "~~/hooks/scaffold-eth";
 
 export const AddressLogsTab = ({ address }: { address: string }) => {
-  const [logs, setLogs] = useState<any[]>([]);
-
   const contractLogs = useContractLogs(address);
-
-  useEffect(() => {
-    setLogs(contractLogs);
-  }, [contractLogs]);
 
   return (
     <div className="flex flex-col gap-3 p-4">
       <div className="mockup-code overflow-auto max-h-[500px]">
         <pre className="px-5 whitespace-pre-wrap break-words">
-          {logs.map((log, i) => (
+          {contractLogs.map((log, i) => (
             <div key={i}>
               <strong>Log:</strong> {JSON.stringify(log, null, 2)}
             </div>
