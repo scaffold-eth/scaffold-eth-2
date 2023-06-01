d12fcdea679466c935d42cce4ab264ab4b861b3d
port <108868128+portdeveloper@users.noreply.github.com>
2023-06-01 22:31:10 +0300

    Update packages/nextjs/pages/index.tsx
    Co-authored-by: Carlos Sánchez <oceanrdn@gmail.com>

--- packages/nextjs/pages/index.tsx
+++ packages/nextjs/pages/index.tsx
@@ -48,7 +48,7 @@ const Home: NextPage = () => {
             <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
               <MagnifyingGlassIcon className="h-8 w-8 fill-secondary" />
               <p>
-                Explore your local hardhat node with the{" "}
+                Explore your local transactions with the{" "}
                 <Link href="/blockexplorer" passHref className="link">
                   Block Explorer
                 </Link>{" "}
