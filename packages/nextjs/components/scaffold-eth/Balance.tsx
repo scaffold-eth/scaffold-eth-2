15b9a059293f720b293819f0f545feaa4c1dab3e
Samuel <sverps@gmail.com>
2023-06-14 22:55:42 +0200

    fix(Balance): font-size of symbol should be relative to current font-size, not document root

--- packages/nextjs/components/scaffold-eth/Balance.tsx
+++ packages/nextjs/components/scaffold-eth/Balance.tsx
@@ -27,7 +27,7 @@ export const Balance = ({ address, className = "" }: TBalanceProps) => {
   if (isError) {
     return (
       <div className={`border-2 border-gray-400 rounded-md px-2 flex flex-col items-center max-w-fit cursor-pointer`}>
-        <div className="text-warning text-xs">Error</div>
+        <div className="text-warning">Error</div>
       </div>
     );
   }
@@ -41,11 +41,11 @@ export const Balance = ({ address, className = "" }: TBalanceProps) => {
         {isEthBalance ? (
           <>
             <span>{balance?.toFixed(4)}</span>
-            <span className="text-xs font-bold ml-1">{configuredNetwork.nativeCurrency.symbol}</span>
+            <span className="text-[0.8em] font-bold ml-1">{configuredNetwork.nativeCurrency.symbol}</span>
           </>
         ) : (
           <>
-            <span className="text-xs font-bold mr-1">$</span>
+            <span className="text-[0.8em] font-bold mr-1">$</span>
             <span>{(balance * price).toFixed(2)}</span>
           </>
         )}
