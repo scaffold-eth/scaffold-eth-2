3d3ceab626fa7966ad603716beee8ae0abd6e9cd
Shiv Bhonde <shivbhonde04@gmail.com>
2023-06-22 13:25:48 +0530

    add hardhat-zksync-verify for contract verification

--- packages/hardhat/hardhat.config.ts
+++ packages/hardhat/hardhat.config.ts
@@ -4,6 +4,7 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "hardhat-deploy";
 import "@matterlabs/hardhat-zksync-solc";
+import "@matterlabs/hardhat-zksync-verify";
 
 // If not set, it uses ours Alchemy's default API key.
 // You can get your own at https://dashboard.alchemyapi.io
@@ -81,11 +82,13 @@ const config: HardhatUserConfig = {
       url: "https://testnet.era.zksync.dev",
       zksync: true,
       accounts: [deployerPrivateKey],
+      verifyURL: "https://zksync2-testnet-explorer.zksync.dev/contract_verification",
     },
     zkSync: {
       url: "https://mainnet.era.zksync.io",
       zksync: true,
       accounts: [deployerPrivateKey],
+      verifyURL: "https://zksync2-mainnet-explorer.zksync.io/contract_verification",
     },
   },
   verify: {
