cc1af9f5b227085767b05006baca88b2ca7a74d5
Samuel | solidixen.eth <sverps@gmail.com>
2023-07-07 09:58:32 +0200

    fix: type issues when multiple contracts deployed (#410)

--- packages/nextjs/hooks/scaffold-eth/useScaffoldEventSubscriber.ts
+++ packages/nextjs/hooks/scaffold-eth/useScaffoldEventSubscriber.ts
@@ -1,4 +1,4 @@
-import { ExtractAbiEventNames } from "abitype";
+import { Abi, ExtractAbiEventNames } from "abitype";
 import { Log } from "viem";
 import { useContractEvent } from "wagmi";
 import { useDeployedContractInfo } from "~~/hooks/scaffold-eth";
@@ -24,7 +24,7 @@ export const useScaffoldEventSubscriber = <
 
   return useContractEvent({
     address: deployedContractData?.address,
-    abi: deployedContractData?.abi,
+    abi: deployedContractData?.abi as Abi,
     chainId: getTargetNetwork().id,
     listener: listener as (logs: Log[]) => void,
     eventName,
