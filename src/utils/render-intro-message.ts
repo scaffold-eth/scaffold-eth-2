42e26bb18c8247b614ac6cd5a4b22433cfad6828
Carlos Sánchez <oceanrdn@gmail.com>
2023-07-07 21:08:27 +0200

    Remove the ascii art and use chalk

--- src/utils/render-intro-message.ts
+++ src/utils/render-intro-message.ts
@@ -1,14 +1,11 @@
 import chalk from "chalk";
 
 export const TITLE_TEXT = `
-┏━━━┓       ┏━┓┏━┓  ┏┓  ┏┓   ┏━━━┓━━━━┓┓ ┏┓  ┏━━━┓
-┃┏━┓┃       ┃┏┛┃┏┛  ┃┃  ┃┃   ┃┏━━┛┏┓┏┓┃┃ ┃┃  ┃┏━┓┃
-┃┗━━┓━━┓━━┓┏┛┗┓┛┗┓━━┓┃┏━┛┃   ┃┗━━┓┛┃┃┗┛┗━┛┃  ┗┛┏┛┃
-┗━━┓┃┏━┛┏┓┃┗┓┏┛┓┏┛┏┓┃┃┃┏┓┃━━━┓┏━━┛ ┃┃ ┃┏━┓┃  ┏━┛┏┛
-┃┗━┛┃┗━┓┗┛┗┓┃┃ ┃┃┃┗┛┃┗┓┗┛┃━━━┛┗━━┓┏┛┗┓┃┃ ┃┃  ┃ ┗━┓
-┗━━━┛━━┛━━━┛┗┛ ┗┛┗━━┛━┛━━┛   ┗━━━┛┗━━┛┗┛ ┗┛  ┗━━━┛
+ ${chalk.bold.blue("+-+-+-+-+-+-+-+-+-+-+-+-+-+-+")}
+ ${chalk.bold.blue("| Create Scaffold-ETH 2 app |")}
+ ${chalk.bold.blue("+-+-+-+-+-+-+-+-+-+-+-+-+-+-+")}
 `;
 
 export function renderIntroMessage() {
-  console.log(chalk.bold.blue(TITLE_TEXT), "\n\n");
+  console.log(TITLE_TEXT);
 }
