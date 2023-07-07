da4395063eb23785695a5b778a674c0063961165
Shiv Bhonde <shivbhonde04@gmail.com>
2023-07-07 20:50:15 +0200

    update the outro message

--- src/utils/render-outro-message.ts
+++ src/utils/render-outro-message.ts
@@ -4,14 +4,31 @@ export function renderOutroMessage(options: Options): void {
   let message = `
   \n
   ${chalk.bold.green("Congratulations!")} Your project has been scaffolded! 🎉
-  \n
+
   ${chalk.bold("Next steps:")}
-    \n
+  
   ${chalk.dim("cd")} ${options.project}
   `;
 
+  if (
+    options.extensions.includes("hardhat") ||
+    options.extensions.includes("foundry")
+  ) {
+    message += `
+    \t${chalk.bold("Start the local development node")}
+    \t${chalk.dim("yarn")} chain
+
+    \t${chalk.bold("In a new terminal window, deploy your contracts")}
+    \t${chalk.dim("yarn")} deploy
+    `;
+  }
+
   message += `
-  \n
+  \t${chalk.bold("In a new terminal window, start the frontend")}
+  \t${chalk.dim("yarn")} start
+  `;
+
+  message += `
   ${chalk.bold.green("Thanks for using Scaffold-ETH 2 🙏, Happy Building!")}
   `;
 
