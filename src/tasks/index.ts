edf96e40c5e18bcebcfe216472418240c056afa4
Shiv Bhonde <shivbhonde04@gmail.com>
2023-07-10 11:31:22 +0200

    fix spellling of the file

--- src/tasks/index.ts
+++ src/tasks/index.ts
@@ -1,4 +1,4 @@
 export * from "./copy-template-files";
 export * from "./create-project-directory";
 export * from "./install-packages";
-export * from "./creat-first-git-commit";
+export * from "./create-first-git-commit";
