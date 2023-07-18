b8570bf4eee8862a1c8a41dfc6811aed5eef6225
Daniel Gonzalez Reina <dangonrei@gmail.com>
2023-07-18 12:04:30 +0200

    Update withDefaults API to receive an object

--- templates/utils.js
+++ templates/utils.js
@@ -1,18 +1,19 @@
 export const withDefaults =
-  (template, expectedArgs, debug = false) =>
+  (template, expectedArgsDefaults, debug = false) =>
   (receivedArgs) => {
     const argsWithDefault = Object.fromEntries(
-      expectedArgs.map((argName) => [argName, receivedArgs[argName] ?? []])
+      expectedArgsDefaults.map(([argName, argDefault]) => [argName, receivedArgs[argName] ?? [argDefault]])
     );
 
     if (debug) {
-      console.log(argsWithDefault, expectedArgs, receivedArgs);
+      console.log(argsWithDefault, expectedArgsDefaults, receivedArgs);
     }
 
+    const expectedArgsNames = Object.keys(expectedArgsDefaults)
     Object.keys(receivedArgs).forEach((receivedArgName) => {
-      if (!expectedArgs.includes(receivedArgName)) {
+      if (!expectedArgsNames.includes(receivedArgName)) {
         throw new Error(
-          `Templated received unexpected argument named "${receivedArgName}". Expecting only ${expectedArgs.join(
+          `Templated received unexpected argument named "${receivedArgName}". Expecting only ${expectedArgsNames.join(
             ", "
           )}`
         );
