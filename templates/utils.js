72b3df0c4f1757d418f5040079113931b300100d
Daniel Gonzalez Reina <dangonrei@gmail.com>
2023-07-19 11:42:28 +0200

    Fix buggy implementation

--- templates/utils.js
+++ templates/utils.js
@@ -2,7 +2,8 @@ export const withDefaults =
   (template, expectedArgsDefaults, debug = false) =>
   (receivedArgs) => {
     const argsWithDefault = Object.fromEntries(
-      expectedArgsDefaults.map(([argName, argDefault]) => [argName, receivedArgs[argName] ?? [argDefault]])
+      Object.entries(expectedArgsDefaults)
+      .map(([argName, argDefault]) => [argName, receivedArgs[argName] ?? [argDefault]])
     );
 
     if (debug) {
@@ -13,9 +14,9 @@ export const withDefaults =
     Object.keys(receivedArgs).forEach((receivedArgName) => {
       if (!expectedArgsNames.includes(receivedArgName)) {
         throw new Error(
-          `Templated received unexpected argument named "${receivedArgName}". Expecting only ${expectedArgsNames.join(
-            ", "
-          )}`
+          `Templated received unexpected argument \`${receivedArgName}\`. Expecting only ${
+            expectedArgsNames.map(name => `\`${name}\``).join(", ")
+          }`
         );
       }
     });
