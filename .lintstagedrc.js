e7634abeedee8cd38f7d3ee3e5ce44bd13de6f65
Shiv Bhonde <shivbhonde04@gmail.com>
2023-07-20 23:11:03 +0200

    update gh actions, gitignore, husky, lintstage

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,14 +1,14 @@
 const path = require("path");
 
 const buildNextEslintCommand = (filenames) =>
-  `yarn next:lint --fix --file ${filenames
+  `pnpm next:lint --fix --file ${filenames
     .map((f) => path.relative(path.join("packages", "nextjs"), f))
     .join(" --file ")}`;
 
 const checkTypesNextCommand = () => "yarn next:check-types";
 
 const buildHardhatEslintCommand = (filenames) =>
-  `yarn hardhat:lint-staged --fix ${filenames
+  `pnpm hardhat:lint-staged --fix ${filenames
     .map((f) => path.relative(path.join("packages", "hardhat"), f))
     .join(" ")}`;
 
