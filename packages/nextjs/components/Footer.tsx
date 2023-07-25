a87f1df57236897dd2fd9470c057944ac35612c0
port <schoolreef2@protonmail.com>
2023-07-25 12:29:43 +0300

    Remove z-index from Footer.tsx

--- packages/nextjs/components/Footer.tsx
+++ packages/nextjs/components/Footer.tsx
@@ -15,7 +15,7 @@ export const Footer = () => {
   return (
     <div className="min-h-0 p-5 mb-11 lg:mb-0">
       <div>
-        <div className="fixed flex justify-between items-center w-full z-20 p-4 bottom-0 left-0 pointer-events-none">
+        <div className="fixed flex justify-between items-center w-full p-4 bottom-0 left-0 pointer-events-none">
           <div className="flex space-x-2 pointer-events-auto">
             {nativeCurrencyPrice > 0 && (
               <div className="btn btn-primary btn-sm font-normal cursor-auto">
