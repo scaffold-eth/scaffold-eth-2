e5a24af49e0c94c5f8b976bb03ab1b4e2379b6d1
Rinat <rinat@hey.com>
2023-08-01 10:20:06 +0700

    move disabled to commonInputProps (#426)

--- packages/nextjs/components/scaffold-eth/Input/utils.ts
+++ packages/nextjs/components/scaffold-eth/Input/utils.ts
@@ -3,6 +3,7 @@ export interface CommonInputProps<T = string> {
   onChange: (newValue: T) => void;
   name?: string;
   placeholder?: string;
+  disabled?: boolean;
 }
 
 export enum IntegerVariant {
