cb6209549cec0ef60cb3dc408444698116aee6fd
Rinat <rinat@hey.com>
2023-08-23 14:21:28 +0700

    fix: dropdown styles

--- packages/nextjs/components/scaffold-eth/RainbowKitCustomConnectButton.tsx
+++ packages/nextjs/components/scaffold-eth/RainbowKitCustomConnectButton.tsx
@@ -91,15 +91,18 @@ export const RainbowKitCustomConnectButton = () => {
                       {chain.name}
                     </span>
                   </div>
-                  <div className="dropdown dropdown-end">
-                    <label tabIndex={0} className="btn btn-secondary btn-sm pl-0 pr-2 shadow-md dropdown-toggle gap-0">
+                  <div className="dropdown dropdown-end leading-3">
+                    <label
+                      tabIndex={0}
+                      className="btn btn-secondary btn-sm pl-0 pr-2 shadow-md dropdown-toggle gap-0 !h-auto"
+                    >
                       <BlockieAvatar address={account.address} size={24} ensImage={account.ensAvatar} />
                       <span className="ml-2 mr-1">{account.displayName}</span>
                       <ChevronDownIcon className="h-6 w-4 ml-2 sm:ml-0" />
                     </label>
                     <ul
                       tabIndex={0}
-                      className="dropdown-content menu p-2 mt-1 shadow-center shadow-accent bg-base-200 rounded-box gap-1"
+                      className="dropdown-content menu p-2 mt-2 shadow-center shadow-accent bg-base-200 rounded-box gap-1"
                     >
                       <li>
                         {addressCopied ? (
