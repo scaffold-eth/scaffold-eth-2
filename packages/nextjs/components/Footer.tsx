f429c3340e13e052b88eb3d44cbc84fce367d6ef
Shiv Bhonde <shivbhonde04@gmail.com>
2023-08-24 11:45:27 +0530

    chore: fix index.tsx code for small devices & tweaks to footer

--- packages/nextjs/components/Footer.tsx
+++ packages/nextjs/components/Footer.tsx
@@ -31,7 +31,7 @@ export const Footer = () => {
       <div className="w-full">
         <ul className="menu menu-horizontal w-full">
           <div className="flex justify-center items-center gap-2 text-sm w-full">
-            <div>
+            <div className="text-center">
               <a
                 href="https://github.com/scaffold-eth/se-2"
                 target="_blank"
@@ -43,18 +43,20 @@ export const Footer = () => {
             </div>
             <span>·</span>
             <div>
-              Built with <HeartIcon className="inline-block h-4 w-4" /> at 🏰{" "}
-              <a
-                href="https://buidlguidl.com/"
-                target="_blank"
-                rel="noreferrer"
-                className="underline underline-offset-2"
-              >
-                BuidlGuidl
-              </a>
+              <p className="m-0 text-center">
+                Built with <HeartIcon className="inline-block h-4 w-4" /> at 🏰{" "}
+                <a
+                  href="https://buidlguidl.com/"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="underline underline-offset-2"
+                >
+                  BuidlGuidl
+                </a>
+              </p>
             </div>
             <span>·</span>
-            <div>
+            <div className="text-center">
               <a
                 href="https://t.me/joinchat/KByvmRe5wkR-8F_zz6AjpA"
                 target="_blank"
