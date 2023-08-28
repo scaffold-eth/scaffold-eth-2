8acdae437b1690c80024bb98b3cba45791ba5772
EngrGord <chijokeogbuanya@gmail.com>
2023-08-28 14:49:03 +0100

    update: add optional classNames for modal styling.

--- packages/nextjs/components/TriggerWithModal/Modal.tsx
+++ packages/nextjs/components/TriggerWithModal/Modal.tsx
@@ -5,14 +5,15 @@
 type ModalProps = {
   id: string; // Unique identifier for the modal.
   children: React.ReactNode; // Content to be displayed inside the modal.
+  className?: string; // Optional classNames to be applied to the modal.
 };
 
-const Modal: React.FC<ModalProps> = ({ id, children }) => {
+const Modal: React.FC<ModalProps> = ({ id, children, className }) => {
   return (
     <div role="dialog">
       {/* Checkbox for toggling the modal. */}
       <input type="checkbox" id={`modal-${id}`} className="modal-toggle" />
-      <div className="modal">
+      <div className={`modal ${className || ""}`}>
         <div className="modal-box">
           {/* Close button for the modal. */}
           <label htmlFor={`modal-${id}`} className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
