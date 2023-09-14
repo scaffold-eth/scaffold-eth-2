0dd9d03c7f2423ac10ce1197ee1515013ea4d377
Anurag-Raut <102049482+Anurag-Raut@users.noreply.github.com>
2023-09-14 14:14:05 +0530

    Update RainbowKitCustomConnectButton.tsx

--- packages/nextjs/components/scaffold-eth/RainbowKitCustomConnectButton.tsx
+++ packages/nextjs/components/scaffold-eth/RainbowKitCustomConnectButton.tsx
@@ -40,7 +40,7 @@ export const RainbowKitCustomConnectButton = () => {
             {(() => {
               if (!connected) {
                 return (
-                  <button className="btn btn-primary btn-sm" onClick={()=>{console.log('conn')}} type="button">
+                  <button className="btn btn-primary btn-sm" onClick={openConnectModal} type="button">
                     Connect Wallet
                   </button>
                 );
