3b9b57cf8b4e77ed41f80a251be3a134f2ead5c6
Shiv Bhonde <shivbhonde04@gmail.com>
2023-09-24 23:44:55 +0530

    update abitype ovveride path

--- packages/nextjs/types/abitype/abi.d.ts
+++ packages/nextjs/types/abitype/abi.d.ts
@@ -1,6 +1,6 @@
 import "abitype";
 
-declare module "viem/node_modules/abitype" {
+declare module ".pnpm/viem@1.9.5_typescript@5.2.2/node_modules/abitype" {
   export interface Config {
     AddressType: string;
   }
