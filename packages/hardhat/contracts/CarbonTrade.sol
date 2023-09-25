af57f94b599e99cc1388a51cc2b15002e2c27b6a
Rodrigo Quiroz <quiroz.rodrigo@gmail.com>
2023-09-25 01:50:19 -0400

    Update CarbonTrade.sol
    add apoenzeppelin contract ERC721

--- packages/hardhat/contracts/CarbonTrade.sol
+++ packages/hardhat/contracts/CarbonTrade.sol
@@ -3,7 +3,7 @@
 pragma solidity >=0.8.0 <0.9.0;
 
 
-
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
