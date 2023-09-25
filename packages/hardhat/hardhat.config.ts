86e340a9905d74bb64d26668d8b8c932ef113337
Rodrigo Quiroz <quiroz.rodrigo@gmail.com>
2023-09-25 01:49:20 -0400

    Update hardhat.config.ts
    change sepolia network

--- packages/hardhat/hardhat.config.ts
+++ packages/hardhat/hardhat.config.ts
@@ -26,7 +26,7 @@ const config: HardhatUserConfig = {
       },
     },
   },
-  defaultNetwork: "localhost",
+  defaultNetwork: "sepolia",
   namedAccounts: {
     deployer: {
       // By default, it will take the first Hardhat account as the deployer
