51346e2ad7a8904947de14927f0dfccd0c84cf02
Rinat <rinat@hey.com>
2023-09-30 22:42:18 +0400

    feat: address client component

--- packages/nextjs/app/blockexplorer/address/[address]/page.tsx
+++ packages/nextjs/app/blockexplorer/address/[address]/page.tsx
@@ -1,8 +1,7 @@
+import { AddressClientComponent } from "./address-client-component";
 import fs from "fs";
 import path from "path";
 import { hardhat } from "viem/chains";
-import { BackButton, ContractTabs } from "~~/app/blockexplorer/_components";
-import { Address, Balance } from "~~/components/scaffold-eth";
 import deployedContracts from "~~/generated/deployedContracts";
 import { GenericContractsDeclaration } from "~~/utils/scaffold-eth/contract";
 
@@ -80,29 +79,7 @@ const getContractData = async (address: string) => {
 const AddressPage = async ({ params }: PageProps) => {
   const address = params?.address as string;
   const contractData: { bytecode: string; assembly: string } | null = await getContractData(address);
-  return (
-    <div className="m-10 mb-20">
-      <div className="flex justify-start mb-5">
-        <BackButton />
-      </div>
-      <div className="col-span-5 grid grid-cols-1 lg:grid-cols-2 gap-8 lg:gap-10">
-        <div className="col-span-1 flex flex-col">
-          <div className="bg-base-100 border-base-300 border shadow-md shadow-secondary rounded-3xl px-6 lg:px-8 mb-6 space-y-1 py-4 overflow-x-auto">
-            <div className="flex">
-              <div className="flex flex-col gap-1">
-                <Address address={address} format="long" />
-                <div className="flex gap-1 items-center">
-                  <span className="font-bold text-sm">Balance:</span>
-                  <Balance address={address} className="text" />
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <ContractTabs address={address} contractData={contractData} />
-    </div>
-  );
+  return <AddressClientComponent address={address} contractData={contractData} />;
 };
 
 export default AddressPage;
