baeb69d11b067c8be2ed8c637c492923abdff3a2
Shiv Bhonde <shivbhonde04@gmail.com>
2023-10-07 22:42:51 +0530

    remove forced style from #510

--- packages/nextjs/components/Header.tsx
+++ packages/nextjs/components/Header.tsx
@@ -18,7 +18,7 @@ const NavLink = ({ href, children }: { href: string; children: React.ReactNode }
       passHref
       className={`${
         isActive ? "bg-secondary shadow-md" : ""
-      } hover:bg-secondary hover:shadow-md focus:!bg-secondary active:!text-neutral py-1.5 px-3 text-sm rounded-full gap-2 grid grid-flow-col`}
+      } hover:bg-secondary hover:shadow-md focus:!bg-secondary py-1.5 px-3 text-sm rounded-full gap-2`}
     >
       {children}
     </Link>
