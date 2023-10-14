ee8ddfda310e31c826b62f286af4bcbd43949d82
TwilightLogic <twilightlogic@gmail.com>
2023-10-14 13:48:23 +0800

    Add demo page in navigation bar

--- packages/nextjs/components/Header.tsx
+++ packages/nextjs/components/Header.tsx
@@ -2,7 +2,7 @@ import React, { useCallback, useRef, useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/router";
-import { Bars3Icon, BugAntIcon, MagnifyingGlassIcon, SparklesIcon } from "@heroicons/react/24/outline";
+import { Bars3Icon, BugAntIcon, MagnifyingGlassIcon, SparklesIcon, WalletIcon } from "@heroicons/react/24/outline";
 import { FaucetButton, RainbowKitCustomConnectButton } from "~~/components/scaffold-eth";
 import { useOutsideClick } from "~~/hooks/scaffold-eth";
 
@@ -32,6 +32,7 @@ export const menuLinks: HeaderMenuLink[] = [
     href: "/blockexplorer",
     icon: <MagnifyingGlassIcon className="h-4 w-4" />,
   },
+  { label: "Demo", href: "/demo", icon: <WalletIcon className="h-4 w-4" /> },
 ];
 
 export const HeaderMenuLinks = () => {
