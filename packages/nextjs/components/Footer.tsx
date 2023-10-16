451388afd262f6f00db298e416a47f0cc7ff2e18
Carlos Sánchez <oceanrdn@gmail.com>
2023-10-16 12:32:58 +0200

    Unify spacing on footer buttons

--- packages/nextjs/components/Footer.tsx
+++ packages/nextjs/components/Footer.tsx
@@ -19,12 +19,12 @@ export const Footer = () => {
   return (
     <div className="min-h-0 py-5 px-1 mb-11 lg:mb-0">
       <div>
-        <div className="fixed flex justify-between items-center w-full z-10 p-4 bottom-0 left-0 pointer-events-none ">
+        <div className="fixed flex justify-between items-center w-full z-10 p-4 bottom-0 left-0 pointer-events-none">
           <div className="flex flex-col md:flex-row gap-2 pointer-events-auto">
             {nativeCurrencyPrice > 0 && (
               <div>
-                <div className="btn btn-primary btn-sm font-normal cursor-auto gap-0">
-                  <CurrencyDollarIcon className="h-4 w-4 mr-0.5" />
+                <div className="btn btn-primary btn-sm font-normal normal-case gap-1 cursor-auto">
+                  <CurrencyDollarIcon className="h-4 w-4" />
                   <span>{nativeCurrencyPrice}</span>
                 </div>
               </div>
@@ -32,8 +32,8 @@ export const Footer = () => {
             {isLocalNetwork && (
               <>
                 <Faucet />
-                <Link href="/blockexplorer" passHref className="btn btn-primary btn-sm font-normal gap-0 normal-case">
-                  <MagnifyingGlassIcon className="h-4 w-4 mr-0.5" />
+                <Link href="/blockexplorer" passHref className="btn btn-primary btn-sm font-normal normal-case gap-1">
+                  <MagnifyingGlassIcon className="h-4 w-4" />
                   <span>Block Explorer</span>
                 </Link>
               </>
