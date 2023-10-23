365e56d31cfb4f33ae7cf8dfc58a1a9976f3d9b7
filip <filip.harald@gmail.com>
2023-10-23 20:33:13 +0200

    feat: added InformationCircleIcon

--- packages/nextjs/components/scaffold-eth/Contract/InheritanceTooltip.tsx
+++ packages/nextjs/components/scaffold-eth/Contract/InheritanceTooltip.tsx
@@ -1,3 +1,5 @@
+import { InformationCircleIcon } from "@heroicons/react/24/solid";
+
 export const InheritanceTooltip = ({ inheritedFrom }: { inheritedFrom?: string }) => (
   <>
     {inheritedFrom && (
@@ -5,7 +7,7 @@ export const InheritanceTooltip = ({ inheritedFrom }: { inheritedFrom?: string }
         className="cursor-help tooltip tooltip-top tooltip-secondary px-2 before:content-[attr(data-tip)]"
         data-tip={`Inherited from: ${inheritedFrom}`}
       >
-        *
+        <InformationCircleIcon className="h-4 w-4 text-gray-500" aria-hidden="true" />
       </span>
     )}
   </>
