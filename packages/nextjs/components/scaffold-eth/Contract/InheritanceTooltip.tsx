f51102d2289880082d145f2ddf31ea7342c995b9
filip <filip@kryha.io>
2023-10-26 13:03:44 +0200

    fix: tooltip styles

--- packages/nextjs/components/scaffold-eth/Contract/InheritanceTooltip.tsx
+++ packages/nextjs/components/scaffold-eth/Contract/InheritanceTooltip.tsx
@@ -4,7 +4,7 @@ export const InheritanceTooltip = ({ inheritedFrom }: { inheritedFrom?: string }
   <>
     {inheritedFrom && (
       <span
-        className={"tooltip tooltip-top tooltip-accent px-2 break-normal"}
+        className="tooltip tooltip-top tooltip-accent px-2 md:break-normal"
         data-tip={`Inherited from: ${inheritedFrom}`}
       >
         <InformationCircleIcon className="h-4 w-4" aria-hidden="true" />
