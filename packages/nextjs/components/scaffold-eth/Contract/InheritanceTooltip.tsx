3f1cb8f4944d6519c142d4a37eeb9c4ee978b28d
Filip Harald <Filip.harald@gmail.com>
2023-10-26 10:38:03 +0200

    Update packages/nextjs/components/scaffold-eth/Contract/InheritanceTooltip.tsx
    Co-authored-by: Shiv Bhonde | shivbhonde.eth <shivbhonde04@gmail.com>

--- packages/nextjs/components/scaffold-eth/Contract/InheritanceTooltip.tsx
+++ packages/nextjs/components/scaffold-eth/Contract/InheritanceTooltip.tsx
@@ -1,4 +1,4 @@
-import { InformationCircleIcon } from "@heroicons/react/24/solid";
+import { InformationCircleIcon } from "@heroicons/react/20/solid";
 
 export const InheritanceTooltip = ({ inheritedFrom }: { inheritedFrom?: string }) => (
   <>
