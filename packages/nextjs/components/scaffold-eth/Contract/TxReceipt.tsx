92d07d74bb213575c19acc24d7c2845630e24e1f
Damian <damianmarti@gmail.com>
2023-10-27 12:37:07 -0300

    Add copy to clipboard to TxReceipt

--- packages/nextjs/components/scaffold-eth/Contract/TxReceipt.tsx
+++ packages/nextjs/components/scaffold-eth/Contract/TxReceipt.tsx
@@ -1,9 +1,14 @@
+import { useState } from "react";
+import { CopyToClipboard } from "react-copy-to-clipboard";
 import { TransactionReceipt } from "viem";
+import { CheckCircleIcon, DocumentDuplicateIcon } from "@heroicons/react/24/outline";
 import { displayTxResult } from "~~/components/scaffold-eth";
 
 export const TxReceipt = (
   txResult: string | number | bigint | Record<string, any> | TransactionReceipt | undefined,
 ) => {
+  const [txResultCopied, setTxResultCopied] = useState(false);
+
   return (
     <div className="flex-wrap collapse collapse-arrow mb-2">
       <input type="checkbox" className="min-h-0 peer" />
@@ -11,6 +16,27 @@ export const TxReceipt = (
         <strong>Transaction Receipt</strong>
       </div>
       <div className="collapse-content overflow-auto bg-secondary rounded-t-none rounded-3xl">
+        {txResultCopied ? (
+          <CheckCircleIcon
+            className="ml-1.5 text-xl font-normal text-sky-600 h-5 w-5 cursor-pointer"
+            aria-hidden="true"
+          />
+        ) : (
+          <CopyToClipboard
+            text={displayTxResult(txResult) as string}
+            onCopy={() => {
+              setTxResultCopied(true);
+              setTimeout(() => {
+                setTxResultCopied(false);
+              }, 800);
+            }}
+          >
+            <DocumentDuplicateIcon
+              className="ml-1.5 text-xl font-normal text-sky-600 h-5 w-5 cursor-pointer"
+              aria-hidden="true"
+            />
+          </CopyToClipboard>
+        )}
         <pre className="text-xs pt-4">{displayTxResult(txResult)}</pre>
       </div>
     </div>
