0330e5475a4c18daa39f018f65d3d5527a0a5274
Damian <damianmarti@gmail.com>
2023-10-27 15:49:56 -0300

    Move copy tx receipt icon to title left

--- packages/nextjs/components/scaffold-eth/Contract/TxReceipt.tsx
+++ packages/nextjs/components/scaffold-eth/Contract/TxReceipt.tsx
@@ -10,12 +10,8 @@ export const TxReceipt = (
   const [txResultCopied, setTxResultCopied] = useState(false);
 
   return (
-    <div className="flex-wrap collapse collapse-arrow mb-2">
-      <input type="checkbox" className="min-h-0 peer" />
-      <div className="collapse-title text-sm rounded-3xl peer-checked:rounded-b-none min-h-0 bg-secondary py-1.5">
-        <strong>Transaction Receipt</strong>
-      </div>
-      <div className="collapse-content overflow-auto bg-secondary rounded-t-none rounded-3xl">
+    <div className="flex text-sm rounded-3xl peer-checked:rounded-b-none min-h-0 bg-secondary py-0">
+      <div className="mt-1 pl-2">
         {txResultCopied ? (
           <CheckCircleIcon
             className="ml-1.5 text-xl font-normal text-sky-600 h-5 w-5 cursor-pointer"
@@ -37,7 +33,15 @@ export const TxReceipt = (
             />
           </CopyToClipboard>
         )}
-        <pre className="text-xs pt-4">{displayTxResult(txResult)}</pre>
+      </div>
+      <div className="flex-wrap collapse collapse-arrow">
+        <input type="checkbox" className="min-h-0 peer" />
+        <div className="collapse-title text-sm min-h-0 py-1.5 pl-1">
+          <strong>Transaction Receipt</strong>
+        </div>
+        <div className="collapse-content overflow-auto bg-secondary rounded-t-none rounded-3xl">
+          <pre className="text-xs pt-4">{displayTxResult(txResult)}</pre>
+        </div>
       </div>
     </div>
   );
