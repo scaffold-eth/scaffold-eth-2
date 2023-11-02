61b1c64e20076a7130b7bba2aed3e39bb08dc1b3
Shiv Bhonde | shivbhonde.eth <shivbhonde04@gmail.com>
2023-11-02 15:12:12 +0530

    deployedContracts & externalContracts (#592)

---
+++ packages/nextjs/contracts/externalContracts.ts
@@ -0,0 +1,15 @@
+import { GenericContractsDeclaration } from "~~/utils/scaffold-eth/contract";
+
+/**
+ * @example
+ * const externalContracts = {
+ *   1: {
+ *     DAI: {
+ *      address: "0x...",
+ *      abi: [...],
+ *    }
+ * } as const;
+ */
+const externalContracts = {} as const;
+
+export default externalContracts satisfies GenericContractsDeclaration;
