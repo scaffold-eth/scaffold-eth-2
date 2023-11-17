be119b7525489785b47da7480519b4fb9ee80b00
banasa44 <cfbennassar@gmail.com>
2023-11-17 23:30:35 +0300

    publish + uri func v1

--- packages/hardhat/contracts/SecretFans.sol
+++ packages/hardhat/contracts/SecretFans.sol
@@ -6,15 +6,19 @@ import "hardhat/console.sol";
 
 // Use openzeppelin to inherit battle-tested implementations (ERC20, ERC721, etc)
 // import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
 
 /**
  * A smart contract that allows changing a state variable of the contract and tracking the changes
  * It also allows the owner to withdraw the Ether in the contract
  * @author BuidlGuidl
  */
-contract SecretFans {
+contract SecretFans is ERC1155 {
 	uint256 constant defaultMinSubFee = 0.01 ether;
 	uint256 constant maxSubs = 128;
+	uint256 public currentTokenId = 0;
 
 	struct ContentCreatorChannel {
 		uint256 nSubs;
@@ -25,6 +29,13 @@ contract SecretFans {
 		bytes32[maxSubs * 2 - 1] subsMerkleTree;
 	}
 
+	struct NftRegister {
+		string uri;
+		address contentCreator;
+		bytes32 subsMerkleRoot;
+	}
+
+	mapping(uint256 => NftRegister) NftRegistry;
 	mapping(address => ContentCreatorChannel) Channels;
 	//--------------------------------------------Events------------------------------------------------------
 	event subscription(
@@ -33,6 +44,8 @@ contract SecretFans {
 		bytes32 publicKey,
 		uint256 subsShares
 	);
+
+	event newNFTPublished(address indexed contentCreator, uint256 tokenId);
 
 	function subscribeSpotsAvaliable(
 		address contentCreator,
@@ -55,6 +68,26 @@ contract SecretFans {
 	}
 
 	function subscribeSpotsFull(uint256 _subscriptionFee) public {}
+
+	function publish(
+		string memory uri,
+		bytes calldata encryptedContentEncriptionKeys
+	) public {
+		uint256 _currentTokenId = currentTokenId;
+		// TODO verify ZKP
+		NftRegistry[_currentTokenId] = NftRegister(
+			uri,
+			msg.sender,
+			channels[contentCreator].subsMerkleRoot
+		);
+		//TODO withdraw money form totalETH (transfer, actualize pool)
+		currentTokenId++;
+		emit newNFTPublished(msg.sender, _currentTokenId);
+	}
+
+	function uri(uint256 tokenId) public view override returns (string memory) {
+		return (NftRegistry[tokenId].uri);
+	}
 
 	/**
 	 * Function that allows the contract to receive ETH
