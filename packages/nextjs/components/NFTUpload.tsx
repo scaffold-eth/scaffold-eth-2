b776ed5ee8e0abdd202ceff9fac4a62f6861689a
KramPiotr <piotrek@krambit.pl>
2023-11-19 02:50:48 +0300

    Fix package.json

--- packages/nextjs/components/NFTUpload.tsx
+++ packages/nextjs/components/NFTUpload.tsx
@@ -147,10 +147,10 @@ export default function NFTUpload() {
         display="flex"
         flexDirection="column"
         alignItems="center"
-        justifyContent="center"
+        justifyContent="space-between"
         bg="white"
         border="2px gray solid"
-        style={{ borderRadius: "3px" }}
+        style={{ borderRadius: "3px", height: "385px" }}
         pt={2}
       >
         <Heading style={{ borderRadius: 0 }} textAlign="center" mb={4}>
