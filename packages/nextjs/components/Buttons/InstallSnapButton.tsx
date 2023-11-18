09466597e6e4aff95f3d83a0ff7879bd906f5411
Arnau <arnaubennassar5@gmail.com>
2023-11-18 23:00:15 +0300

    Get public key

--- packages/nextjs/components/Buttons/InstallSnapButton.tsx
+++ packages/nextjs/components/Buttons/InstallSnapButton.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
+import { useEffect, useState } from "react";
 
 export default function InstallSnapButton() {
-    const handleWrite = async () => {
+    const [publicKey, setPublicKey] = useState(null);
+    const install = async () => {
         console.log("Installing");
         const snapId = "local:http://localhost:8080"
         await window.ethereum.request({
@@ -10,14 +12,26 @@ export default function InstallSnapButton() {
                 [snapId]: {},
             },
         });
+        console.log("installed")
+        let response = await window.ethereum.request({
+            method: 'wallet_invokeSnap',
+            params: {
+                snapId,
+                request: { method: 'getPublicKey' }
+            },
+        });
+        console.log("my publick key is: ", response);
+        setPublicKey(response);      
     };
 
     return (
+        
+        
         <div>
             <button
                 className="mb-1 w-56 bg-green-700 text-white active:bg-slate-700 font-bold uppercase text-sm px-4 py-1 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
                 type="button"
-                onClick={handleWrite}
+                onClick={install}
             >
                 Install Snap
             </button>
--- packages/nextjs/pages/index.tsx
+++ packages/nextjs/pages/index.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { Button } from "@chakra-ui/react";
 import type { NextPage } from "next";
 import Carousel from "~~/components/Carousel";
@@ -11,23 +12,29 @@ import InstallSnapButton from "~~/components/Buttons/InstallSnapButton";
 const Home: NextPage = () => {
   const images = ["/slideshow-the-first.jpg", "/slideshow-share-content.jpg", "/slideshow-community.jpg"];
   const loggedIn = false;
-
+  
+  // TODO: Get public key from InstallSnapButton
+  // const [publicKey, setPublicKey] = useState(true);
+  const publicKey = "0x04fcbbf4c8055a8e04271d4e36dec9be5bdfdfe544fc7ccf8b80e71d11b080b09830e1776c66e99ffbe73accfd2d367e9631eac125d5983a6cfa2f4a514eb7c6f5";
   const { data: contract, isLoading: deployedContractLoading } = useDeployedContractInfo("SecretFans");
-
+  console.log({publicKey})
   return (
     <>
       <MetaHeader />
       {
-        !deployedContractLoading && contract && (
+        publicKey && !deployedContractLoading && contract && (
           < SubscribeButton
             smartContract={contract}
             amountETH={BigInt("0x44444444")}
             contentCreatorAddr="0x4838b106fce9647bdf1e7877bf73ce8b0bad5f97"
+            publicKey={publicKey}
           />
         )
       }
-
-      <InstallSnapButton />
+      {publicKey && (
+        <InstallSnapButton />
+      )}
+      
 
 
       {false && !loggedIn && (
