930d312db022965e1e1f0679df198231a3977539
Arnau <arnaubennassar5@gmail.com>
2023-11-18 21:43:31 +0300

    Install snap button

---
+++ packages/nextjs/components/Buttons/InstallSnapButton.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+export default function InstallSnapButton() {
+    const handleWrite = async () => {
+        console.log("Installing");
+        const snapId = "local:http://localhost:8080"
+        await window.ethereum.request({
+            method: 'wallet_requestSnaps',
+            params: {
+                [snapId]: {},
+            },
+        });
+    };
+
+    return (
+        <div>
+            <button
+                className="mb-1 w-56 bg-green-700 text-white active:bg-slate-700 font-bold uppercase text-sm px-4 py-1 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                type="button"
+                onClick={handleWrite}
+            >
+                Install Snap
+            </button>
+
+        </div>
+    )
+}
