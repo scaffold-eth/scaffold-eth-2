085a5ac60615a7d77075cfa0b42804d378c5bdd3
DannyThomx <dannithomx@gmail.com>
2023-11-18 11:27:39 +0100

    Refactored ContractUI.tsx to use { configuredNetwork } from useScaffoldConfig hook instead of getTargetNetwork()

--- packages/nextjs/components/scaffold-eth/Contract/ContractUI.tsx
+++ packages/nextjs/components/scaffold-eth/Contract/ContractUI.tsx
@@ -4,8 +4,8 @@ import { ContractVariables } from "./ContractVariables";
 import { ContractWriteMethods } from "./ContractWriteMethods";
 import { Spinner } from "~~/components/assets/Spinner";
 import { Address, Balance } from "~~/components/scaffold-eth";
+import { useScaffoldConfig } from "~~/context/ScaffoldConfigContext";
 import { useDeployedContractInfo, useNetworkColor } from "~~/hooks/scaffold-eth";
-import { getTargetNetwork } from "~~/utils/scaffold-eth";
 import { ContractName } from "~~/utils/scaffold-eth/contract";
 
 type ContractUIProps = {
@@ -18,8 +18,7 @@ type ContractUIProps = {
  **/
 export const ContractUI = ({ contractName, className = "" }: ContractUIProps) => {
   const [refreshDisplayVariables, triggerRefreshDisplayVariables] = useReducer(value => !value, false);
-  const configuredNetwork = getTargetNetwork();
-
+  const { configuredNetwork } = useScaffoldConfig();
   const { data: deployedContractData, isLoading: deployedContractLoading } = useDeployedContractInfo(contractName);
   const networkColor = useNetworkColor();
 
