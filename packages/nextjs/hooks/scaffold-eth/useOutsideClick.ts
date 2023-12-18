4b737b594e65216499a5cc4158ac3d72ad5cc0db
Shiv Bhonde <shivbhonde04@gmail.com>
2023-12-18 10:59:48 +0530

    update comment for useOutsideClick hook

--- packages/nextjs/hooks/scaffold-eth/useOutsideClick.ts
+++ packages/nextjs/hooks/scaffold-eth/useOutsideClick.ts
@@ -1,7 +1,9 @@
 import React, { useEffect } from "react";
 
 /**
- * Checks if a click was made outside of passed ref
+ * Handles clicks outside of passed ref element
+ * @param ref - react ref of the element
+ * @param callback - callback function to call when clicked outside
  */
 export const useOutsideClick = (ref: React.RefObject<HTMLElement>, callback: { (): void }) => {
   useEffect(() => {
