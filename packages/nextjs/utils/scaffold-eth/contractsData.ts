34f196ab17abae1a2ae9d418eb3931236fa8ff9a
Shiv Bhonde | shivbhonde.eth <shivbhonde04@gmail.com>
2023-12-29 23:11:27 +0530

    remove log from contractsData (#675)

--- packages/nextjs/utils/scaffold-eth/contractsData.ts
+++ packages/nextjs/utils/scaffold-eth/contractsData.ts
@@ -3,6 +3,5 @@ import { contracts } from "~~/utils/scaffold-eth/contract";
 
 export function getAllContracts() {
   const contractsData = contracts?.[scaffoldConfig.targetNetworks[0].id];
-  console.log("The contracts data is:", contractsData);
   return contractsData ? contractsData : {};
 }
