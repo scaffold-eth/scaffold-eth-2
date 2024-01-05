096e9640da6e851ecbaa016be0522cc8d5908812
Shiv Bhonde <shivbhonde04@gmail.com>
2024-01-06 00:31:18 +0530

    remove zksync packages and @typechain/ethers-v6

--- packages/hardhat/hardhat.config.ts
+++ packages/hardhat/hardhat.config.ts
@@ -9,7 +9,6 @@ import "solidity-coverage";
 import "@nomicfoundation/hardhat-verify";
 import "hardhat-deploy";
 import "hardhat-deploy-ethers";
-import "@matterlabs/hardhat-zksync-solc";
 
 // If not set, it uses ours Alchemy's default API key.
 // You can get your own at https://dashboard.alchemyapi.io
