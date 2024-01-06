9c3cca6038b3d9c4bf92e21c963b28d7a9c263b8
Shiv Bhonde <shivbhonde04@gmail.com>
2024-01-06 23:56:26 +0530

    add new line b/w 9-10 in layout.tsx

--- packages/nextjs/app/layout.tsx
+++ packages/nextjs/app/layout.tsx
@@ -7,6 +7,7 @@ const baseUrl = process.env.NEXT_PUBLIC_VERCEL_URL
   ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
   : `http://localhost:${process.env.PORT}`;
 const imageUrl = `${baseUrl}/thumbnail.jpg`;
+
 export const metadata: Metadata = {
   metadataBase: new URL(baseUrl),
   title: {
