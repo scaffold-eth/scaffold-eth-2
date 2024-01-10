baa1de40b95fd5dadcf72e689b25c95dc6cbd0c8
Didier <didier.moet@gmail.com>
2024-01-09 19:11:23 -0600

    Update [address].tsx

--- packages/nextjs/pages/blockexplorer/address/[address].tsx
+++ packages/nextjs/pages/blockexplorer/address/[address].tsx
@@ -57,6 +57,9 @@ const AddressPage = ({ address, contractData }: PageProps) => {
   );
 
   return (
+
+    
+
     <div className="m-10 mb-20">
       <div className="flex justify-start mb-5">
         <button className="btn btn-sm btn-primary" onClick={() => router.back()}>
@@ -65,13 +68,13 @@ const AddressPage = ({ address, contractData }: PageProps) => {
       </div>
       <div className="col-span-5 grid grid-cols-1 lg:grid-cols-2 gap-8 lg:gap-10">
         <div className="col-span-1 flex flex-col">
-          <div className="bg-base-100 border-base-300 border shadow-md shadow-secondary rounded-3xl px-6 lg:px-8 mb-6 space-y-1 py-4 overflow-x-auto">
+          <div className="bg-base-100 border-secondary border shadow-md shadow-secondary rounded px-6 lg:px-8 mb-6 space-y-1 py-4 overflow-x-auto">
             <div className="flex">
               <div className="flex flex-col gap-1">
                 <Address address={address} format="long" />
                 <div className="flex gap-1 items-center">
-                  <span className="font-bold text-sm">Balance:</span>
-                  <Balance address={address} className="text" />
+                  <span className="text-xs">Balance:</span>
+                  <Balance address={address} className="text font-mono text-lg" />
                 </div>
               </div>
             </div>
@@ -90,12 +93,12 @@ const AddressPage = ({ address, contractData }: PageProps) => {
             Code
           </button>
           <button
-            className={`tab  ${activeTab === "storage" ? "tab-active" : ""}`}
+            className={`tab ${activeTab === "storage" ? "tab-active" : ""}`}
             onClick={() => setActiveTab("storage")}
           >
             Storage
           </button>
-          <button className={`tab  ${activeTab === "logs" ? "tab-active" : ""}`} onClick={() => setActiveTab("logs")}>
+          <button className={`tab ${activeTab === "logs" ? "tab-active" : ""}`} onClick={() => setActiveTab("logs")}>
             Logs
           </button>
         </div>
@@ -116,6 +119,8 @@ const AddressPage = ({ address, contractData }: PageProps) => {
       {activeTab === "storage" && <AddressStorageTab address={address} />}
       {activeTab === "logs" && <AddressLogsTab address={address} />}
     </div>
+
+    
   );
 };
 
