66d8354a7c92ccf406b1c03e8c575710fdb752dd
Didier <didier.moet@gmail.com>
2024-01-09 18:50:28 -0600

    Update Footer.tsx

--- packages/nextjs/components/Footer.tsx
+++ packages/nextjs/components/Footer.tsx
@@ -18,6 +18,7 @@ export const Footer = () => {
   const isLocalNetwork = targetNetwork.id === hardhat.id;
 
   return (
+    
     <div className="min-h-0 py-5 px-1 mb-11 lg:mb-0">
       <div>
         <div className="fixed flex justify-between items-center w-full z-10 p-4 bottom-0 left-0 pointer-events-none">
@@ -76,5 +77,6 @@ export const Footer = () => {
         </ul>
       </div>
     </div>
+    
   );
 };
