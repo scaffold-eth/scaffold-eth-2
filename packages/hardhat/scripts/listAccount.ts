5efa23943cda2fe92b7319ed31e9b9e127e2189a
Shiv Bhonde | shivbhonde.eth <shivbhonde04@gmail.com>
2024-01-17 00:30:07 +0530

    Ethers v6 migration in hardhat (#692)
    Co-authored-by: Carlos Sánchez <oceanrdn@gmail.com>

--- packages/hardhat/scripts/listAccount.ts
+++ packages/hardhat/scripts/listAccount.ts
@@ -24,10 +24,11 @@ async function main() {
     try {
       const network = availableNetworks[networkName];
       if (!("url" in network)) continue;
-      const provider = new ethers.providers.JsonRpcProvider(network.url);
+      const provider = new ethers.JsonRpcProvider(network.url);
+      await provider._detectNetwork();
       const balance = await provider.getBalance(address);
       console.log("--", networkName, "-- 📡");
-      console.log("   balance:", +ethers.utils.formatEther(balance));
+      console.log("   balance:", +ethers.formatEther(balance));
       console.log("   nonce:", +(await provider.getTransactionCount(address)));
     } catch (e) {
       console.log("Can't connect to network", networkName);
