5cc01658371db37d3b242e03e4cb05e9c3cd6b5c
Kevin J <6829515+kmjones1979@users.noreply.github.com>
2024-02-08 10:54:10 -0800

    attempt at app-router update for subgraph-package

---
+++ packages/nextjs/app/subgraph/page.tsx
@@ -0,0 +1,112 @@
+import Link from "next/link";
+// import { gql, useQuery } from "@apollo/client";
+import type { NextPage } from "next";
+import { MagnifyingGlassIcon, PlusIcon, PowerIcon, RocketLaunchIcon } from "@heroicons/react/24/outline";
+
+// const GREETINGS_GRAPHQL = `
+// {
+//   greetings(first: 25, orderBy: createdAt, orderDirection: desc) {
+//     id
+//     greeting
+//     premium
+//     value
+//     createdAt
+//     sender {
+//       address
+//       greetingCount
+//     }
+//   }
+// }
+// `;
+
+// const GREETINGS_GQL = gql(GREETINGS_GRAPHQL);
+// const greetingsData = useQuery(GREETINGS_GQL, { pollInterval: 1000 });
+
+// console.log("greetingsData: ", greetingsData);
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <div>
+        <div className="flex items-center flex-col flex-grow pt-10">
+          <h1 className="text-center mb-8">
+            <span className="block text-2xl mb-2">Welcome to your</span>
+            <span className="block text-4xl font-bold">Subgraph</span>
+          </h1>
+          <p className="text-center text-lg">
+            Look at the subgraph manifest defined in{" "}
+            <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
+              packages/subgraph/subgraph.yaml
+            </code>
+          </p>
+          <p className="text-center text-lg">
+            Examine the entities in{" "}
+            <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
+              schema.graphql
+            </code>{" "}
+            located in{" "}
+            <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
+              packages/subgraph/src
+            </code>
+          </p>
+          <p className="text-center text-lg">
+            Data is processed using AssemblyScript Mappings in{" "}
+            <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
+              packages/subgraph/src/mapping.ts
+            </code>
+          </p>
+        </div>
+        <div className="flex-grow bg-base-300 w-full mt-16 px-8 py-12">
+          <div className="flex justify-center items-center gap-12 flex-col sm:flex-row">
+            <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
+              <PowerIcon className="h-8 w-8 fill-secondary" />
+              <p className="text-center text-lg">
+                Start your subgraph environment using{" "}
+                <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
+                  yarn run-node
+                </code>
+              </p>
+            </div>
+            <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
+              <PlusIcon className="h-8 w-8 fill-secondary" />
+              <p className="text-center text-lg">
+                Create your subgraph on graph-node with{" "}
+                <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
+                  yarn local-create
+                </code>
+              </p>
+            </div>
+            <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
+              <RocketLaunchIcon className="h-8 w-8 fill-secondary" />
+              <p className="text-center text-lg">
+                Deploy your subgraph configuration with{" "}
+                <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
+                  yarn local-ship
+                </code>
+              </p>
+            </div>
+          </div>{" "}
+        </div>
+
+        <div className="flex items-center flex-col flex-grow pt-10">
+          <p>
+            Explore data in your local graph-node using the built in{" "}
+            <Link
+              href="http://localhost:8000/subgraphs/name/scaffold-eth/your-contract/graphql"
+              passHref
+              className="link"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              GraphiQL
+            </Link>{" "}
+            tool.
+          </p>
+          <MagnifyingGlassIcon className="h-8 w-8 fill-secondary" />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
--- packages/nextjs/components/Header.tsx
+++ packages/nextjs/components/Header.tsx
@@ -4,7 +4,7 @@ import React, { useCallback, useRef, useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { Bars3Icon, BugAntIcon } from "@heroicons/react/24/outline";
+import { Bars3Icon, BugAntIcon, MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 import { FaucetButton, RainbowKitCustomConnectButton } from "~~/components/scaffold-eth";
 import { useOutsideClick } from "~~/hooks/scaffold-eth";
 
@@ -23,6 +23,11 @@ export const menuLinks: HeaderMenuLink[] = [
     label: "Debug Contracts",
     href: "/debug",
     icon: <BugAntIcon className="h-4 w-4" />,
+  },
+  {
+    label: "Subgraph",
+    href: "/subgraph",
+    icon: <MagnifyingGlassIcon className="h-4 w-4" />,
   },
 ];
 
