b64c3e0c6fc48002cc7c2d32323157049ae81762
Shiv Bhonde <shivbhonde04@gmail.com>
2024-02-09 19:21:53 +0530

    rename darkTheme as dark

--- packages/nextjs/tailwind.config.js
+++ packages/nextjs/tailwind.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   content: ["./app/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}", "./utils/**/*.{js,ts,jsx,tsx}"],
   plugins: [require("daisyui")],
-  darkTheme: "scaffoldEthDark",
+  darkTheme: "dark",
   // DaisyUI theme colors
   daisyui: {
     themes: [
