1394397f3f1f738573d6053ec25e029c5337aecc
Shiv Bhonde <shivbhonde04@gmail.com>
2024-02-10 23:57:51 +0530

    add better structs to ReadOnlyFunctionForm too

--- packages/hardhat/contracts/YourContract.sol
+++ packages/hardhat/contracts/YourContract.sol
@@ -23,7 +23,27 @@ contract YourContract {
 		sData = _nestedStruct; // Assigns the entire struct. For dynamic arrays, you might need more complex logic.
 	}
 
-	// Function to get the current data
+	// Read function which accepts _nestedStruct
+	function totalPassedStruct(
+		NestedStruct calldata _nestedStruct
+	) public pure returns (uint totalA, uint totalX, uint totalY) {
+		totalA = _nestedStruct.a;
+		uint totalXSum = 0;
+		uint totalYSum = 0;
+
+		for (uint i = 0; i < _nestedStruct.b.length; i++) {
+			for (uint j = 0; j < _nestedStruct.b[i].length; j++) {
+				for (uint k = 0; k < _nestedStruct.b[i][j].length; k++) {
+					totalXSum += _nestedStruct.b[i][j][k].x;
+					totalYSum += _nestedStruct.b[i][j][k].y;
+				}
+			}
+		}
+
+		return (totalA, totalXSum, totalYSum);
+	}
+
+	// Function to get the current datahe current data
 	function geAllSData() public view returns (NestedStruct memory) {
 		return (sData);
 	}
