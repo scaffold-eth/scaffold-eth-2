9c9e56efe491924a2edd7e627ecf0294a1e90058
Carlos Sánchez <oceanrdn@gmail.com>
2024-02-15 09:08:31 +0100

    Better color for struct titles

--- packages/nextjs/app/debug/_components/contract/Tuple.tsx
+++ packages/nextjs/app/debug/_components/contract/Tuple.tsx
@@ -29,7 +29,7 @@ export const Tuple = ({ abiTupleParameter, setParentForm, parentStateObjectKey }
     <div>
       <div className="collapse collapse-arrow bg-base-200 pl-2 py-1.5 border-2 border-secondary">
         <input type="checkbox" className="min-h-fit peer" />
-        <div className="collapse-title p-0 min-h-fit peer-checked:mb-2 text-accent">
+        <div className="collapse-title p-0 min-h-fit peer-checked:mb-2 text-primary-content/50">
           <p className="m-0 p-0 text-[1rem]">{abiTupleParameter.internalType}</p>
         </div>
         <div className="ml-3 flex-col space-y-4 border-secondary/80 border-l-2 pl-4 collapse-content">
--- packages/nextjs/app/debug/_components/contract/TupleArray.tsx
+++ packages/nextjs/app/debug/_components/contract/TupleArray.tsx
@@ -99,7 +99,7 @@ export const TupleArray = ({ abiTupleParameter, setParentForm, parentStateObject
     <div>
       <div className="collapse collapse-arrow bg-base-200 pl-2 py-1.5 border-2 border-secondary">
         <input type="checkbox" className="min-h-fit peer" />
-        <div className="collapse-title p-0 min-h-fit peer-checked:mb-1 text-accent">
+        <div className="collapse-title p-0 min-h-fit peer-checked:mb-1 text-primary-content/50">
           <p className="m-0 text-[1rem]">{abiTupleParameter.internalType}</p>
         </div>
         <div className="ml-3 flex-col space-y-2 border-secondary/70 border-l-2 pl-4 collapse-content">
