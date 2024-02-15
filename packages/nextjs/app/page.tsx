f20d6b772d2278bad702c942baf888172ca3c1f7
Kevin J <6829515+kmjones1979@users.noreply.github.com>
2024-02-15 12:11:46 -0800

    Subgraph package updates from se2 main (#724)
    * Add Optimism Sepolia config (#711)
    
    * Use arbitrumSepolia instead of Goerli (#716)
    
    * up rainbowkit version to 1.3.5 (#719)
    
    * removing lock file
    
    * use next-themes to handle theme and update usehooks-ts (#707)
    
    * Feat: Better complex struct inputs (#702)
    
    * improve debug struct UI (#726)
    
    * add basic example to show connected address (#721)
    
    ---------
    
    Co-authored-by: winnsterx <46658657+winnsterx@users.noreply.github.com>
    Co-authored-by: Shiv Bhonde | shivbhonde.eth <shivbhonde04@gmail.com>
    Co-authored-by: Carlos Sánchez <oceanrdn@gmail.com>

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -1,16 +1,26 @@
+"use client";
+
 import Link from "next/link";
 import type { NextPage } from "next";
+import { useAccount } from "wagmi";
 import { BugAntIcon, MagnifyingGlassIcon } from "@heroicons/react/24/outline";
+import { Address } from "~~/components/scaffold-eth";
 
 const Home: NextPage = () => {
+  const { address: connectedAddress } = useAccount();
+
   return (
     <>
       <div className="flex items-center flex-col flex-grow pt-10">
         <div className="px-5">
-          <h1 className="text-center mb-8">
+          <h1 className="text-center">
             <span className="block text-2xl mb-2">Welcome to</span>
             <span className="block text-4xl font-bold">Scaffold-ETH 2</span>
           </h1>
+          <div className="flex justify-center items-center space-x-2">
+            <p className="my-2 font-medium">Connected Address:</p>
+            <Address address={connectedAddress} />
+          </div>
           <p className="text-center text-lg">
             Get started by editing{" "}
             <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
--- packages/nextjs/components/ScaffoldEthAppWithProviders.tsx
+++ packages/nextjs/components/ScaffoldEthAppWithProviders.tsx
@@ -1,8 +1,9 @@
 "use client";
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 import { RainbowKitProvider, darkTheme, lightTheme } from "@rainbow-me/rainbowkit";
+import { useTheme } from "next-themes";
 import { Toaster } from "react-hot-toast";
 import { WagmiConfig } from "wagmi";
 import { Footer } from "~~/components/Footer";
@@ -10,7 +11,6 @@ import { Header } from "~~/components/Header";
 import { BlockieAvatar } from "~~/components/scaffold-eth";
 import { ProgressBar } from "~~/components/scaffold-eth/ProgressBar";
 import { useNativeCurrencyPrice } from "~~/hooks/scaffold-eth";
-import { useDarkMode } from "~~/hooks/scaffold-eth/useDarkMode";
 import { useGlobalState } from "~~/services/store/store";
 import { wagmiConfig } from "~~/services/web3/wagmiConfig";
 import { appChains } from "~~/services/web3/wagmiConnectors";
@@ -38,7 +38,13 @@ const ScaffoldEthApp = ({ children }: { children: React.ReactNode }) => {
 };
 
 export const ScaffoldEthAppWithProviders = ({ children }: { children: React.ReactNode }) => {
-  const { isDarkMode } = useDarkMode();
+  const { resolvedTheme } = useTheme();
+  const isDarkMode = resolvedTheme === "dark";
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
 
   const subgraphUri = "http://localhost:8000/subgraphs/name/scaffold-eth/your-contract";
   const apolloClient = new ApolloClient({
@@ -53,7 +59,7 @@ export const ScaffoldEthAppWithProviders = ({ children }: { children: React.Reac
         <RainbowKitProvider
           chains={appChains.chains}
           avatar={BlockieAvatar}
-          theme={isDarkMode ? darkTheme() : lightTheme()}
+          theme={mounted ? (isDarkMode ? darkTheme() : lightTheme()) : lightTheme()}
         >
           <ScaffoldEthApp>{children}</ScaffoldEthApp>
         </RainbowKitProvider>
