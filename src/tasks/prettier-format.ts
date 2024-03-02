e47cdddc1ee1d9237408e3bced44695314084c29
Shiv Bhonde <shivbhonde04@gmail.com>
2024-03-02 16:35:58 +0530

    create a new for format with prettier

---
+++ src/tasks/prettier-format.ts
@@ -0,0 +1,15 @@
+import { execa } from "execa";
+
+export async function prettierFormat(targetDir: string) {
+  try {
+    const result = await execa("yarn", ["format"], { cwd: targetDir });
+
+    if (result.failed) {
+      throw new Error("There was a problem running the format commend");
+    }
+  } catch (error) {
+    throw new Error("Failed to create directory", { cause: error });
+  }
+
+  return true;
+}
