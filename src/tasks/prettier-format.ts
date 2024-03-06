a5e61924c9eacc370450ac6ed393fb7c4c7f2e23
Shiv Bhonde <shivbhonde04@gmail.com>
2024-03-06 13:29:42 +0530

    fix typo

--- src/tasks/prettier-format.ts
+++ src/tasks/prettier-format.ts
@@ -5,7 +5,7 @@ export async function prettierFormat(targetDir: string) {
     const result = await execa("yarn", ["format"], { cwd: targetDir });
 
     if (result.failed) {
-      throw new Error("There was a problem running the format commend");
+      throw new Error("There was a problem running the format command");
     }
   } catch (error) {
     throw new Error("Failed to create directory", { cause: error });
