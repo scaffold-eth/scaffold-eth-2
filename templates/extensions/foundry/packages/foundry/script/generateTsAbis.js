5c5c0b53786b52092a646fe32b61692353af054c
Jacob Homanics <homanicsjake@gmail.com>
2024-03-15 18:52:13 -0500

    updated lint.yaml and fixed an error in generateTsAbis

--- templates/extensions/foundry/packages/foundry/script/generateTsAbis.js
+++ templates/extensions/foundry/packages/foundry/script/generateTsAbis.js
@@ -36,15 +36,19 @@ function getArtifactOfContract(contractName) {
 
 function getInheritedFromContracts(artifact) {
   let inheritedFromContracts = [];
-  for (const astNode of artifact.ast.nodes) {
-    if (astNode.nodeType == "ContractDefinition") {
-      if (astNode.baseContracts.length > 0) {
-        inheritedFromContracts = astNode.baseContracts.map(
-          ({ baseName }) => baseName.name
-        );
+
+  if (artifact.ast) {
+    for (const astNode of artifact.ast.nodes) {
+      if (astNode.nodeType == "ContractDefinition") {
+        if (astNode.baseContracts.length > 0) {
+          inheritedFromContracts = astNode.baseContracts.map(
+            ({ baseName }) => baseName.name
+          );
+        }
       }
     }
   }
+  
   return inheritedFromContracts;
 }
 
