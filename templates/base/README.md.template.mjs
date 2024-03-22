0ff3c4cef4f59274b44baad8bed1bf7023a5a04a
Shiv Bhonde <shivbhonde04@gmail.com>
2024-03-23 02:08:58 +0530

    use zero'th index for quickStart templating

--- templates/base/README.md.template.mjs
+++ templates/base/README.md.template.mjs
@@ -33,7 +33,7 @@ Before you begin, you need to install the following tools:
 - Yarn ([v1](https://classic.yarnpkg.com/en/docs/install/) or [v2+](https://yarnpkg.com/getting-started/install))
 - [Git](https://git-scm.com/downloads)
 
-${quickStart}
+${quickStart[0]}
 
 ## Documentation
 
