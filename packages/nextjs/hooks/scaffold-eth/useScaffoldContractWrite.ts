ed5c282492207d7b75c4e72f1f6ad23f5e4c9354
Shiv Bhonde <shivbhonde04@gmail.com>
2024-03-27 03:29:35 +0530

    update useSCWrite comments

--- packages/nextjs/hooks/scaffold-eth/useScaffoldContractWrite.ts
+++ packages/nextjs/hooks/scaffold-eth/useScaffoldContractWrite.ts
@@ -15,15 +15,10 @@ import {
 } from "~~/utils/scaffold-eth/contract";
 
 /**
- * Wrapper around wagmi's useContractWrite hook which automatically loads (by name) the contract ABI and address from
+ * Wrapper around wagmi's useWriteContract hook which automatically loads (by name) the contract ABI and address from
  * the contracts present in deployedContracts.ts & externalContracts.ts corresponding to targetNetworks configured in scaffold.config.ts
- * @param config - The config settings, including extra wagmi configuration
- * @param config.contractName - contract name
- * @param config.functionName - name of the function to be called
- * @param config.args - arguments for the function
- * @param config.value - value in ETH that will be sent with transaction
- * @param config.blockConfirmations - number of block confirmations to wait for (default: 1)
- * @param config.onBlockConfirmation - callback that will be called after blockConfirmations.
+ * @param contractName - contract name
+ * @param writeContractParams - wagmi's useWriteContract parameters
  */
 export const useScaffoldWriteContract = <TContractName extends ContractName>(
   contractName: TContractName,
