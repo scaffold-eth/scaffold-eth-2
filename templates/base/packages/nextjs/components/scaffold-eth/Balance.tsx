686f13d89d0b824f099f1334ec97d95ebcde6e4f
Jacob Homanics <homanicsjake@gmail.com>
2024-03-27 21:24:55 -0500

    removed useAccountBalance

--- templates/base/packages/nextjs/components/scaffold-eth/Balance.tsx
+++ templates/base/packages/nextjs/components/scaffold-eth/Balance.tsx
@@ -2,8 +2,9 @@
 
 import { useState } from "react";
 import { Address } from "viem";
-import { useAccountBalance } from "~~/hooks/scaffold-eth";
+import { useBalance } from "wagmi";
 import { useTargetNetwork } from "~~/hooks/scaffold-eth/useTargetNetwork";
+import { useGlobalState } from "~~/services/store/store";
 
 type BalanceProps = {
   address?: Address;
@@ -16,7 +17,17 @@ type BalanceProps = {
  */
 export const Balance = ({ address, className = "", usdMode }: BalanceProps) => {
   const { targetNetwork } = useTargetNetwork();
-  const { balance, price, isError, isLoading } = useAccountBalance(address);
+
+  const price = useGlobalState(state => state.nativeCurrencyPrice);
+  const {
+    data: balance,
+    isError,
+    isLoading,
+  } = useBalance({
+    address,
+    watch: true,
+  });
+
   const [displayUsdMode, setDisplayUsdMode] = useState(price > 0 ? Boolean(usdMode) : false);
 
   const toggleBalanceMode = () => {
@@ -44,6 +55,8 @@ export const Balance = ({ address, className = "", usdMode }: BalanceProps) => {
     );
   }
 
+  const formattedBalance = balance ? Number(balance.formatted) : 0;
+
   return (
     <button
       className={`btn btn-sm btn-ghost flex flex-col font-normal items-center hover:bg-transparent ${className}`}
@@ -53,11 +66,11 @@ export const Balance = ({ address, className = "", usdMode }: BalanceProps) => {
         {displayUsdMode ? (
           <>
             <span className="text-[0.8em] font-bold mr-1">$</span>
-            <span>{(balance * price).toFixed(2)}</span>
+            <span>{(formattedBalance * price).toFixed(2)}</span>
           </>
         ) : (
           <>
-            <span>{balance?.toFixed(4)}</span>
+            <span>{formattedBalance.toFixed(4)}</span>
             <span className="text-[0.8em] font-bold ml-1">{targetNetwork.nativeCurrency.symbol}</span>
           </>
         )}
