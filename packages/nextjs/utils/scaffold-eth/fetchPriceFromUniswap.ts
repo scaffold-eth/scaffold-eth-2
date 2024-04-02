7674eaed025e401d0d9b7382c9871dffa4b650c7
Shiv Bhonde <shivbhonde04@gmail.com>
2024-04-02 17:27:56 +0530

    use getAlchemyHttpUrl in fetchPriceFromUniwswap with realtive import

--- packages/nextjs/utils/scaffold-eth/fetchPriceFromUniswap.ts
+++ packages/nextjs/utils/scaffold-eth/fetchPriceFromUniswap.ts
@@ -1,13 +1,12 @@
+import { ChainWithAttributes, getAlchemyHttpUrl } from "./networks";
 import { CurrencyAmount, Token } from "@uniswap/sdk-core";
 import { Pair, Route } from "@uniswap/v2-sdk";
 import { Address, createPublicClient, http, parseAbi } from "viem";
 import { mainnet } from "viem/chains";
-import scaffoldConfig from "~~/scaffold.config";
-import { ChainWithAttributes } from "~~/utils/scaffold-eth";
 
 const publicClient = createPublicClient({
   chain: mainnet,
-  transport: http(`https://eth-mainnet.g.alchemy.com/v2/${scaffoldConfig.alchemyApiKey}`),
+  transport: http(getAlchemyHttpUrl(mainnet.id)),
 });
 
 const ABI = parseAbi([
