cb47ae904651fa68de1a9aa3e24153368aeee7e8
Carlos Sánchez <oceanrdn@gmail.com>
2024-04-16 11:55:55 +0200

    Use metadata on app/layout

--- packages/nextjs/app/layout.tsx
+++ packages/nextjs/app/layout.tsx
@@ -1,57 +1,13 @@
 import "@rainbow-me/rainbowkit/styles.css";
-import { Metadata } from "next";
 import { ScaffoldEthAppWithProviders } from "~~/components/ScaffoldEthAppWithProviders";
 import { ThemeProvider } from "~~/components/ThemeProvider";
 import "~~/styles/globals.css";
+import { getMetadata } from "~~/utils/scaffold-eth/getMetadata";
 
-const baseUrl = process.env.VERCEL_URL
-  ? `https://${process.env.VERCEL_URL}`
-  : `http://localhost:${process.env.PORT || 3000}`;
-const imageUrl = `${baseUrl}/thumbnail.jpg`;
-
-const title = "Scaffold-ETH 2 App";
-const titleTemplate = "%s | Scaffold-ETH 2";
-const description = "Built with 🏗 Scaffold-ETH 2";
-
-export const metadata: Metadata = {
-  metadataBase: new URL(baseUrl),
-  title: {
-    default: title,
-    template: titleTemplate,
-  },
-  description,
-  openGraph: {
-    title: {
-      default: title,
-      template: titleTemplate,
-    },
-    description,
-    images: [
-      {
-        url: imageUrl,
-      },
-    ],
-  },
-  twitter: {
-    card: "summary_large_image",
-    images: [imageUrl],
-    title: {
-      default: title,
-      template: titleTemplate,
-    },
-    description,
-  },
-  icons: {
-    icon: [{ url: "/favicon.png", sizes: "32x32", type: "image/png" }],
-  },
-  other: {
-    "fc:frame": "vNext",
-    "fc:frame:image": imageUrl,
-    "fc:frame:button:1": "Open my 🏗️ Scaffold-ETH 2 dApp",
-    "fc:frame:button:1:action": "link",
-    "fc:frame:button:1:target": baseUrl,
-  },
-};
+export const metadata = getMetadata({
+  title: "Scaffold-ETH 2 App",
+  description: "Built with 🏗 Scaffold-ETH 2",
+});
 
 const ScaffoldEthApp = ({ children }: { children: React.ReactNode }) => {
   return (
