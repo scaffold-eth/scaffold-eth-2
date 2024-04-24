aeb45c92500c6dc58f506c297dbf76b86c7e3b4e
Shiv Bhonde <shivbhonde04@gmail.com>
2024-04-24 23:29:33 +0530

    use isZeroAddress util and minor tweak to comment

--- packages/nextjs/app/blockexplorer/address/[address]/page.tsx
+++ packages/nextjs/app/blockexplorer/address/[address]/page.tsx
@@ -3,6 +3,7 @@ import path from "path";
 import { hardhat } from "viem/chains";
 import { AddressComponent } from "~~/app/blockexplorer/_components/AddressComponent";
 import deployedContracts from "~~/contracts/deployedContracts";
+import { isZeroAddress } from "~~/utils/scaffold-eth/common";
 import { GenericContractsDeclaration } from "~~/utils/scaffold-eth/contract";
 
 type PageProps = {
@@ -77,13 +78,14 @@ const getContractData = async (address: string) => {
 };
 
 export function generateStaticParams() {
-  // An workaround to enable static exports in Next.js
+  // An workaround to enable static exports in Next.js, generating single dummy page.
   return [{ address: "0x0000000000000000000000000000000000000000" }];
 }
 
 const AddressPage = async ({ params }: PageProps) => {
   const address = params?.address as string;
-  if (address === "0x0000000000000000000000000000000000000000") return null;
+
+  if (isZeroAddress(address)) return null;
 
   const contractData: { bytecode: string; assembly: string } | null = await getContractData(address);
   return <AddressComponent address={address} contractData={contractData} />;
--- packages/nextjs/app/blockexplorer/transaction/[txHash]/page.tsx
+++ packages/nextjs/app/blockexplorer/transaction/[txHash]/page.tsx
@@ -1,19 +1,21 @@
 import TransactionComp from "../_components/TransactionComp";
 import type { NextPage } from "next";
 import { Hash } from "viem";
+import { isZeroAddress } from "~~/utils/scaffold-eth/common";
 
 type PageProps = {
   params: { txHash?: Hash };
 };
 
 export function generateStaticParams() {
-  // An workaround to enable static exports in Next.js
+  // An workaround to enable static exports in Next.js, generating single dummy page.
   return [{ txHash: "0x0000000000000000000000000000000000000000" }];
 }
 const TransactionPage: NextPage<PageProps> = ({ params }: PageProps) => {
   const txHash = params?.txHash as Hash;
 
-  if (txHash === "0x0000000000000000000000000000000000000000") return null;
+  if (isZeroAddress(txHash)) return null;
+
   return <TransactionComp txHash={txHash} />;
 };
 
