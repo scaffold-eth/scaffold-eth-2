1e38c6399dbc3a9043a37dd8967aa4b46b752cf3
chair28980 <vrycmfy@protonmail.com>
2024-04-24 19:14:43 -0700

    add magma testnet

--- packages/hardhat/hardhat.config.ts
+++ packages/hardhat/hardhat.config.ts
@@ -118,6 +118,10 @@ const config: HardhatUserConfig = {
       url: "https://sepolia.publicgoods.network",
       accounts: [deployerPrivateKey],
     },
+    magmaTestnet: {
+      url: "https://turbo.magma-rpc.com/",
+      accounts: [deployerPrivateKey],
+    }
   },
   // configuration for harhdat-verify plugin
   etherscan: {
