18c6e7f8daeaf5270391b1caa0fe7003c42c6379
chair28980 <vrycmfy@protonmail.com>
2024-04-24 19:24:46 -0700

    Revert "set locl burner wallet to false"
    This reverts commit 186dc3c54f4446a5d7b61aa380eed777ff696652.

--- packages/nextjs/scaffold.config.ts
+++ packages/nextjs/scaffold.config.ts
@@ -29,7 +29,7 @@ const scaffoldConfig = {
   walletConnectProjectId: process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID || "3a8170812b534d0ff9d794f19a901d64",
 
   // Only show the Burner Wallet when running on hardhat network
-  onlyLocalBurnerWallet: false,
+  onlyLocalBurnerWallet: true,
 } as const satisfies ScaffoldConfig;
 
 export default scaffoldConfig;
