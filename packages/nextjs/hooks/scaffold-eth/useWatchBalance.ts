ee1026955450c9a2b97ef16fc0e02ed2559be1e1
Shiv Bhonde <shivbhonde04@gmail.com>
2024-05-01 17:39:13 +0530

    nit: inline parameter type

--- packages/nextjs/hooks/scaffold-eth/useWatchBalance.ts
+++ packages/nextjs/hooks/scaffold-eth/useWatchBalance.ts
@@ -3,12 +3,10 @@ import { useTargetNetwork } from "./useTargetNetwork";
 import { useQueryClient } from "@tanstack/react-query";
 import { UseBalanceParameters, useBalance, useBlockNumber } from "wagmi";
 
-type UseWatchBalanceParameters = UseBalanceParameters;
-
 /**
  * Wrapper around wagmi's useBalance hook. Updates data on every block change.
  */
-export const useWatchBalance = (useBalanceParameters: UseWatchBalanceParameters) => {
+export const useWatchBalance = (useBalanceParameters: UseBalanceParameters) => {
   const { targetNetwork } = useTargetNetwork();
   const queryClient = useQueryClient();
   const { data: blockNumber } = useBlockNumber({ watch: true, chainId: targetNetwork.id });
