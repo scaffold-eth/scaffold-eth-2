5051944627f7e6f03a6479d2d5a2758a1b848989
Rinat <rinat@hey.com>
2024-05-07 21:52:21 +0200

    fix: useTargetNetwork reexport

--- packages/nextjs/hooks/scaffold-eth/index.ts
+++ packages/nextjs/hooks/scaffold-eth/index.ts
@@ -11,5 +11,6 @@ export * from "./useScaffoldEventHistory";
 export * from "./useScaffoldReadContract";
 export * from "./useScaffoldWatchContractEvent";
 export * from "./useScaffoldWriteContract";
+export * from "./useTargetNetwork";
 export * from "./useTransactor";
 export * from "./useWatchBalance";
