490e60fc5f71a37c3da0a34fb096deca5c67e52a
ByteAtATime <ByteAtATime@proton.me>
2024-05-26 18:54:55 -0700

    fix: allow tx result to scroll

--- packages/nextjs/app/debug/_components/contract/ReadOnlyFunctionForm.tsx
+++ packages/nextjs/app/debug/_components/contract/ReadOnlyFunctionForm.tsx
@@ -77,9 +77,9 @@ export const ReadOnlyFunctionForm = ({
       </p>
       {inputElements}
       <div className="flex justify-between gap-2 flex-wrap">
-        <div className="flex-grow w-4/5">
+        <div className="flex-grow max-w-[80%]">
           {result !== null && result !== undefined && (
-            <div className="bg-secondary rounded-3xl text-sm px-4 py-1.5 break-words">
+            <div className="bg-secondary rounded-3xl text-sm px-4 py-1.5 break-words overflow-auto">
               <p className="font-bold m-0 mb-1">Result:</p>
               <pre className="whitespace-pre-wrap break-words">{displayTxResult(result)}</pre>
             </div>
