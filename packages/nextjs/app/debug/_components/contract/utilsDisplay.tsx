576886aab92493cabbb51089d022dcec1c752b59
Shiv Bhonde <shivbhonde04@gmail.com>
2024-06-10 20:04:13 +0530

    use btn-ghost for Number action btn

--- packages/nextjs/app/debug/_components/contract/utilsDisplay.tsx
+++ packages/nextjs/app/debug/_components/contract/utilsDisplay.tsx
@@ -64,7 +64,7 @@ const NumberDisplay = ({ value }: { value: bigint }) => {
         className="tooltip tooltip-secondary font-sans ml-2"
         data-tip={isEther ? "Multiply by 1e18" : "Divide by 1e18"}
       >
-        <button className="btn btn-primary btn-circle btn-xs" onClick={() => setIsEther(!isEther)}>
+        <button className="btn btn-ghost btn-circle btn-xs" onClick={() => setIsEther(!isEther)}>
           <ArrowsRightLeftIcon className="h-3 w-3" />
         </button>
       </span>
