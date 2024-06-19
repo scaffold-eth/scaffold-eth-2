1a7a13153d34c6b2a195bb8a34f1213205a3c3a0
Shiv Bhonde <shivbhonde04@gmail.com>
2024-06-19 07:57:47 +0530

    add foundry changes without submodules

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -7,15 +7,9 @@ const buildNextEslintCommand = (filenames) =>
 
 const checkTypesNextCommand = () => "yarn next:check-types";
 
-const buildHardhatEslintCommand = (filenames) =>
-  `yarn hardhat:lint-staged --fix ${filenames
-    .map((f) => path.relative(path.join("packages", "hardhat"), f))
-    .join(" ")}`;
-
 module.exports = {
   "packages/nextjs/**/*.{ts,tsx}": [
     buildNextEslintCommand,
     checkTypesNextCommand,
   ],
-  "packages/hardhat/**/*.{ts,tsx}": [buildHardhatEslintCommand],
 };
