3d0071a32b7fde17136a889866c9ce4a0cde1489
Shiv Bhonde <shivbhonde04@gmail.com>
2024-06-23 01:15:26 +0530

    get basic working

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -1,13 +1,27 @@
 "use client";
 
-import Link from "next/link";
+import { useState } from "react";
 import type { NextPage } from "next";
 import { useAccount } from "wagmi";
-import { BugAntIcon, MagnifyingGlassIcon } from "@heroicons/react/24/outline";
-import { Address } from "~~/components/scaffold-eth";
+import { InputBase } from "~~/components/scaffold-eth";
+import { useScaffoldWriteContract } from "~~/hooks/scaffold-eth";
+import { useSCEventHistory } from "~~/hooks/scaffold-eth/useSCEventHistory";
 
 const Home: NextPage = () => {
   const { address: connectedAddress } = useAccount();
+
+  const [newGreetings, setNewGreetings] = useState<string>("");
+
+  const { data: eventHistory } = useSCEventHistory({
+    contractName: "YourContract",
+    eventName: "GreetingChange",
+    filters: { greetingSetter: connectedAddress },
+    fromBlock: 0n,
+  });
+
+  const { writeContractAsync: writeYourContractAsync } = useScaffoldWriteContract("YourContract");
+
+  console.log(eventHistory?.pages.flat());
 
   return (
     <>
@@ -17,50 +31,44 @@ const Home: NextPage = () => {
             <span className="block text-2xl mb-2">Welcome to</span>
             <span className="block text-4xl font-bold">Scaffold-ETH 2</span>
           </h1>
-          <div className="flex justify-center items-center space-x-2 flex-col sm:flex-row">
-            <p className="my-2 font-medium">Connected Address:</p>
-            <Address address={connectedAddress} />
-          </div>
-          <p className="text-center text-lg">
-            Get started by editing{" "}
-            <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
-              packages/nextjs/app/page.tsx
-            </code>
-          </p>
-          <p className="text-center text-lg">
-            Edit your smart contract{" "}
-            <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
-              YourContract.sol
-            </code>{" "}
-            in{" "}
-            <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
-              packages/hardhat/contracts
-            </code>
-          </p>
-        </div>
-
-        <div className="flex-grow bg-base-300 w-full mt-16 px-8 py-12">
-          <div className="flex justify-center items-center gap-12 flex-col sm:flex-row">
-            <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
-              <BugAntIcon className="h-8 w-8 fill-secondary" />
-              <p>
-                Tinker with your smart contract using the{" "}
-                <Link href="/debug" passHref className="link">
-                  Debug Contracts
-                </Link>{" "}
-                tab.
-              </p>
-            </div>
-            <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
-              <MagnifyingGlassIcon className="h-8 w-8 fill-secondary" />
-              <p>
-                Explore your local transactions with the{" "}
-                <Link href="/blockexplorer" passHref className="link">
-                  Block Explorer
-                </Link>{" "}
-                tab.
-              </p>
-            </div>
+          <div className="overflow-x-auto">
+            <InputBase value={newGreetings} onChange={setNewGreetings} />
+            <button
+              className="btn btn-primary"
+              onClick={async () => {
+                try {
+                  await writeYourContractAsync({
+                    functionName: "setGreeting",
+                    args: [newGreetings],
+                  });
+                } catch (e) {
+                  console.error("Error setting greeting:", e);
+                }
+              }}
+            >
+              Set Greeting
+            </button>
+            <table className="table">
+              {/* head */}
+              <thead>
+                <tr>
+                  <th></th>
+                  <th>Gretting Setter</th>
+                  <th>greeting</th>
+                </tr>
+              </thead>
+              {/* <tbody>
+                {eventHistory?.map((event, index) => (
+                  <tr key={index}>
+                    <th>{index + 1}</th>
+                    <td>
+                      <Address address={event.args.greetingSetter} />
+                    </td>
+                    <td>{event.args.newGreeting}</td>
+                  </tr>
+                ))}
+              </tbody> */}
+            </table>
           </div>
         </div>
       </div>
---
+++ packages/nextjs/hooks/scaffold-eth/useSCEventHistory.ts
@@ -0,0 +1,131 @@
+import { useCallback, useEffect, useMemo, useState } from "react";
+import { useTargetNetwork } from "./useTargetNetwork";
+import { useInfiniteQuery, useQuery, useQueryClient } from "@tanstack/react-query";
+import { Abi, AbiEvent, ExtractAbiEventNames } from "abitype";
+import { useInterval } from "usehooks-ts";
+import { BlockNumber, GetLogsParameters, Hash } from "viem";
+import * as chains from "viem/chains";
+import { Config, UsePublicClientReturnType, useBlockNumber, usePublicClient } from "wagmi";
+import deployedContracts from "~~/contracts/deployedContracts";
+import { useDeployedContractInfo } from "~~/hooks/scaffold-eth";
+import scaffoldConfig from "~~/scaffold.config";
+import { replacer } from "~~/utils/scaffold-eth/common";
+import {
+  ContractAbi,
+  ContractName,
+  UseScaffoldEventHistoryConfig,
+  UseScaffoldEventHistoryData,
+} from "~~/utils/scaffold-eth/contract";
+
+const getLogs = async (
+  getLogsParams: GetLogsParameters<AbiEvent | undefined, AbiEvent[] | undefined, boolean, BlockNumber, BlockNumber>,
+  publicClient?: UsePublicClientReturnType<Config, number>,
+) => {
+  console.log("Getting event logs", getLogsParams);
+  const logs = await publicClient?.getLogs({
+    address: getLogsParams.address,
+    fromBlock: getLogsParams.fromBlock,
+    args: getLogsParams.args,
+    event: getLogsParams.event,
+  });
+
+  return logs;
+};
+
+/**
+ * Reads events from a deployed contract
+ * @param config - The config settings
+ * @param config.contractName - deployed contract name
+ * @param config.eventName - name of the event to listen for
+ * @param config.fromBlock - the block number to start reading events from
+ * @param config.filters - filters to be applied to the event (parameterName: value)
+ * @param config.blockData - if set to true it will return the block data for each event (default: false)
+ * @param config.transactionData - if set to true it will return the transaction data for each event (default: false)
+ * @param config.receiptData - if set to true it will return the receipt data for each event (default: false)
+ * @param config.watch - if set to true, the events will be updated every pollingInterval milliseconds set at scaffoldConfig (default: false)
+ * @param config.enabled - set this to false to disable the hook from running (default: true)
+ */
+export const useSCEventHistory = <
+  TContractName extends ContractName,
+  TEventName extends ExtractAbiEventNames<ContractAbi<TContractName>>,
+  TBlockData extends boolean = false,
+  TTransactionData extends boolean = false,
+  TReceiptData extends boolean = false,
+>({
+  contractName,
+  eventName,
+  fromBlock,
+  filters,
+  blockData,
+  transactionData,
+  receiptData,
+  watch,
+  enabled = true,
+}: UseScaffoldEventHistoryConfig<TContractName, TEventName, TBlockData, TTransactionData, TReceiptData>) => {
+  const { targetNetwork } = useTargetNetwork();
+  const publicClient = usePublicClient({
+    chainId: targetNetwork.id,
+  });
+  const [initalFromBlock, setInitialFromBlock] = useState(fromBlock);
+
+  const { data: blockNumber } = useBlockNumber({ watch: true, chainId: targetNetwork.id });
+
+  const { data: deployedContractData } = useDeployedContractInfo(contractName);
+
+  const event =
+    deployedContractData &&
+    ((deployedContractData.abi as Abi).find(part => part.type === "event" && part.name === eventName) as AbiEvent);
+
+  const query = useInfiniteQuery({
+    queryKey: ["eventHistory", { contractName, eventName, fromBlock: fromBlock.toString(), chainId: targetNetwork.id }],
+    queryFn: async ({ pageParam }) => {
+      console.log("The page param is", pageParam);
+      if (!Boolean(deployedContractData) || !Boolean(publicClient)) return undefined;
+      const data = await getLogs(
+        { address: deployedContractData?.address, event, fromBlock: pageParam, args: filters },
+        publicClient,
+      );
+
+      return data;
+    },
+    enabled: Boolean(deployedContractData) && Boolean(publicClient),
+    initialPageParam: fromBlock,
+    getNextPageParam: () => {
+      return blockNumber;
+    },
+  });
+
+  useEffect(() => {
+    if (!blockNumber) return;
+    if (initalFromBlock < blockNumber) {
+      console.log("Setting initial from block", initalFromBlock, blockNumber);
+      setInitialFromBlock(blockNumber + 1000n);
+      return;
+    } else {
+      query.fetchNextPage();
+    }
+  }, [blockNumber]);
+
+  return query;
+};
+
+export const addIndexedArgsToEvent = (event: any) => {
+  if (event.args && !Array.isArray(event.args)) {
+    return { ...event, args: { ...event.args, ...Object.values(event.args) } };
+  }
+
+  return event;
+};
+
+/* return useQuery({
+    queryKey: ["eventHistory", { contractName, eventName, fromBlock: fromBlock.toString(), chainId: targetNetwork.id }],
+    queryFn: async () => {
+      const data = await getLogs(
+        { address: deployedContractData?.address, event, fromBlock, args: filters },
+        publicClient,
+      );
+
+      return data;
+    },
+    enabled: Boolean(deployedContractData) && Boolean(publicClient),
+  }); */
