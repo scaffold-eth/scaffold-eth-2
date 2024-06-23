d586a7ad45f4cab6635b2e78864ce91307e70728
Shiv Bhonde <shivbhonde04@gmail.com>
2024-06-23 16:47:37 +0530

    put other files to main version

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -1,19 +1,13 @@
 "use client";
 
+import Link from "next/link";
 import type { NextPage } from "next";
+import { useAccount } from "wagmi";
+import { BugAntIcon, MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 import { Address } from "~~/components/scaffold-eth";
-import { useScaffoldEventHistory } from "~~/hooks/scaffold-eth/useScaffoldEventHistory";
 
 const Home: NextPage = () => {
-  const {
-    data: eventHistory,
-    status,
-    isFetchingNewEvent,
-  } = useScaffoldEventHistory({
-    contractName: "YourContract",
-    eventName: "GreetingChange",
-    fromBlock: 4738147n,
-  });
+  const { address: connectedAddress } = useAccount();
 
   return (
     <>
@@ -23,43 +17,50 @@ const Home: NextPage = () => {
             <span className="block text-2xl mb-2">Welcome to</span>
             <span className="block text-4xl font-bold">Scaffold-ETH 2</span>
           </h1>
-          <div className="overflow-x-auto">
-            <table className="table">
-              {/* head */}
-              <thead>
-                <tr>
-                  <th></th>
-                  <th>Gretting Setter</th>
-                  <th>greeting</th>
-                </tr>
-              </thead>
-              <tbody>
-                {status === "pending" && (
-                  <tr className="animate-pulse">
-                    <th>
-                      <span className="loading loading-dots loading-xs"></span>
-                    </th>
-                    <td>
-                      <span className="loading loading-dots loading-xs"></span>
-                    </td>
-                    <td>
-                      <span className="loading loading-dots loading-xs"></span>
-                    </td>
-                  </tr>
-                )}
-                {eventHistory?.map((event, index) => (
-                  <tr key={index}>
-                    <th className={`${isFetchingNewEvent ? "animate-pulse opacity-80" : ""}`}>{index + 1}</th>
-                    <td className={`${isFetchingNewEvent ? "animate-pulse opacity-80" : ""}`}>
-                      <Address address={event?.args.greetingSetter} />
-                    </td>
-                    <td className={`${isFetchingNewEvent ? "animate-pulse opacity-80" : ""}`}>
-                      {event?.args.newGreeting}
-                    </td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
+          <div className="flex justify-center items-center space-x-2 flex-col sm:flex-row">
+            <p className="my-2 font-medium">Connected Address:</p>
+            <Address address={connectedAddress} />
+          </div>
+          <p className="text-center text-lg">
+            Get started by editing{" "}
+            <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
+              packages/nextjs/app/page.tsx
+            </code>
+          </p>
+          <p className="text-center text-lg">
+            Edit your smart contract{" "}
+            <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
+              YourContract.sol
+            </code>{" "}
+            in{" "}
+            <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
+              packages/hardhat/contracts
+            </code>
+          </p>
+        </div>
+
+        <div className="flex-grow bg-base-300 w-full mt-16 px-8 py-12">
+          <div className="flex justify-center items-center gap-12 flex-col sm:flex-row">
+            <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
+              <BugAntIcon className="h-8 w-8 fill-secondary" />
+              <p>
+                Tinker with your smart contract using the{" "}
+                <Link href="/debug" passHref className="link">
+                  Debug Contracts
+                </Link>{" "}
+                tab.
+              </p>
+            </div>
+            <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
+              <MagnifyingGlassIcon className="h-8 w-8 fill-secondary" />
+              <p>
+                Explore your local transactions with the{" "}
+                <Link href="/blockexplorer" passHref className="link">
+                  Block Explorer
+                </Link>{" "}
+                tab.
+              </p>
+            </div>
           </div>
         </div>
       </div>
