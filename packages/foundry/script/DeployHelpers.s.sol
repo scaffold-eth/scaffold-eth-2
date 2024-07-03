24da39df4420ed90ca6725ebc8425dd56a7aa5c1
CarlosR <carlos.ramos@grupoimportia.com>
2024-07-03 22:06:16 +0700

    fix : simplify logic

--- packages/foundry/script/DeployHelpers.s.sol
+++ packages/foundry/script/DeployHelpers.s.sol
@@ -15,31 +15,30 @@ contract ScaffoldETHDeploy is Script {
   string root;
   string path;
   Deployment[] public deployments;
-  uint256 constant SCAFFOLD_BASE_BALANCE = 9999 * 1e18;
+  uint256 constant ANVIL_FIRST_PK =
+    0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80;
+  address constant ANVIL_FIRST_ACCOUNT =
+    0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266;
 
   function _startBroadcast() internal returns (address deployer) {
+    vm.startBroadcast();
+    (, deployer,) = vm.readCallers();
+
     if (block.chainid == 31337) {
       // starts a broadcast and reads the caller
-      vm.startBroadcast();
-      (, deployer,) = vm.readCallers();
       vm.stopBroadcast();
       // check balance of first anvil account
       // if balance is not 0, is the first run
-      uint256 balance = 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266.balance;
-      uint256 anvilFirstPk =
-        0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80;
-      if (balance > 1 ether && deployer != vm.addr(anvilFirstPk)) {
-        vm.startBroadcast(anvilFirstPk);
-        (bool success,) = deployer.call{ value: SCAFFOLD_BASE_BALANCE }("");
+      uint256 balance = ANVIL_FIRST_ACCOUNT.balance;
+      if (balance > 1 ether && deployer != vm.addr(ANVIL_FIRST_PK)) {
+        vm.startBroadcast(ANVIL_FIRST_PK);
+        (bool success,) = deployer.call{ value: balance - 1 ether }("");
         if (!success) {
           revert();
         }
         vm.stopBroadcast();
       }
       vm.startBroadcast(deployer);
-    } else {
-      vm.startBroadcast();
-      (, deployer,) = vm.readCallers();
     }
   }
 
