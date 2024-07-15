1528a711ff2fd6edbd4344f5250baf6ca2e38884
Rinat <rinat@hey.com>
2024-07-15 19:37:51 +0200

    feat: update ts version and abi.d.ts

--- packages/nextjs/types/abitype/abi.d.ts
+++ packages/nextjs/types/abitype/abi.d.ts
@@ -1,23 +1,7 @@
 import "abitype";
-import "viem";
-import "wagmi";
 
 type AddressType = string;
 
 declare module "abitype" {
-  export interface Register {
-    AddressType: AddressType;
-  }
-}
-
-declare module "viem/node_modules/abitype" {
-  export interface Register {
-    AddressType: AddressType;
-  }
-}
-
-declare module "wagmi/node_modules/abitype" {
-  export interface Register {
-    AddressType: AddressType;
-  }
+  export type Address = AddressType;
 }
