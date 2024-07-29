90c5401948ee8ecd2c09057838c534c6257705d1
Shiv Bhonde <shivbhonde04@gmail.com>
2024-07-29 20:52:13 +0900

    use modifiers

--- packages/foundry/script/Deploy.s.sol
+++ packages/foundry/script/Deploy.s.sol
@@ -6,27 +6,30 @@ import "./DeployHelpers.s.sol";
 import { DeployYourContract } from "./00_deploy_your_contract.s.sol";
 
 contract DeployScript is ScaffoldETHDeploy {
+  uint256 deployerPrivateKey;
+
   error InvalidPrivateKey(string);
 
-  function run() external {
-    // check for the deployer account
-    uint256 deployerPrivateKey = setupLocalhostEnv();
+  constructor() {
+    deployerPrivateKey = setupLocalhostEnv();
+  }
+
+  function run() external checkDeployerPrivateKey deploymentExporter {
+    DeployYourContract deployYourContract = new DeployYourContract();
+    deployYourContract.run();
+  }
+
+  modifier checkDeployerPrivateKey() {
     if (deployerPrivateKey == 0) {
       revert InvalidPrivateKey(
         "You don't have a deployer account. Make sure you have set DEPLOYER_PRIVATE_KEY in .env or use `yarn generate` to generate a new random account"
       );
     }
-
-    DeployYourContract deployYourContract = new DeployYourContract();
-    deployYourContract.run();
-
-    /**
-     * This function generates the file containing the contracts Abi definitions.
-     * These definitions are used to derive the types needed in the custom scaffold-eth hooks, for example.
-     * This function should be called last.
-     */
-    exportDeployments();
+    _;
   }
 
-  function test() public { }
+  modifier deploymentExporter() {
+    _;
+    exportDeployments();
+  }
 }
