bc49e2856efec8b6181ec6245b5af745a794eb76
Rinat <rinat@hey.com>
2024-08-05 14:00:57 +0200

    fix: typescript eslint + up eslint

---
+++ packages/nextjs/eslint.config.mjs
@@ -0,0 +1,21 @@
+// @ts-check
+import eslint from "@eslint/js";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    rules: {
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": "error",
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/ban-ts-comment": "off",
+    },
+    /*... */
+  },
+  // @ts-ignore
+  eslintPluginPrettierRecommended,
+  // ...
+);
