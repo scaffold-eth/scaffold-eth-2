16d4c383eee1660c6b420ddf7de48d8847ce115d
Rinat <rinat@hey.com>
2024-08-05 14:24:21 +0200

    fix: remove comments

--- packages/nextjs/eslint.config.mjs
+++ packages/nextjs/eslint.config.mjs
@@ -13,9 +13,7 @@ export default tseslint.config(
       "@typescript-eslint/no-explicit-any": "off",
       "@typescript-eslint/ban-ts-comment": "error",
     },
-    /*... */
   },
   // @ts-expect-error eslintPluginPrettierRecommended types are broken for now
   eslintPluginPrettierRecommended,
-  // ...
 );
