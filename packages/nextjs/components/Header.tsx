c1e0aaaf6b86761ddb37a504cd0a53f6d791ef0e
robbiekruszynski <robbiekruszynski@gmail..com>
2024-08-08 16:34:57 +0200

    tweaks

--- packages/nextjs/components/Header.tsx
+++ packages/nextjs/components/Header.tsx
@@ -89,8 +89,7 @@ export const Header = () => {
             <Image alt="SE2 logo" className="cursor-pointer" fill src="/logo.svg" />
           </div>
           <div className="flex flex-col">
-            <span className="font-bold leading-tight">Scaffold-ETH</span>
-            <span className="text-xs">Ethereum dev stack</span>
+            <span className="font-bold leading-tight">Scaffold-ETH + ApeChain</span>
           </div>
         </Link>
         <ul className="hidden lg:flex lg:flex-nowrap menu menu-horizontal px-1 gap-2">
