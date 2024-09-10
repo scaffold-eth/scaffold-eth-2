dda7295f626eac957c78e80707f555b3ceda1903
Rinat <rinat@hey.com>
2024-09-10 12:49:38 +0300

    chore: viem, wagmi and rainbowkit patches

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -1,191 +1,68 @@
 "use client";
 
-import { ReactNode, useState } from "react";
+import Link from "next/link";
 import type { NextPage } from "next";
-import { formatEther, parseEther } from "viem";
-import { useAccount, useWalletClient } from "wagmi";
-import { Address, AddressInput, Balance, EtherInput, InputBase } from "~~/components/scaffold-eth";
-import {
-  useScaffoldContract,
-  useScaffoldEventHistory,
-  useScaffoldReadContract,
-  useScaffoldWriteContract,
-  useTransactor,
-} from "~~/hooks/scaffold-eth";
-
-const ExampleWrapper = ({ children }: { children: ReactNode }) => {
-  return <div className="flex flex-col gap-4 bg-base-100 shadow-xl rounded-xl p-5">{children}</div>;
-};
+import { useAccount } from "wagmi";
+import { BugAntIcon, MagnifyingGlassIcon } from "@heroicons/react/24/outline";
+import { Address } from "~~/components/scaffold-eth";
 
 const Home: NextPage = () => {
   const { address: connectedAddress } = useAccount();
 
-  // ------ State ------
-  const [newGreeting, setNewGreeting] = useState("");
-  const [greetingValue, setNewGreetingValue] = useState("");
-  const [counterAddress, setCounterAddress] = useState("");
-
-  // --------------------- SE-2 Custom Hook -------------------
-  const transactorWrite = useTransactor();
-
-  // ---- WRITE METHODS ----
-  // TEST: Autocompleteion for contractName
-  const { writeContractAsync: writeYourContractAsync } = useScaffoldWriteContract("YourContract");
-
-  const { data: walletClient } = useWalletClient();
-  const { data: YourContract } = useScaffoldContract({
-    // TEST: Autocompleteion for contractName
-    contractName: "YourContract",
-    // TEST: If you remove walletClient, this it should give TS error at line 115 that `write` is not defined property
-    walletClient,
-  });
-
-  // --- READ METHODS ---
-  const { data: userGreetingCounter, refetch } = useScaffoldReadContract({
-    // TEST: Autocompleteion for contractName and functionName
-    contractName: "YourContract",
-    functionName: "userGreetingCounter",
-    args: [counterAddress],
-    query: {
-      enabled: false,
-    },
-  });
-
-  const { data: connectedAddressCounter } = useScaffoldReadContract({
-    contractName: "YourContract",
-    functionName: "userGreetingCounter",
-    args: [connectedAddress],
-  });
-
-  const { data: greetingsChangeLogs } = useScaffoldEventHistory({
-    contractName: "YourContract",
-    eventName: "GreetingChange",
-    watch: true,
-    fromBlock: 0n,
-  });
-
   return (
     <>
-      <div className="flex items-center flex-col flex-grow pt-10 gap-8">
-        <div className="px-5 flex flex-col items-center">
+      <div className="flex items-center flex-col flex-grow pt-10">
+        <div className="px-5">
           <h1 className="text-center">
             <span className="block text-2xl mb-2">Welcome to</span>
             <span className="block text-4xl font-bold">Scaffold-ETH 2</span>
           </h1>
-          <div className="flex gap-2">
-            <span>Connected Address: </span>
+          <div className="flex justify-center items-center space-x-2 flex-col sm:flex-row">
+            <p className="my-2 font-medium">Connected Address:</p>
             <Address address={connectedAddress} />
           </div>
-          <div className="flex gap-2 mt-2 items-center">
-            <span>Balance: </span>
-            <Balance address={connectedAddress} />
-          </div>
-          <div className="flex gap-2 mb-3">
-            <span>My counter</span>
-            <span>{connectedAddressCounter?.toString() || 0}</span>
-          </div>
-
-          <button
-            className="btn btn-primary"
-            onClick={async () => {
-              try {
-                await transactorWrite({
-                  to: "0x55b9CB0bCf56057010b9c471e7D42d60e1111EEa",
-                  value: parseEther("5"),
-                });
-              } catch (error) {}
-            }}
-          >
-            Send 5 ETH
-          </button>
+          <p className="text-center text-lg">
+            Get started by editing{" "}
+            <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
+              packages/nextjs/app/page.tsx
+            </code>
+          </p>
+          <p className="text-center text-lg">
+            Edit your smart contract{" "}
+            <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
+              YourContract.sol
+            </code>{" "}
+            in{" "}
+            <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
+              packages/hardhat/contracts
+            </code>
+          </p>
         </div>
-        {/*--------- Write Demo -------------- */}
-        {/*Demo with useScaffoldContractWrite Input + Value*/}
-        <ExampleWrapper>
-          <h1 className="text-center text-lg underline">UseScaffoldContractWrite Example</h1>
-          <InputBase placeholder="Set new greeting" value={newGreeting} onChange={value => setNewGreeting(value)} />
-          <EtherInput value={greetingValue} onChange={value => setNewGreetingValue(value)} />
-          <button
-            className="btn btn-primary btn-md"
-            onClick={async () => {
-              try {
-                await writeYourContractAsync({
-                  // TEST: TS autocompletion and types for functioName, args and value
-                  functionName: "setGreeting",
-                  args: [newGreeting],
-                  value: greetingValue ? parseEther(greetingValue) : undefined,
-                });
-              } catch (error) {
-                console.error("Error setting greeting", error);
-              }
-            }}
-          >
-            Set Greeting
-          </button>
-        </ExampleWrapper>
 
-        {/*Demo with bare useScaffolContract */}
-        <ExampleWrapper>
-          <h1 className="text-center text-lg underline">useScaffoldContract Example</h1>
-          <InputBase placeholder="Set new greeting" value={newGreeting} onChange={value => setNewGreeting(value)} />
-          <EtherInput value={greetingValue} onChange={value => setNewGreetingValue(value)} />
-          <button
-            className="btn btn-primary btn-md"
-            onClick={async () => {
-              try {
-                if (!YourContract) throw new Error("YourContract not loaded");
-                await transactorWrite(() =>
-                  YourContract.write.setGreeting([newGreeting], {
-                    value: greetingValue ? parseEther(greetingValue) : undefined,
-                  }),
-                );
-              } catch (error) {
-                console.error("Error setting greeting", error);
-              }
-            }}
-          >
-            Set Greeting
-          </button>
-        </ExampleWrapper>
-
-        {/*--------- Read Demo -------------- */}
-        <ExampleWrapper>
-          <h1 className="text-center text-lg underline">useScaffoldContractRead Example</h1>
-          <AddressInput placeholder="Counter for address" value={counterAddress} onChange={setCounterAddress} />
-          <p>Counter: {userGreetingCounter?.toString()} </p>
-          <button className="btn btn-primary btn-md" onClick={() => refetch()}>
-            Read
-          </button>
-        </ExampleWrapper>
-
-        <ExampleWrapper>
-          <h1 className="text-center text-lg underline">useScaffoldEventHistory Example</h1>
-          <div className="overflow-x-auto">
-            <table className="table">
-              {/* head */}
-              <thead>
-                <tr>
-                  <th></th>
-                  <th>Address</th>
-                  <th>Greetings</th>
-                  <th>Premium</th>
-                </tr>
-              </thead>
-              <tbody>
-                {greetingsChangeLogs?.map((log, index) => (
-                  <tr key={index}>
-                    <th>{index + 1}</th>
-                    <th>
-                      <Address address={log.args.greetingSetter} />
-                    </th>
-                    <th>{log.args.newGreeting}</th>
-                    <th>{log.args.value ? formatEther(log.args.value) : "0"} ETH</th>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
+        <div className="flex-grow bg-base-300 w-full mt-16 px-8 py-12">
+          <div className="flex justify-center items-center gap-12 flex-col sm:flex-row">
+            <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
+              <BugAntIcon className="h-8 w-8 fill-secondary" />
+              <p>
+                Tinker with your smart contract using the{" "}
+                <Link href="/debug" passHref className="link">
+                  Debug Contracts
+                </Link>{" "}
+                tab.
+              </p>
+            </div>
+            <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
+              <MagnifyingGlassIcon className="h-8 w-8 fill-secondary" />
+              <p>
+                Explore your local transactions with the{" "}
+                <Link href="/blockexplorer" passHref className="link">
+                  Block Explorer
+                </Link>{" "}
+                tab.
+              </p>
+            </div>
           </div>
-        </ExampleWrapper>
+        </div>
       </div>
     </>
   );
