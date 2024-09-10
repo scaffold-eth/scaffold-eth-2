e15f2876e5f38ab649015eaa9aacf305deb8fe64
Rinat <rinat@hey.com>
2024-09-10 12:44:02 +0300

    fix: react-copy-to-clipboard types

--- packages/nextjs/app/debug/_components/contract/TxReceipt.tsx
+++ packages/nextjs/app/debug/_components/contract/TxReceipt.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { CopyToClipboard } from "react-copy-to-clipboard";
+import CopyToClipboard from "react-copy-to-clipboard";
 import { TransactionReceipt } from "viem";
 import { CheckCircleIcon, DocumentDuplicateIcon } from "@heroicons/react/24/outline";
 import { ObjectFieldDisplay } from "~~/app/debug/_components/contract";
