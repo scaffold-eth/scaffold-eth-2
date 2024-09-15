5e269fe98158071fddb7c3ed47523af9514565ac
Rinat <rinat@hey.com>
2024-09-15 19:44:55 +0300

    revert: ghost button colors

--- packages/nextjs/components/ui/button.tsx
+++ packages/nextjs/components/ui/button.tsx
@@ -12,7 +12,7 @@ const buttonVariants = cva(
         destructive: "bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90",
         outline: "border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground",
         secondary: "bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80",
-        ghost: "hover:!bg-muted",
+        ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
       },
       size: {
