a8eadc0c36be345cdcc85018fa704a1180bae86c
Rinat <rinat@hey.com>
2024-09-19 21:19:08 +0300

    fix: qr code description

--- packages/nextjs/components/scaffold-eth/RainbowKitCustomConnectButton/AddressQRCodeModal.tsx
+++ packages/nextjs/components/scaffold-eth/RainbowKitCustomConnectButton/AddressQRCodeModal.tsx
@@ -1,7 +1,14 @@
 import { QRCodeSVG } from "qrcode.react";
 import { Address as AddressType } from "viem";
 import { Address } from "~~/components/scaffold-eth";
-import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogTrigger } from "~~/components/ui/dialog";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "~~/components/ui/dialog";
 
 type AddressQRCodeModalProps = {
   address: AddressType;
@@ -16,6 +23,7 @@ export const AddressQRCodeModal = ({ address, children }: AddressQRCodeModalProp
         <DialogHeader>
           <DialogTitle>Address QR Code</DialogTitle>
         </DialogHeader>
+        <DialogDescription className="hidden">Address QR Code</DialogDescription>
         <div className="flex flex-col items-center gap-6 py-6 break-all">
           <QRCodeSVG value={address} size={256} />
           <Address address={address} format="long" disableAddressLink onlyEnsOrAddress />
