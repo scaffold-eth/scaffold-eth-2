7db95b672a966513649227afe818d7fcaee41adf
Paul Gadi <paulgadi@gmail.com>
2024-10-03 13:06:45 -0400

    add rpc and example code creating leaderboards

--- nakama/main.ts
+++ nakama/main.ts
@@ -1,4 +1,32 @@
+function createLeaderboard(nk: nkruntime.Nakama, id: string) {
+    // let id = '4ec4f126-3f9d-11e7-84ef-b7c182b36521';
+    let authoritative = false;
+    let sort = nkruntime.SortOrder.DESCENDING;
+    let operator = nkruntime.Operator.BEST;
+    let reset = '0 0 * * 1';
+    let metadata = {
+      weatherConditions: 'rain',
+    };
+    try {
+        nk.leaderboardCreate(id, authoritative, sort, operator, reset, metadata);
+    } catch(error) {
+        // Handle error
+    }
+}
+
+function rpcCreateTournament(ctx: nkruntime.Context, logger: nkruntime.Logger, nk: nkruntime.Nakama, id: string): string {
+    createLeaderboard(nk, id);    
+
+    logger.info("leaderboard " + id + " created");
+    return JSON.stringify({ success: true });
+
+}
+
 function InitModule(ctx: nkruntime.Context, logger: nkruntime.Logger, nk:nkruntime.Nakama, initializer: nkruntime.Initializer) {
+    createLeaderboard(nk, "4ec4f126-3f9d-11e7-84ef-b7c182b36521");
+
     initializer.registerRpc("healthcheck", rpcHealthcheck);
+    initializer.registerRpc("createTournament", rpcCreateTournament);
+
     logger.info("JavaScript module loaded");
 }
--- nakama/modules/index.js
+++ nakama/modules/index.js
@@ -1,6 +1,29 @@
 "use strict";
+function createLeaderboard(nk, id) {
+    // let id = '4ec4f126-3f9d-11e7-84ef-b7c182b36521';
+    var authoritative = false;
+    var sort = "descending" /* nkruntime.SortOrder.DESCENDING */;
+    var operator = "best" /* nkruntime.Operator.BEST */;
+    var reset = '0 0 * * 1';
+    var metadata = {
+        weatherConditions: 'rain',
+    };
+    try {
+        nk.leaderboardCreate(id, authoritative, sort, operator, reset, metadata);
+    }
+    catch (error) {
+        // Handle error
+    }
+}
+function rpcCreateTournament(ctx, logger, nk, id) {
+    createLeaderboard(nk, id);
+    logger.info("leaderboard " + id + " created");
+    return JSON.stringify({ success: true });
+}
 function InitModule(ctx, logger, nk, initializer) {
+    createLeaderboard(nk, "4ec4f126-3f9d-11e7-84ef-b7c182b36521");
     initializer.registerRpc("healthcheck", rpcHealthcheck);
+    initializer.registerRpc("createTournament", rpcCreateTournament);
     logger.info("JavaScript module loaded");
 }
 function rpcHealthcheck(ctx, logger, nk, payload) {
