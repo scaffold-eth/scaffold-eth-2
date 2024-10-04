8f274f5ffe81a8d1c5ffacb71bc1f0b6db6439ec
AP <paulgadi@gmail.com>
2024-10-04 14:05:53 -0400

    added example leaderboard reset function

--- nakama/main.ts
+++ nakama/main.ts
@@ -1,9 +1,11 @@
+const LEADERBOARD_ID = "radar";
+
 function createLeaderboard(nk: nkruntime.Nakama, id: string) {
     // let id = '4ec4f126-3f9d-11e7-84ef-b7c182b36521';
     let authoritative = false;
     let sort = nkruntime.SortOrder.DESCENDING;
     let operator = nkruntime.Operator.BEST;
-    let reset = '0 0 * * 1';
+    let reset = '*/1 * * * *'; // Every minute
     let metadata = {
       weatherConditions: 'rain',
     };
@@ -22,11 +24,37 @@ function rpcCreateTournament(ctx: nkruntime.Context, logger: nkruntime.Logger, n
 
 }
 
+let leaderboardReset: nkruntime.LeaderboardResetFunction = function (ctx: nkruntime.Context, logger: nkruntime.Logger, nk: nkruntime.Nakama, leaderboard: nkruntime.Leaderboard, reset: number) {
+    if (leaderboard.id != LEADERBOARD_ID) {    
+        return;
+    }
+
+    // Get top 3
+    let result = nk.leaderboardRecordsList(leaderboard.id, [], 3, undefined, reset);
+
+    let walletUpdates : nkruntime.WalletUpdate[] = [];
+  
+    if (result && result.records) {
+        result.records.forEach(function (r) {
+        let reward = 100;
+        
+        walletUpdates.push({
+            userId: r.ownerId,
+            changeset: { coins: reward},
+            metadata: {}
+        })
+        });
+    }
+  
+    nk.walletsUpdate(walletUpdates, true);
+  }
+
 function InitModule(ctx: nkruntime.Context, logger: nkruntime.Logger, nk:nkruntime.Nakama, initializer: nkruntime.Initializer) {
-    createLeaderboard(nk, "4ec4f126-3f9d-11e7-84ef-b7c182b36521");
+    createLeaderboard(nk, LEADERBOARD_ID);
 
     initializer.registerRpc("healthcheck", rpcHealthcheck);
     initializer.registerRpc("createTournament", rpcCreateTournament);
+    initializer.registerLeaderboardReset(leaderboardReset);
 
     logger.info("JavaScript module loaded");
 }
--- nakama/modules/index.js
+++ nakama/modules/index.js
@@ -1,10 +1,11 @@
 "use strict";
+var LEADERBOARD_ID = "radar";
 function createLeaderboard(nk, id) {
     // let id = '4ec4f126-3f9d-11e7-84ef-b7c182b36521';
     var authoritative = false;
     var sort = "descending" /* nkruntime.SortOrder.DESCENDING */;
     var operator = "best" /* nkruntime.Operator.BEST */;
-    var reset = '0 0 * * 1';
+    var reset = '*/1 * * * *'; // Every minute
     var metadata = {
         weatherConditions: 'rain',
     };
@@ -20,10 +21,30 @@ function rpcCreateTournament(ctx, logger, nk, id) {
     logger.info("leaderboard " + id + " created");
     return JSON.stringify({ success: true });
 }
+var leaderboardReset = function (ctx, logger, nk, leaderboard, reset) {
+    if (leaderboard.id != LEADERBOARD_ID) {
+        return;
+    }
+    // Get top 3
+    var result = nk.leaderboardRecordsList(leaderboard.id, [], 3, undefined, reset);
+    var walletUpdates = [];
+    if (result && result.records) {
+        result.records.forEach(function (r) {
+            var reward = 100;
+            walletUpdates.push({
+                userId: r.ownerId,
+                changeset: { coins: reward },
+                metadata: {}
+            });
+        });
+    }
+    nk.walletsUpdate(walletUpdates, true);
+};
 function InitModule(ctx, logger, nk, initializer) {
-    createLeaderboard(nk, "4ec4f126-3f9d-11e7-84ef-b7c182b36521");
+    createLeaderboard(nk, LEADERBOARD_ID);
     initializer.registerRpc("healthcheck", rpcHealthcheck);
     initializer.registerRpc("createTournament", rpcCreateTournament);
+    initializer.registerLeaderboardReset(leaderboardReset);
     logger.info("JavaScript module loaded");
 }
 function rpcHealthcheck(ctx, logger, nk, payload) {
