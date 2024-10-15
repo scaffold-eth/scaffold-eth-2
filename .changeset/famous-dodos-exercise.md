e9a969d1b5addcbb69a8076fa48925345296e189
Tamas Molnar <moltam89@gmail.com>
2024-10-15 04:38:06 +0200

    Template for externalContracts.ts (#138)

---
+++ .changeset/famous-dodos-exercise.md
@@ -0,0 +1,5 @@
+---
+"create-eth": patch
+---
+
+Template for externalContracts.ts
---
+++ templates/base/packages/nextjs/contracts/externalContracts.ts.template.mjs
@@ -0,0 +1,24 @@
+import { withDefaults } from '../../../../utils.js'
+
+const contents = ({ externalContracts }) =>
+`import { GenericContractsDeclaration } from "~~/utils/scaffold-eth/contract";
+
+/**
+ * @example
+ * const externalContracts = {
+ *   1: {
+ *     DAI: {
+ *       address: "0x...",
+ *       abi: [...],
+ *     },
+ *   },
+ * } as const;
+ */
+const externalContracts = ${JSON.stringify(externalContracts[0])} as const;
+
+export default externalContracts satisfies GenericContractsDeclaration;
+`
+
+export default withDefaults(contents, {
+    externalContracts: {}
+})
