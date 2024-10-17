037e6e198285e485fccd9637311603b34e8cda93
Rinat <rinat@hey.com>
2024-10-17 21:52:06 +0200

    fix: update trivago import

--- packages/nextjs/.prettierrc.js
+++ packages/nextjs/.prettierrc.js
@@ -1,9 +1,9 @@
-{
+module.exports = {
   "arrowParens": "avoid",
   "printWidth": 120,
   "tabWidth": 2,
   "trailingComma": "all",
   "importOrder": ["^react$", "^next/(.*)$", "<THIRD_PARTY_MODULES>", "^@heroicons/(.*)$", "^~~/(.*)$"],
   "importOrderSortSpecifiers": true,
-  "plugins": ["@trivago/prettier-plugin-sort-imports"]
+  "plugins": [require.resolve("@trivago/prettier-plugin-sort-imports")],
 }
