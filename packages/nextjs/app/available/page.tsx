8ff35a97008334845a6da75a4e784a7b831d7130
Nora <71670932+noraloch@users.noreply.github.com>
2024-10-20 03:50:46 -0700

    Styling, Work on available page (#3)
    * formatting, remove unused components, add available
    
    * remove ethers
    
    * fix locks

--- packages/nextjs/app/available/page.tsx
+++ packages/nextjs/app/available/page.tsx
@@ -1,20 +1,16 @@
+"use client";
+
 import type { NextPage } from "next";
 import { getMetadata } from "~~/utils/scaffold-eth/getMetadata";
+import Prints from "./Prints"
 
-export const metadata = getMetadata({
-  title: "Available NFTs",
-  description: "Available NFTs",
-});
 
 const Available: NextPage = () => {
   return (
-    <>
-      {/*<DebugContracts />*/}
-      <div className="text-center mt-8 bg-secondary p-10">
-        <h1 className="text-4xl my-0">Available Prints</h1>
-        <p className="text-neutral"></p>
-      </div>
-    </>
+    <div>
+      <h1>Prints</h1>
+      <Prints />
+    </div>
   );
 };
 
