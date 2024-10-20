80174b15323b8f14646425b4360c67c7fe3102b3
andrew.rottier <andrew.rottier@bytedance.com>
2024-10-19 23:44:58 -0700

    updates

---
+++ packages/nextjs/app/api/chat/route.ts
@@ -0,0 +1,20 @@
+import OpenAI from "openai";
+import { NextResponse } from "next/server";
+
+const openai = new OpenAI();
+
+export const runtime = "edge";
+
+export async function POST(req: Request) {
+    const { messages } = await req.json();
+
+    const response = await openai.chat.completions.create({
+        model: "gpt-4o-mini",
+        // model: "dall-e-2",
+        messages,
+    });
+
+    return NextResponse.json({
+        content: response.choices[0].message.content,
+    });
+}
