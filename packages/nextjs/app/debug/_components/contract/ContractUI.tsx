e8daa1611283e08203688b044e0f4d5d110217e3
coco <cocoboy1337@gmail.com>
2024-11-04 05:21:46 +0100

    ...

--- packages/nextjs/app/debug/_components/contract/ContractUI.tsx
+++ packages/nextjs/app/debug/_components/contract/ContractUI.tsx
@@ -26,8 +26,8 @@ export const ContractUI = ({ contractName, className = "" }: ContractUIProps) =>
 
   if (deployedContractLoading) {
     return (
-      <div className="mt-14">
-        <span className="loading loading-spinner loading-lg"></span>
+      <div className="mt-6 ml-6">
+        <span className="loading loading-spinner loading-xs loading-primary"></span>
       </div>
     );
   }
@@ -41,60 +41,52 @@ export const ContractUI = ({ contractName, className = "" }: ContractUIProps) =>
   }
 
   return (
-    <div className={`grid grid-cols-1 lg:grid-cols-6 px-6 lg:px-10 lg:gap-12 w-full max-w-7xl my-0 ${className}`}>
-      <div className="col-span-5 grid grid-cols-1 lg:grid-cols-3 gap-8 lg:gap-10">
-        <div className="col-span-1 flex flex-col">
-          <div className="bg-base-100 border-base-300 border shadow-md shadow-secondary rounded-3xl px-6 lg:px-8 mb-6 space-y-1 py-4">
-            <div className="flex">
-              <div className="flex flex-col gap-1">
-                <span className="font-bold">{contractName}</span>
-                <Address address={deployedContractData.address} onlyEnsOrAddress />
-                <div className="flex gap-1 items-center">
-                  <span className="font-bold text-sm">Balance:</span>
-                  <Balance address={deployedContractData.address} className="px-0 h-1.5 min-h-[0.375rem]" />
-                </div>
+    <div className={`${className} p-6`}>
+      <h1 className="text-xs my-0">Debug Contracts</h1>
+      <p className="text-primary mt-6">
+        You can debug & interact with your deployed contracts here. Check{" "}
+        <code className="bg-base-300 font-bold [word-spacing:-0.5rem] px-1">
+          packages / nextjs / app / debug / page.tsx
+        </code>{" "}
+      </p>
+      <div className="flex">
+        <div className="divide-y divide-base-300 w-1/2 border border-secondary">
+          <div className="p-6  min-w-80 ">
+            <p className="my-0 text-xs pb-6">Read</p>
+            <ContractReadMethods deployedContractData={deployedContractData} />
+          </div>
+          <div className="p-6  min-w-80 ">
+            <p className="my-0 text-xs pb-6">Write</p>
+            <ContractWriteMethods
+              deployedContractData={deployedContractData}
+              onChange={triggerRefreshDisplayVariables}
+            />
+          </div>
+        </div>
+
+        <div className="min-w-80 pl-6 w-1/2">
+          <div className="border border-primary p-6 space-y-6 text-primary">
+            <div className="flex flex-col space-y-3">
+              <div className="contract-name font-[900] pb-6 text-lg lg:text-2xl">{contractName}</div>
+              <Address address={deployedContractData.address} onlyEnsOrAddress />
+              <div className="flex gap-1 items-center">
+                <span className="font-bold text-sm">Balance:</span>
+                <Balance address={deployedContractData.address} className="px-0 h-1.5 min-h-[0.375rem]" />
               </div>
             </div>
+
             {targetNetwork && (
               <p className="my-0 text-sm">
                 <span className="font-bold">Network</span>:{" "}
                 <span style={{ color: networkColor }}>{targetNetwork.name}</span>
               </p>
             )}
-          </div>
-          <div className="bg-base-300 rounded-3xl px-6 lg:px-8 py-4 shadow-lg shadow-base-300">
-            <ContractVariables
-              refreshDisplayVariables={refreshDisplayVariables}
-              deployedContractData={deployedContractData}
-            />
-          </div>
-        </div>
-        <div className="col-span-1 lg:col-span-2 flex flex-col gap-6">
-          <div className="z-10">
-            <div className="bg-base-100 rounded-3xl shadow-md shadow-secondary border border-base-300 flex flex-col mt-10 relative">
-              <div className="h-[5rem] w-[5.5rem] bg-base-300 absolute self-start rounded-[22px] -top-[38px] -left-[1px] -z-10 py-[0.65rem] shadow-lg shadow-base-300">
-                <div className="flex items-center justify-center space-x-2">
-                  <p className="my-0 text-sm">Read</p>
-                </div>
-              </div>
-              <div className="p-5 divide-y divide-base-300">
-                <ContractReadMethods deployedContractData={deployedContractData} />
-              </div>
-            </div>
-          </div>
-          <div className="z-10">
-            <div className="bg-base-100 rounded-3xl shadow-md shadow-secondary border border-base-300 flex flex-col mt-10 relative">
-              <div className="h-[5rem] w-[5.5rem] bg-base-300 absolute self-start rounded-[22px] -top-[38px] -left-[1px] -z-10 py-[0.65rem] shadow-lg shadow-base-300">
-                <div className="flex items-center justify-center space-x-2">
-                  <p className="my-0 text-sm">Write</p>
-                </div>
-              </div>
-              <div className="p-5 divide-y divide-base-300">
-                <ContractWriteMethods
-                  deployedContractData={deployedContractData}
-                  onChange={triggerRefreshDisplayVariables}
-                />
-              </div>
+
+            <div className="pt-8">
+              <ContractVariables
+                refreshDisplayVariables={refreshDisplayVariables}
+                deployedContractData={deployedContractData}
+              />
             </div>
           </div>
         </div>
