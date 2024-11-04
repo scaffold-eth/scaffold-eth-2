c193e62199a62082c18f19072237d3e75c2b740d
coco <cocoboy1337@gmail.com>
2024-11-04 16:50:47 +0100

    receipt styling

--- packages/nextjs/app/debug/_components/contract/TxReceipt.tsx
+++ packages/nextjs/app/debug/_components/contract/TxReceipt.tsx
@@ -9,13 +9,10 @@ export const TxReceipt = ({ txResult }: { txResult: TransactionReceipt }) => {
   const [txResultCopied, setTxResultCopied] = useState(false);
 
   return (
-    <div className="flex text-sm rounded-3xl peer-checked:rounded-b-none min-h-0 bg-secondary py-0">
+    <div className="flex text-sm peer-checked:rounded-b-none min-h-0 bg-secondary/50 py-0">
       <div className="mt-1 pl-2">
         {txResultCopied ? (
-          <CheckCircleIcon
-            className="ml-1.5 text-xl font-normal text-sky-600 h-5 w-5 cursor-pointer"
-            aria-hidden="true"
-          />
+          <CheckCircleIcon className="mt-1 text-xl font-normal h-4 w-4 cursor-pointer" aria-hidden="true" />
         ) : (
           <CopyToClipboard
             text={JSON.stringify(txResult, replacer, 2)}
@@ -26,20 +23,17 @@ export const TxReceipt = ({ txResult }: { txResult: TransactionReceipt }) => {
               }, 800);
             }}
           >
-            <DocumentDuplicateIcon
-              className="ml-1.5 text-xl font-normal text-sky-600 h-5 w-5 cursor-pointer"
-              aria-hidden="true"
-            />
+            <DocumentDuplicateIcon className="mt-1 text-xl font-normal h-4 w-4 cursor-pointer" aria-hidden="true" />
           </CopyToClipboard>
         )}
       </div>
       <div className="flex-wrap collapse collapse-arrow">
         <input type="checkbox" className="min-h-0 peer" />
-        <div className="collapse-title text-sm min-h-0 py-1.5 pl-1 after:!top-4">
+        <div className="collapse-title text-xs min-h-0 py-1.5 pr-0 pl-1 after:!top-4">
           <strong>Transaction Receipt</strong>
         </div>
-        <div className="collapse-content overflow-auto bg-secondary rounded-t-none rounded-3xl !pl-0">
-          <pre className="text-xs">
+        <div className="collapse-content overflow-auto !pl-0 pr-6">
+          <pre className="text-[9px]">
             {Object.entries(txResult).map(([k, v]) => (
               <ObjectFieldDisplay name={k} value={v} size="xs" leftPad={false} key={k} />
             ))}
