62300549d55db7a57faa701fa1b8e45f44f137a1
coco <cocoboy1337@gmail.com>
2024-11-04 16:55:15 +0100

    tweak 'results' visuals

--- packages/nextjs/app/debug/_components/contract/ReadOnlyFunctionForm.tsx
+++ packages/nextjs/app/debug/_components/contract/ReadOnlyFunctionForm.tsx
@@ -76,15 +76,7 @@ export const ReadOnlyFunctionForm = ({
         <InheritanceTooltip inheritedFrom={inheritedFrom} />
       </p>
       {inputElements}
-      <div className="flex flex-col md:flex-row justify-between gap-2 flex-wrap">
-        <div className="flex-grow w-full md:max-w-[80%]">
-          {result !== null && result !== undefined && (
-            <div className="bg-secondary rounded-3xl text-sm px-4 py-1.5 break-words overflow-auto">
-              <p className="font-bold m-0 mb-1">Result:</p>
-              <pre className="whitespace-pre-wrap break-words">{displayTxResult(result, "sm")}</pre>
-            </div>
-          )}
-        </div>
+      <div className="space-y-3">
         <button
           className="btn btn-md btn-outline btn-primary rounded-full w-full lg:w-auto"
           onClick={async () => {
@@ -94,8 +86,16 @@ export const ReadOnlyFunctionForm = ({
           disabled={isFetching}
         >
           {isFetching && <span className="loading loading-spinner loading-xs"></span>}
-          Read 📡
+          Read
         </button>
+        <div className="flex-grow w-full">
+          {result !== null && result !== undefined && (
+            <div className="bg-secondary/50 text-primary rounded-none text-sm p-3 break-words overflow-auto">
+              <p className="font-bold m-0 mb-1">Result:</p>
+              <pre className="whitespace-pre-wrap break-words">{displayTxResult(result, "sm")}</pre>
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
