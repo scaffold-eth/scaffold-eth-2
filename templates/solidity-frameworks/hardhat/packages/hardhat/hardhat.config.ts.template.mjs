5680dd9f84ff025af31e27d6dea78d3a55fec94b
Shiv Bhonde <shivbhonde04@gmail.com>
2024-12-11 18:46:42 +0530

    fix hardhat.config template string with espace backticks

--- templates/solidity-frameworks/hardhat/packages/hardhat/hardhat.config.ts.template.mjs
+++ templates/solidity-frameworks/hardhat/packages/hardhat/hardhat.config.ts.template.mjs
@@ -36,7 +36,7 @@ ${imports.filter(Boolean).join("\n")}
 // You can get your own at https://dashboard.alchemyapi.io
 const providerApiKey = process.env.ALCHEMY_API_KEY || "oKxs-03sij-U_N0iOlrSsZFr29-IqbuF";
 // If not set, it uses the hardhat account 0 private key.
-// You can generate a random account with `yarn generate` or `yarn account:import` to import your existing PK
+// You can generate a random account with \`yarn generate\` or \`yarn account:import\` to import your existing PK
 const deployerPrivateKey =
   process.env.__RUNTIME_DEPLOYER_PRIVATE_KEY ?? "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80";
 // If not set, it uses our block explorers default API keys.
