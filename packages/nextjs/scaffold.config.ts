21aef39d5f584476189c299ef3f301f58e60ba66
Carlos Sánchez <oceanrdn@gmail.com>
2025-01-30 14:36:59 +0100

    Reduce polling interval

--- packages/nextjs/scaffold.config.ts
+++ packages/nextjs/scaffold.config.ts
@@ -16,7 +16,7 @@ const scaffoldConfig = {
 
   // The interval at which your front-end polls the RPC servers for new data
   // it has no effect if you only target the local network (default is 4000)
-  pollingInterval: 30000,
+  pollingInterval: 3000,
 
   // This is ours Alchemy's default API key.
   // You can get your own at https://dashboard.alchemyapi.io
