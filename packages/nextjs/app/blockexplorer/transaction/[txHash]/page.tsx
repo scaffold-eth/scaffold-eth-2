b5dd20b234ed0152b711c393730d1835586a4b65
michaelliu <denchance@gmail.com>
2025-02-27 15:29:17 +0800

    Update page.tsx
    It might be 64-Hex.
    So,
    changing it from 
    0x0000000000000000000000000000000000000000
    to
    0x0000000000000000000000000000000000000000000000000000000000000000

--- packages/nextjs/app/blockexplorer/transaction/[txHash]/page.tsx
+++ packages/nextjs/app/blockexplorer/transaction/[txHash]/page.tsx
@@ -9,7 +9,8 @@ type PageProps = {
 
 export function generateStaticParams() {
   // An workaround to enable static exports in Next.js, generating single dummy page.
-  return [{ txHash: "0x0000000000000000000000000000000000000000" }];
+  // return [{ txHash: "0x0000000000000000000000000000000000000000" }];
+  return [{ txHash: "0x0000000000000000000000000000000000000000000000000000000000000000" }];
 }
 const TransactionPage: NextPage<PageProps> = ({ params }: PageProps) => {
   const txHash = params?.txHash as Hash;
