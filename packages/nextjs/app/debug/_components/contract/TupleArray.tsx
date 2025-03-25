09556ca6c554f2ae2d45bd4cde3d358e757e8460
Carlos Sánchez <oceanrdn@gmail.com>
2025-03-25 10:46:45 +0100

    Run TWv4 codemod tool. Missing DaisyUI themes / migration

--- packages/nextjs/app/debug/_components/contract/TupleArray.tsx
+++ packages/nextjs/app/debug/_components/contract/TupleArray.tsx
@@ -102,7 +102,7 @@ export const TupleArray = ({ abiTupleParameter, setParentForm, parentStateObject
     <div>
       <div className="collapse collapse-arrow bg-base-200 pl-4 py-1.5 border-2 border-secondary">
         <input type="checkbox" className="min-h-fit peer" />
-        <div className="collapse-title after:!top-3.5 p-0 min-h-fit peer-checked:mb-1 text-primary-content/50">
+        <div className="collapse-title after:top-3.5! p-0 min-h-fit peer-checked:mb-1 text-primary-content/50">
           <p className="m-0 text-[1rem]">{abiTupleParameter.internalType}</p>
         </div>
         <div className="ml-3 flex-col space-y-2 border-secondary/70 border-l-2 pl-4 collapse-content">
