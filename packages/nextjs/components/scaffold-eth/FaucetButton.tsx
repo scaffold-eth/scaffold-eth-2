dc14666009c675c38447cff216ad66ec36487dc9
Shiv Bhonde <shivbhonde04@gmail.com>
2025-04-10 12:34:53 +0530

    change tooltip color to primary

--- packages/nextjs/components/scaffold-eth/FaucetButton.tsx
+++ packages/nextjs/components/scaffold-eth/FaucetButton.tsx
@@ -57,7 +57,7 @@ export const FaucetButton = () => {
       className={
         !isBalanceZero
           ? "ml-1"
-          : "ml-1 tooltip tooltip-bottom tooltip-secondary tooltip-open font-bold before:left-auto before:transform-none before:content-[attr(data-tip)] before:-translate-x-2/5"
+          : "ml-1 tooltip tooltip-bottom tooltip-primary tooltip-open font-bold before:left-auto before:transform-none before:content-[attr(data-tip)] before:-translate-x-2/5"
       }
       data-tip="Grab funds from faucet"
     >
