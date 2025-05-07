79a46cd37da909f3d9ad1a7ab73ba36d6e79e294
Rinat <rinat@hey.com>
2025-05-07 14:48:25 +0300

    feat: header dropdown menu

--- packages/nextjs/components/Header.tsx
+++ packages/nextjs/components/Header.tsx
@@ -4,6 +4,7 @@ import React, { useRef } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { Dropdown, DropdownItem } from "./Dropdown";
 import { hardhat } from "viem/chains";
 import { Bars3Icon, BugAntIcon } from "@heroicons/react/24/outline";
 import { FaucetButton, RainbowKitCustomConnectButton } from "~~/components/scaffold-eth";
@@ -27,30 +28,22 @@ export const menuLinks: HeaderMenuLink[] = [
   },
 ];
 
-export const HeaderMenuLinks = () => {
-  const pathname = usePathname();
-
-  return (
-    <>
-      {menuLinks.map(({ label, href, icon }) => {
-        const isActive = pathname === href;
-        return (
-          <li key={href}>
-            <Link
-              href={href}
-              passHref
-              className={`${
-                isActive ? "bg-secondary shadow-md" : ""
-              } hover:bg-secondary hover:shadow-md focus:!bg-secondary active:!text-neutral py-1.5 px-3 text-sm rounded-full gap-2 grid grid-flow-col`}
-            >
-              {icon}
-              <span>{label}</span>
-            </Link>
-          </li>
-        );
-      })}
-    </>
-  );
+const getHeaderMenuLinks = (pathname: string) => {
+  return menuLinks.map(({ label, href, icon }) => {
+    const isActive = pathname === href;
+    return (
+      <Link
+        key={href}
+        href={href}
+        className={`${
+          isActive ? "bg-secondary shadow-md" : ""
+        } hover:bg-secondary hover:shadow-md w-full text-nowrap focus:!bg-secondary active:!text-neutral py-1.5 px-3 text-sm rounded-full gap-2 flex items-center`}
+      >
+        {icon}
+        <span>{label}</span>
+      </Link>
+    );
+  });
 };
 
 /**
@@ -59,6 +52,7 @@ export const HeaderMenuLinks = () => {
 export const Header = () => {
   const { targetNetwork } = useTargetNetwork();
   const isLocalNetwork = targetNetwork.id === hardhat.id;
+  const pathname = usePathname();
 
   const burgerMenuRef = useRef<HTMLDetailsElement>(null);
   useOutsideClick(burgerMenuRef, () => {
@@ -68,19 +62,17 @@ export const Header = () => {
   return (
     <div className="sticky lg:static top-0 navbar bg-base-100 min-h-0 shrink-0 justify-between z-20 shadow-md shadow-secondary px-0 sm:px-2">
       <div className="navbar-start w-auto lg:w-1/2">
-        <details className="dropdown" ref={burgerMenuRef}>
-          <summary className="ml-1 btn btn-ghost lg:hidden hover:bg-transparent">
-            <Bars3Icon className="h-1/2" />
-          </summary>
-          <ul
-            className="menu menu-compact dropdown-content mt-3 p-2 shadow-sm bg-base-100 rounded-box w-52"
-            onClick={() => {
-              burgerMenuRef?.current?.removeAttribute("open");
-            }}
-          >
-            <HeaderMenuLinks />
-          </ul>
-        </details>
+        <Dropdown
+          triggerContent={<Bars3Icon className="h-6 w-6 hover:bg-transparent" />}
+          triggerClassName="rounded-md p-1"
+          className="lg:hidden"
+          hideChevron
+        >
+          {getHeaderMenuLinks(pathname).map((headerMenuLink, idx) => (
+            <DropdownItem key={idx}>{headerMenuLink}</DropdownItem>
+          ))}
+        </Dropdown>
+
         <Link href="/" passHref className="hidden lg:flex items-center gap-2 ml-4 mr-6 shrink-0">
           <div className="flex relative w-10 h-10">
             <Image alt="SE2 logo" className="cursor-pointer" fill src="/logo.svg" />
@@ -90,9 +82,7 @@ export const Header = () => {
             <span className="text-xs">Ethereum dev stack</span>
           </div>
         </Link>
-        <ul className="hidden lg:flex lg:flex-nowrap menu menu-horizontal px-1 gap-2">
-          <HeaderMenuLinks />
-        </ul>
+        <div className="hidden lg:flex lg:flex-nowrap px-1 gap-2">{getHeaderMenuLinks(pathname)}</div>
       </div>
       <div className="navbar-end grow mr-4">
         <RainbowKitCustomConnectButton />
