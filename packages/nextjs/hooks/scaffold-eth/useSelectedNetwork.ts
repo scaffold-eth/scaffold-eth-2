9f908f4eae4c64de690959809aa8697881436749
pabl0cks <pablo@asdloop.com>
2025-05-08 15:37:25 +0200

    Add chain metadata to selected network returned

--- packages/nextjs/hooks/scaffold-eth/useSelectedNetwork.ts
+++ packages/nextjs/hooks/scaffold-eth/useSelectedNetwork.ts
@@ -1,8 +1,12 @@
 import scaffoldConfig from "~~/scaffold.config";
 import { useGlobalState } from "~~/services/store/store";
 import { AllowedChainIds } from "~~/utils/scaffold-eth";
+import { NETWORKS_EXTRA_DATA } from "~~/utils/scaffold-eth/networks";
 
 export function useSelectedNetwork(chainId?: AllowedChainIds) {
-  const targetNetwork = useGlobalState(({ targetNetwork }) => targetNetwork);
-  return scaffoldConfig.targetNetworks.find(targetNetwork => targetNetwork.id === chainId) ?? targetNetwork;
+  const globalTargetNetwork = useGlobalState(({ targetNetwork }) => targetNetwork);
+  const targetNetwork =
+    scaffoldConfig.targetNetworks.find(targetNetwork => targetNetwork.id === chainId) ?? globalTargetNetwork;
+
+  return targetNetwork ? { ...targetNetwork, ...NETWORKS_EXTRA_DATA[targetNetwork.id] } : targetNetwork;
 }
