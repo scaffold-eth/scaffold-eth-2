90afb0dd23776d78c2f8bfa57bf8a847e261c6ff
Shiv Bhonde <shivbhonde04@gmail.com>
2025-05-12 11:48:48 +0530

    just return targetNetwork in useTargetNetwork hook

--- packages/nextjs/hooks/scaffold-eth/useTargetNetwork.ts
+++ packages/nextjs/hooks/scaffold-eth/useTargetNetwork.ts
@@ -20,13 +20,5 @@ export function useTargetNetwork(): { targetNetwork: ChainWithAttributes } {
     }
   }, [chain?.id, setTargetNetwork, targetNetwork.id]);
 
-  return useMemo(
-    () => ({
-      targetNetwork: {
-        ...targetNetwork,
-        ...NETWORKS_EXTRA_DATA[targetNetwork.id],
-      },
-    }),
-    [targetNetwork],
-  );
+  return useMemo(() => ({ targetNetwork }), [targetNetwork]);
 }
