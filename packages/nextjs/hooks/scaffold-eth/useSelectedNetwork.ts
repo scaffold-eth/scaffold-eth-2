b4bf4d53668723649493a787bdfcd13281f1dad4
Shiv Bhonde <shivbhonde04@gmail.com>
2025-05-12 11:41:05 +0530

    add comment for useSelectNetwork hook

--- packages/nextjs/hooks/scaffold-eth/useSelectedNetwork.ts
+++ packages/nextjs/hooks/scaffold-eth/useSelectedNetwork.ts
@@ -3,6 +3,10 @@ import { useGlobalState } from "~~/services/store/store";
 import { AllowedChainIds } from "~~/utils/scaffold-eth";
 import { NETWORKS_EXTRA_DATA } from "~~/utils/scaffold-eth/networks";
 
+/**
+ * Given a chainId, retrives the network object from `scaffold.config`,
+ * if not found default to network set by `useTargetNetwork` hook
+ */
 export function useSelectedNetwork(chainId?: AllowedChainIds) {
   const globalTargetNetwork = useGlobalState(({ targetNetwork }) => targetNetwork);
   const targetNetwork =
