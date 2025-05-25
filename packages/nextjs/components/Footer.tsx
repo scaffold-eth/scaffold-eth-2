142e53a94966f0e358c5fa74f78a3bec3b152918
gotnoshoeson <miles_patterson@icloud.com>
2025-05-25 08:45:51 -0700

    Footer links

--- packages/nextjs/components/Footer.tsx
+++ packages/nextjs/components/Footer.tsx
@@ -47,7 +47,7 @@ export const Footer = () => {
         <ul className="menu menu-horizontal w-full">
           <div className="flex justify-center items-center gap-2 text-sm w-full">
             <div className="text-center">
-              <a href="https://github.com/scaffold-eth/se-2" target="_blank" rel="noreferrer" className="link">
+              <a href="https://github.com/scaffold-dot/scaffold-dot" target="_blank" rel="noreferrer" className="link">
                 Fork me
               </a>
             </div>
@@ -68,7 +68,7 @@ export const Footer = () => {
             </div>
             <span>·</span>
             <div className="text-center">
-              <a href="https://t.me/joinchat/KByvmRe5wkR-8F_zz6AjpA" target="_blank" rel="noreferrer" className="link">
+              <a href="https://github.com/scaffold-dot/scaffold-dot" target="_blank" rel="noreferrer" className="link">
                 Support
               </a>
             </div>
