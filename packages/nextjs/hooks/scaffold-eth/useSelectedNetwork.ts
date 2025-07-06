efc0bca42eae543d6ae1cdc47c93282dcf5e3af3
leopardracer <136604165+leopardracer@users.noreply.github.com>
2025-07-06 16:02:10 +0300

    Update useSelectedNetwork.ts

--- packages/nextjs/hooks/scaffold-eth/useSelectedNetwork.ts
+++ packages/nextjs/hooks/scaffold-eth/useSelectedNetwork.ts
@@ -4,7 +4,7 @@ import { AllowedChainIds } from "~~/utils/scaffold-eth";
 import { ChainWithAttributes, NETWORKS_EXTRA_DATA } from "~~/utils/scaffold-eth/networks";
 
 /**
- * Given a chainId, retrives the network object from `scaffold.config`,
+ * Given a chainId, retrieves the network object from `scaffold.config`,
  * if not found default to network set by `useTargetNetwork` hook
  */
 export function useSelectedNetwork(chainId?: AllowedChainIds): ChainWithAttributes {
