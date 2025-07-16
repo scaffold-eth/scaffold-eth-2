b6a7c80777545f604fec4b10290a0dc5916203dd
Carlos Sánchez <oceanrdn@gmail.com>
2025-07-16 17:31:00 +0200

    Show create-eth version on initial commit (#314)

--- src/tasks/create-first-git-commit.ts
+++ src/tasks/create-first-git-commit.ts
@@ -2,13 +2,17 @@ import { execa } from "execa";
 import { Options } from "../types";
 import path from "path";
 import { SOLIDITY_FRAMEWORKS } from "../utils/consts";
+import packageJson from "../../package.json";
 
 const foundryLibraries = ["foundry-rs/forge-std", "OpenZeppelin/openzeppelin-contracts", "gnsps/solidity-bytes-utils"];
+const createEthVersion = packageJson.version;
 
 export async function createFirstGitCommit(targetDir: string, options: Options) {
   try {
     await execa("git", ["add", "-A"], { cwd: targetDir });
-    await execa("git", ["commit", "-m", "Initial commit with 🏗️ Scaffold-ETH 2", "--no-verify"], { cwd: targetDir });
+    await execa("git", ["commit", "-m", `Initial commit with 🏗️ Scaffold-ETH 2 @ ${createEthVersion}`, "--no-verify"], {
+      cwd: targetDir,
+    });
 
     if (options.solidityFramework === SOLIDITY_FRAMEWORKS.FOUNDRY) {
       const foundryWorkSpacePath = path.resolve(targetDir, "packages", SOLIDITY_FRAMEWORKS.FOUNDRY);
