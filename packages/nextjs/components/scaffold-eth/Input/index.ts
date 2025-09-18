743d9fef465e13d7b51e31ff32c7a2412d0eb729
Shiv Bhonde <shivbhonde04@gmail.com>
2025-09-18 22:03:01 +0530

    remove all Inputs

--- packages/nextjs/components/scaffold-eth/Input/index.ts
+++
@@ -1,8 +0,0 @@
-"use client";
-
-export * from "./AddressInput";
-export * from "./Bytes32Input";
-export * from "./BytesInput";
-export * from "./InputBase";
-export * from "./IntegerInput";
-export * from "./utils";
