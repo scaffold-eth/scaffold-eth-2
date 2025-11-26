56b893ffab873ef8e4ad61bbb4862dcd2f93f1da
Farhan <144435177+toufiqfarhan0@users.noreply.github.com>
2025-11-26 09:55:00 +0000

    1

--- packages/nextjs/components/Header.tsx
+++ packages/nextjs/components/Header.tsx
@@ -83,11 +83,11 @@ export const Header = () => {
         </details>
         <Link href="/" passHref className="hidden lg:flex items-center gap-2 ml-4 mr-6 shrink-0">
           <div className="flex relative w-10 h-10">
-            <Image alt="SE2 logo" className="cursor-pointer" fill src="/logo.svg" />
+            <Image alt="SE2 logo" className="cursor-pointer" fill src="/Logomark.svg" />
           </div>
           <div className="flex flex-col">
-            <span className="font-bold leading-tight">Scaffold-ETH</span>
-            <span className="text-xs">Ethereum dev stack</span>
+            <span className="font-bold leading-tight">My First Dapp</span>
+            <span className="text-xs">for learning monad</span>
           </div>
         </Link>
         <ul className="hidden lg:flex lg:flex-nowrap menu menu-horizontal px-1 gap-2">
