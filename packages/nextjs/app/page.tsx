5254393fa139f33574ef02a24c535d4145174318
Farhan <144435177+toufiqfarhan0@users.noreply.github.com>
2025-11-26 15:49:30 +0000

    basic UI

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -1,35 +1,42 @@
 "use client";
 
 // import Link from "next/link";
-import { Address } from "@scaffold-ui/components";
+// import { Address } from "@scaffold-ui/components";
 import type { NextPage } from "next";
-import { hardhat } from "viem/chains";
-import { useAccount } from "wagmi";
+
+// import { hardhat } from "viem/chains";
+// import { useAccount } from "wagmi";
 // import { BugAntIcon, MagnifyingGlassIcon } from "@heroicons/react/24/outline";
-import { useTargetNetwork } from "~~/hooks/scaffold-eth";
+// import { useTargetNetwork } from "~~/hooks/scaffold-eth";
 
 const Home: NextPage = () => {
-  const { address: connectedAddress } = useAccount();
-  const { targetNetwork } = useTargetNetwork();
+  // const { address: connectedAddress } = useAccount();
+  // const { targetNetwork } = useTargetNetwork();
 
   return (
     <>
-      <div className="flex items-center flex-col grow pt-10">
-        <div className="px-5">
-          <h1 className="text-center">
-            <span className="block text-2xl mb-2">Welcome to</span>
-            <span className="block text-4xl font-bold">Monad</span>
-          </h1>
-          <div className="flex justify-center items-center space-x-2 flex-col">
-            <p className="my-2 font-medium">Connected Address:</p>
-            <Address
-              address={connectedAddress}
-              chain={targetNetwork}
-              blockExplorerAddressLink={
-                targetNetwork.id === hardhat.id ? `/blockexplorer/address/${connectedAddress}` : undefined
-              }
-            />
-          </div>
+      <div className="grid grid-cols-12 gap-x-4 m-5">
+        <div className="col-span-6">Wallet Connection Status</div>
+        <div className="col-span-6">Network</div>
+        <div className="col-span-6">
+          <input type="text" placeholder="Not Connected!" className="input !input-error rounded-md w-full" disabled />
+        </div>
+        <div className="col-span-6">
+          <input type="text" placeholder="No Network!" className="input !input-error rounded-md w-full" disabled />
+        </div>
+      </div>
+      <div className="grid grid-cols-12 gap-x-4 m-5">
+        <div className="col-span-4">Wallet Address</div>
+        <div className="col-span-4">Monad Name Service</div>
+        <div className="col-span-4">Wallet Balance</div>
+        <div className="col-span-4">
+          <input type="text" placeholder="0x" className="input !input-error rounded-md w-full" disabled />
+        </div>
+        <div className="col-span-4">
+          <input type="text" placeholder="xxxxx.nad" className="input !input-error rounded-md w-full" disabled />
+        </div>
+        <div className="col-span-4">
+          <input type="text" placeholder="0 MON" className="input !input-error rounded-md w-full" disabled />
         </div>
       </div>
     </>
--- packages/nextjs/components/Header.tsx
+++ packages/nextjs/components/Header.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { hardhat } from "viem/chains";
-import { Bars3Icon, BugAntIcon } from "@heroicons/react/24/outline";
+import { Bars3Icon } from "@heroicons/react/24/outline";
 import { FaucetButton, RainbowKitCustomConnectButton } from "~~/components/scaffold-eth";
 import { useOutsideClick, useTargetNetwork } from "~~/hooks/scaffold-eth";
 
@@ -19,11 +19,6 @@ export const menuLinks: HeaderMenuLink[] = [
   {
     label: "Home",
     href: "/",
-  },
-  {
-    label: "Debug Contracts",
-    href: "/debug",
-    icon: <BugAntIcon className="h-4 w-4" />,
   },
 ];
 
