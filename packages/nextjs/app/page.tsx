6bbe2a3d7923c2d548ece567530fb3f3f5141d58
Farhan <144435177+toufiqfarhan0@users.noreply.github.com>
2025-11-27 12:48:42 +0530

    ui done

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -3,14 +3,15 @@
 // import Link from "next/link";
 // import { Address } from "@scaffold-ui/components";
 import type { NextPage } from "next";
-
 // import { hardhat } from "viem/chains";
-// import { useAccount } from "wagmi";
+import { useAccount, useBalance } from "wagmi";
+
 // import { BugAntIcon, MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 // import { useTargetNetwork } from "~~/hooks/scaffold-eth";
 
 const Home: NextPage = () => {
-  // const { address: connectedAddress } = useAccount();
+  const { address, chain, isConnected } = useAccount();
+  const balance = useBalance({ address: address });
   // const { targetNetwork } = useTargetNetwork();
 
   return (
@@ -19,10 +20,18 @@ const Home: NextPage = () => {
         <div className="col-span-6">Wallet Connection Status</div>
         <div className="col-span-6">Network</div>
         <div className="col-span-6">
-          <input type="text" placeholder="Not Connected!" className="input !input-error rounded-md w-full" disabled />
+          <input
+            type="text"
+            placeholder={isConnected ? "Connected" : "Not Connected"}
+            className={`input ${isConnected ? "!input-success" : "!input-error"} rounded-md w-full`}
+          />
         </div>
         <div className="col-span-6">
-          <input type="text" placeholder="No Network!" className="input !input-error rounded-md w-full" disabled />
+          <input
+            type="text"
+            placeholder={isConnected ? chain?.name || "unknowm" : "No Nework"}
+            className={`input ${isConnected ? "!input-success" : "!input-error"} rounded-md w-full`}
+          />
         </div>
       </div>
       <div className="grid grid-cols-12 gap-x-4 m-5">
@@ -30,13 +39,31 @@ const Home: NextPage = () => {
         <div className="col-span-4">Monad Name Service</div>
         <div className="col-span-4">Wallet Balance</div>
         <div className="col-span-4">
-          <input type="text" placeholder="0x" className="input !input-error rounded-md w-full" disabled />
+          <input
+            type="text"
+            placeholder={isConnected ? address : "No Nework"}
+            className={`input ${isConnected ? "!input-success" : "!input-error"} rounded-md w-full`}
+          />
         </div>
         <div className="col-span-4">
-          <input type="text" placeholder="xxxxx.nad" className="input !input-error rounded-md w-full" disabled />
+          <input
+            type="text"
+            placeholder="xxxxx.nad"
+            className={`input ${isConnected ? "!input-success" : "!input-error"} rounded-md w-full`}
+          />
         </div>
         <div className="col-span-4">
-          <input type="text" placeholder="0 MON" className="input !input-error rounded-md w-full" disabled />
+          <input
+            type="text"
+            placeholder={
+              balance.isLoading
+                ? "loading.."
+                : balance.data
+                  ? `${balance.data?.formatted} ${balance.data.symbol}`
+                  : "Not Connected!"
+            }
+            className={`input ${isConnected ? "!input-success" : "!input-error"} rounded-md w-full`}
+          />
         </div>
       </div>
 
@@ -47,7 +74,7 @@ const Home: NextPage = () => {
         </div>
         <div className="col-span-4">
           {" "}
-          <input type="text" placeholder="0 NFT" className="input !input-error rounded-md w-100" disabled />
+          <input type="text" placeholder="0 NFT" className="input !input-error rounded-md w-100" />
         </div>
       </div>
     </>
