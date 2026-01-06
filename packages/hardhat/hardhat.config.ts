60aa5f8d4befe0ccc7f47c1cc6e2d81b30ba3f84
Shiv Bhonde <shivbhonde04@gmail.com>
2026-01-06 14:35:40 +0530

    use latest solidity version

--- packages/hardhat/hardhat.config.ts
+++ packages/hardhat/hardhat.config.ts
@@ -27,7 +27,7 @@ const config: HardhatUserConfig = {
   solidity: {
     compilers: [
       {
-        version: "0.8.20",
+        version: "0.8.28",
         settings: {
           optimizer: {
             enabled: true,
